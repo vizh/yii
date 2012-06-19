0990a417a0529c1290b65c55692f767686bd0a5e
Nikitin Vitaly <alaris.nik@gmail.com>
2012-06-19 16:15:44 +0400

    Добавил партнерский билдер для Api

---
+++ library/rocid/api/builders/PartnerDataBuilder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nikitin
+ * Date: 19.06.12
+ * Time: 16:03
+ * To change this template use File | Settings | File Templates.
+ */
+class PartnerDataBuilder extends BaseDataBuilder
+{
+
+}
