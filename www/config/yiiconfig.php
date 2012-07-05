d52db945f3978768a13b80cc296f34541ad5bd2f
Nikitin Vitaly <alaris.nik@gmail.com>
2012-07-05 15:58:51 +0400

    Изменение тестовых файлов

--- www/config/yiiconfig.php
+++ www/config/yiiconfig.php
@@ -52,6 +52,7 @@ return array(
       'connectionString' => 'mysql:host=localhost;dbname=rocidbeta',
       'emulatePrepare' => true,
       'username' => 'root',
+      'password' => '123456',
       'charset' => 'utf8',
       'enableProfiling' => true,
       'enableParamLogging'=>true,
