223956293917001728ccc87c5f730966756b9482
Maxim Borzov <max.borzov@gmail.com>
2012-08-14 18:48:19 +0400

    Closes ticket #54
    Added new tag "x-webkit-speech" into search input

--- templates/core/public/widgets/userbar/auth.php
+++ templates/core/public/widgets/userbar/auth.php
@@ -3,14 +3,9 @@
 	<div class="center">
 	<ul>
     <li><span style="color: #000;"><strong><?=number_format($this->CountEventUsers, 0, '.', ' ');?></strong> <?=Yii::t('app', 'регистрация|регистрации|регистраций|регистрации', $this->CountEventUsers);?> на мероприятия</span></li>
-		<!--<li><a href="">Обновления</a> (12)</li>
-		<li><a href="">Календарь</a> (3)</li>
-		<li><a href="">Уведомления</a> (2)</li>-->
 		<li>
 			<form class="panel-search" action="/search/" method="get" accept-charset="utf-8">
-				<input id="search" class="in-text js-autosuggest-field"
-                name="q" type="text"
-            placeholder="поиск по людям, компаниям, новостям" value=""  autocomplete="off">
+				<input id="search" class="in-text js-autosuggest-field" name="q" type="text" placeholder="поиск по людям, компаниям, новостям" value="" autocomplete="off" x-webkit-speech="x-webkit-speech" />
 				<div class="js-autosuggest-output-container"><div class="js-autosuggest-output hidden"></div></div>
 			</form>
 		</li>
--- templates/core/public/widgets/userbar/nonauth.php
+++ templates/core/public/widgets/userbar/nonauth.php
@@ -5,8 +5,7 @@
     <li><span style="color: #000;"><strong><?=number_format($this->CountEventUsers, 0, '.', ' ');?></strong> <?=Yii::t('app', 'регистрация|регистрации|регистраций|регистрации', $this->CountEventUsers);?> на мероприятия</span></li>
 		<li>
 			<form class="panel-search" action="/search/" method="get" accept-charset="utf-8">
-				<input id="search" class="in-text js-autosuggest-field"
-                name="q" type="text" placeholder="поиск по людям, компаниям, новостям"  value=""  autocomplete="off">
+				<input id="search" class="in-text js-autosuggest-field" name="q" type="text" placeholder="поиск по людям, компаниям, новостям" value="" autocomplete="off" x-webkit-speech="x-webkit-speech" />
 				<div class="js-autosuggest-output-container"><div class="js-autosuggest-output hidden"></div></div>
 			</form>
 		</li>
