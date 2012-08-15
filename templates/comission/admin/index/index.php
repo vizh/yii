7eda2d5a1d4c280d1f15092257e6bdf44d59f609
Maxim Borzov <max.borzov@gmail.com>
2012-08-15 21:37:39 +0400

    Close ticket #94

--- templates/comission/admin/index/index.php
+++ templates/comission/admin/index/index.php
@@ -1,18 +1,10 @@
 <div class="row">
   <div class="span16">
-    <a href="<?=RouteRegistry::GetAdminUrl('comission', '', 'edit');?>" class="button positive"><span class="plus icon"></span>Добавить комиссию</a>
-  </div>
-</div>
-    <hr>
-<div class="row">
-  <div class="span16">
     <?if (!empty($this->Comissions)):?>
     <table>
       <thead>
       <tr>
-        <th>Название</th>
-        <th>Описание</th>
-        <!--<th>Количество участников</th>-->
+        <th>Комиссия</th>
         <th>Управление</th>
       </tr>
       </thead>
@@ -22,5 +14,13 @@
     </table>
     <?else:?>
     <?endif;?>
+  </div>
+</div>
+
+<hr>
+
+<div class="row">
+  <div class="span16">
+      <a href="<?=RouteRegistry::GetAdminUrl('comission', '', 'edit');?>" class="btn btn-primary"><span class="plus icon"></span>Добавить комиссию</a>
   </div>
 </div>
