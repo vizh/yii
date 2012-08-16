872cba3986143522d2fedb19435e8cfe3a40c174
Korotov Andrey <korotov@internetmediaholding.com>
2012-08-16 14:15:46 +0400

    Сделал таск 61

--- api/application/pay/public/PayUrl.php
+++ api/application/pay/public/PayUrl.php
@@ -8,14 +8,19 @@
  */
 class PayUrl extends ApiCommand
 {
-
   /**
    * Основные действия комманды
    * @return void
    */
   protected function doExecute()
   {
-    $result = 'http://pay.rocid.ru/' . $this->Account->EventId . '/';
+    $payerRocId = (int) Registry::GetRequestVar('PayerRocId', null);
+    if ($payerRocId == 0)
+    {
+      throw new ApiException(110);
+    }
+    $result = new stdClass();
+    $result->URL = 'http://pay.'.ROCID_HOST.'/auth/'.$this->Account->EventId.'/'.$payerRocId.'/';
     $this->SendJson($result);
   }
 }
