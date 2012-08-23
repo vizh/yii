25384c71eae8355809d4a6c8a898656259149243
Maxim Borzov <max.borzov@gmail.com>
2012-08-23 23:35:11 +0400

    Hide updates from main page and clear some shit

--- templates/core/public/activity/main.php
+++ templates/core/public/activity/main.php
@@ -1,70 +1,14 @@
+<!--
 <div class="content-header">
   <div class="title">Обновления</div>
-  <!--<ul class="content-links">
+  <ul class="content-links">
     <li><a href="#" class="all">все</a></li>
     <li class="icon-1"><a href="#">люди</a></li>
     <li class="icon-2"><a href="#">мероприятия</a></li>
     <li class="icon-3"><a href="#">медиа</a></li>
     <li class="icon-4"><a href="#">компании</a></li>
-  </ul>-->
+  </ul>
 </div>
 
 <?php echo $this->Days;?>
-
-
-<!--
-<div class="line-1">
-  <a href="#" class="blue">Аскар Туганбаев</a> добавил новое место работы:
-  <a href="#" class="blue">СТС Медиа</a> (с марта 2010 года);
-</div>
-<div class="line-2">Открыта регистрация на конференцию <a href="#" class="blue">КИБ+РИФ 2010</a>
-  <p><a href="#" class="red">вступить в группу</a></p>
-</div>
-<div class="line-3">
-  Вышел второй номер журнала <a href="#" class="blue">«Интернет в цифрах»</a>
-</div>
-<div class="line-4">
-  Яндекс: <a href="#" class="blue">Вперёд в прошлое на Яндексе</a>
-  <p class="quote">В День космонавтики так и хочется подумать о необычных путешествиях – а что может быть
-    интереснее путешествий во времени?Сегодня на главной странице Яндекса под логотипом появилась
-    кнопка «Назад, в шестидесятые» (если у вас уже установлен какой-нибудь скин - вы увидите ссылку на…</p>
-  <p><a href="#" class="red">комментировать</a></p>
-</div>
-<div class="line-4">
-  <a href="#" class="blue">Мельников Максим</a> обновил свои контактные данные: тел. +7 (903) 123-45-67
-</div>
-<div class="line-2">
-  <div class="line-text">
-    <a href="#" class="blue">Валентин Голев</a>,
-    <a href="#" class="blue">Владимир Шахиджанян</a> и  ещё 3 человека пойдут на
-    <a href="#" class="blue">DrupalCamp</a>
-    <p><a href="#" class="red">присоединиться</a></p>
-  </div>
-  <div class="line-logo"><a href="#"><img src="images/drupalcamp.jpg" alt=""></a></div>
-</div>
-
-<div class="news-date-2">8 апреля</div>
-<div class="line-2"><a href="#" class="blue">Чистов Дмитрий</a> был отмечен на фотографиях с <a href="#" class="blue">КИБ+РИФ 2010</a>
-  <ul class="gallery">
-    <li><a href="#"><img src="images/image-2.jpg" alt=""></a></li>
-    <li><a href="#"><img src="images/image-3.jpg" alt=""></a></li>
-    <li><a href="#"><img src="images/image-4.jpg" alt=""></a></li>
-    <li><a href="#"><img src="images/image-5.jpg" alt=""></a></li>
-  </ul>
-</div>
-<div class="line-2">
-  Открыта регистрация на конференцию <a href="#" class="blue">КИБ+РИФ 2010</a>
-  <p><a href="#" class="red">вступить в группу</a></p>
-</div>
-<div class="line-3">Вышел второй номер журнала <a href="#" class="blue">«Интернет в цифрах»</a></div>
-<div class="line-4">Яндекс: <a href="#" class="blue">Вперёд в прошлое на Яндексе</a>
-  <p class="quote">В День космонавтики так и хочется подумать о необычных путешествиях – а что может быть
-    интереснее путешествий во времени?Сегодня на главной странице Яндекса под логотипом появилась
-    кнопка «Назад, в шестидесятые» (если у вас уже установлен какой-нибудь скин - вы увидите ссылку на…</p>
-  <p><a href="#" class="red">комментировать</a></p>
-</div>
-<div class="line-4"><a href="#" class="blue">Мельников Максим</a> обновил свои контактные данные: тел. +7 (903) 123-45-67</div>
-<div class="line-2">
-  <a href="#" class="blue">Валентин Голев</a>, <a href="#" class="blue">Владимир Шахиджанян</a> и  ещё 3 человека пойдут на <a href="#" class="blue">DrupalCamp</a>
-  <p><a href="#" class="red">присоединиться</a></p>
-</div>-->
+-->
