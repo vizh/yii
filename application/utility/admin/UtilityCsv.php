cab66e37fa3afc1e855918349d926c3c1e85d924
Nikitin Vitaly <alaris.nik@gmail.com>
2012-09-25 18:56:41 +0400

    Скрипт выборки пользователей сайт-12

--- application/utility/admin/UtilityCsv.php
+++ application/utility/admin/UtilityCsv.php
@@ -1,4 +1,5 @@
 <?php
+AutoLoader::Import('library.rocid.pay.*');
 
 class UtilityCsv extends AdminCommand
 {
@@ -12,7 +13,7 @@ class UtilityCsv extends AdminCommand
     //return;
     ini_set("memory_limit", "512M");
 
-    $file = fopen('site12.csv', 'w');
+    $file = fopen('site12_25092012.csv', 'w');
     $eventId = 246;
 
     $model = EventUser::model()->with(array('User', 'User.Settings', 'User.Employments' => array('on' => 'Employments.Primary = 1'), 'User.Employments.Company', 'User.Emails'));
@@ -29,6 +30,10 @@ class UtilityCsv extends AdminCommand
 
     foreach($eventUsers as $eUser)
     {
+      if ($eUser->RoleId == 24)
+      {
+        continue;
+      }
       /** @var $user User */
       $user = $eUser->User;
 
@@ -45,11 +50,23 @@ class UtilityCsv extends AdminCommand
 
       $roleName = iconv('utf-8', 'Windows-1251', $eUser->EventRole->Name);
 
-      $email = iconv('utf-8', 'Windows-1251', $user->GetEmail() != null ? $user->GetEmail()->Email : $eUser->User->Email);
+      $items = OrderItem::GetByOwnerAndEventId($eUser->UserId, $eUser->EventId);
 
-      $sendMail = iconv('utf-8', 'Windows-1251', $user->Settings->ProjNews == 1 ? 'да' : 'нет');
+      $products = '';
+      foreach ($items as $item)
+      {
+        if ($item->Paid != 1)
+        {
+          continue;
+        }
+        $products .= iconv('utf-8', 'Windows-1251', $item->Product->Title) . ' ';
+      }
 
-      fputcsv($file, array(date('d.m.Y H:i:s', $eUser->CreationTime), $user->RocId, $name, $CompanyName, $Position, $email, $roleName, $sendMail), ';');
+      //$email = iconv('utf-8', 'Windows-1251', $user->GetEmail() != null ? $user->GetEmail()->Email : $eUser->User->Email);
+
+      //$sendMail = iconv('utf-8', 'Windows-1251', $user->Settings->ProjNews == 1 ? 'да' : 'нет');
+
+      fputcsv($file, array($user->RocId, $name, $CompanyName, $Position, $roleName, $products), ';');
 
     }
     fclose($file);
