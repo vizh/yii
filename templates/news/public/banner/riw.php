414a4d4bb825cfe969cd3768e1243661d1a05c8c
Nikitin Vitaly <alaris.nik@gmail.com>
2012-10-05 13:35:19 +0400

    Обновил спец. раздел riw12

--- templates/news/public/banner/riw.php
+++ templates/news/public/banner/riw.php
@@ -1,33 +1,3 @@
-<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="960" height="90" id="Banner_960x90" align="middle">
-  <param name="movie" value="/images/banners/banner_960x90.swf" />
-  <param name="quality" value="high" />
-  <param name="bgcolor" value="#ffffff" />
-  <param name="play" value="true" />
-  <param name="loop" value="false" />
-  <param name="wmode" value="window" />
-  <param name="scale" value="showall" />
-  <param name="menu" value="true" />
-  <param name="devicefont" value="false" />
-  <param name="salign" value="" />
-  <param name="allowScriptAccess" value="sameDomain" />
-  <!--[if !IE]>-->
-  <object type="application/x-shockwave-flash" data="/images/banners/banner_960x90.swf" width="960" height="90">
-    <param name="movie" value="/images/banners/banner_960x90.swf" />
-    <param name="quality" value="high" />
-    <param name="bgcolor" value="#ffffff" />
-    <param name="play" value="true" />
-    <param name="loop" value="false" />
-    <param name="wmode" value="window" />
-    <param name="scale" value="showall" />
-    <param name="menu" value="true" />
-    <param name="devicefont" value="false" />
-    <param name="salign" value="" />
-    <param name="allowScriptAccess" value="sameDomain" />
-    <!--<![endif]-->
-    <a href="http://www.adobe.com/go/getflash">
-      <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
-    </a>
-    <!--[if !IE]>-->
-  </object>
-  <!--<![endif]-->
-</object>
+<a target="_blank" title="Неделя Российского Интернета (Russian Internet Week, RIW–2012)" href="http://2012.russianinternetweek.ru/">
+  <img src="/images/banners/riw12_960x90.gif" alt="Неделя Российского Интернета (Russian Internet Week, RIW–2012)">
+</a>
