5d294554d87bd1593c108c36a9838150e4ee869e
Андрей <korotov@internetmediaholding.com>
2012-10-09 12:13:41 +0400

    Поднятие мероприятия в календаре в "Важное"

--- application/event/public/EventMain.php
+++ application/event/public/EventMain.php
@@ -5,7 +5,7 @@ class EventMain extends GeneralCommand
 {
   const MinEventCount = 6;
 
-  public static $ActualEvent = array('site12', 'riw12', 'cloudaward12');
+  public static $ActualEvent = array('riw12', 'cloudaward12', 'presentation-research2012');
 
   protected function preExecute()
   {    
