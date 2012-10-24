46c95b525eb35c2d0ef893753d25ab8185067e0e
Nikitin Vitaly <alaris.nik@gmail.com>
2012-10-24 17:09:03 +0400

    Выборка зарегистрированных на Hl12  через Ruvents

--- protected/modules/partner/controllers/InternalController.php
+++ protected/modules/partner/controllers/InternalController.php
@@ -5,7 +5,8 @@ class InternalController extends \partner\components\Controller
   public function actions()
   {
     return array(
-      'hl12badgefix' => '\partner\controllers\internal\Hl12badgefixAction'
+      'hl12badgefix' => '\partner\controllers\internal\Hl12badgefixAction',
+      'hl12badgeinfo' => '\partner\controllers\internal\Hl12badgeinfoAction'
     );
   }
 }
