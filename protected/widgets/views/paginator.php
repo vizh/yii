8a8685b7c4d9759a3403d67f2fec60ce811c5688
Nikitin Vitaly <alaris.nik@gmail.com>
2013-03-04 14:47:28 +0400

    Изменена логика работы Paginator

--- protected/widgets/views/paginator.php
+++ protected/widgets/views/paginator.php
@@ -1,15 +1,20 @@
+<?php
+/**
+ * @var $this \application\widgets\Paginator
+ */
+?>
 
 <ul class="pager">
-  <li <?php if ($this->page == 1):?>class="disabled"<?php endif;?>>
-    <a href="<?=$this->getUrl($this->page > 1 ? $this->page-1 : 1);?>">&larr;&nbsp;Предыдущая</a>
+  <li <?php if ($this->paginator->page == 1):?>class="disabled"<?php endif;?>>
+    <a href="<?=$this->paginator->getUrl($this->paginator->page > 1 ? $this->paginator->page-1 : 1);?>">&larr;&nbsp;Предыдущая</a>
   </li>
-  <li <?php if ($this->page == $this->count):?>class="disabled"<?php endif;?>>
-    <a href="<?=$this->getUrl($this->page < $this->count ? $this->page+1 : $this->count);?>">Следующая&nbsp;&rarr;</a>
+  <li <?php if ($this->paginator->page == $this->paginator->getCountPages()):?>class="disabled"<?php endif;?>>
+    <a href="<?=$this->paginator->getUrl($this->paginator->page < $this->paginator->getCountPages() ? $this->paginator->page+1 : $this->paginator->getCountPages());?>">Следующая&nbsp;&rarr;</a>
   </li>
 </ul>
 <div class="pagination pagination-centered">
   <ul>
-    <?foreach ($pages as $page):?>
+    <?foreach ($this->paginator->getPages() as $page):?>
       <li <?php if ($page->current): ?>class="active"<?php endif;?>><a href="<?=$page->url;?>"><?=$page->value;?></a></li>
     <?endforeach;?>
   </ul>
