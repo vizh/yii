d6fc61b46e4654a746c020113ed54dc0ca0a9b7b
Nikitin Vitaly <alaris.nik@gmail.com>
2013-03-14 19:21:03 +0400

    Название таблицы в модуле Pay

--- protected/modules/pay/models/CouponActivation.php
+++ protected/modules/pay/models/CouponActivation.php
@@ -26,7 +26,7 @@ class CouponActivation extends \CActiveRecord
 
   public function tableName()
   {
-    return 'PayCouponActivated';
+    return 'PayCouponActivation';
   }
 
   public function primaryKey()
