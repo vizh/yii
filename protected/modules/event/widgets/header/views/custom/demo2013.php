5ad5184581969340c4093951c4762a4c07ed4d63
Maxim Borzov <max.borzov@gmail.com>
2013-03-25 21:08:52 +0400

    Closes #585: Исправление в шапке DEMO-2013

--- protected/modules/event/widgets/header/views/custom/demo2013.php
+++ protected/modules/event/widgets/header/views/custom/demo2013.php
@@ -5,43 +5,16 @@
 $event = $this->event;
 ?>
 
-<div class="b-event-promo">
-  <div class="container">
+<div class="b-event-promo" style="background: #1074ac;">
+  <div class="container" style="background-image: none;  height: 80px;">
     <div class="row">
       <div class="side left span4">
-        <img src="<?=$event->getLogo()->getNormal();?>" alt="" class="logo">
-      </div>
-
-      <div class="details span4 offset4">
-        <h2 class="title"><?=$event->Title;?></h2>
-        <div class="type">
-          <img src="/images/blank.gif" alt="" class="i-event_small <?=$event->Type->CssClass;?>"><?=$event->Type->Title;?>
-        </div>
-        <div class="duration">
-          <span class="datetime">
-            <span class="date">
-              <?$this->widget('\event\widgets\Date', array('event' => $event));?>
-            </span>
-          </span>
-        </div>
-        <?if ($event->getContactAddress() != null && !empty($event->getContactAddress()->Place)):?>
-          <div class="location"><?=$event->getContactAddress()->Place;?></div>
-        <?endif;?>
+        <img src="/img/event/demo2013/logo.png" alt="DEMO Europe 2013" class="logo">
       </div>
 
       <?if ($this->eventPage):?>
-        <div class="side right span4">
-          <div class="actions">
-            <div class="calendar img-circle">
-              <a href="<?='webcal://'.RUNETID_HOST.\Yii::app()->getController()->createUrl('/event/share/ical', array('idName' => $event->IdName));?>" class="pseudo-link">
-                <i class="icon-calendar"></i><br><?=\Yii::t('app', 'Добавить в&nbsp;календарь');?>
-              </a>
-            </div>
-            <nav class="social">
-              <script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
-              <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="none" data-yashareQuickServices="vkontakte,facebook,twitter"></div>
-            </nav>
-          </div>
+        <div class="side right span8" style="text-align: right;">
+          <img src="/img/event/demo2013/menu.png" class="logo">
         </div>
       <?endif;?>
     </div>
