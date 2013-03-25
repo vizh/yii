34a71a29b72c4a79177e0bd0fecd2ec3f7141b61
Eroshenko Vitaliy <eroshenkov@mail.ru>
2013-03-25 15:31:52 +0400

    Update mailer

--- protected/modules/mail/controllers/DefaultController.php
+++ protected/modules/mail/controllers/DefaultController.php
@@ -5,23 +5,31 @@ class DefaultController extends \application\components\controllers\AdminMainCon
   {
     set_time_limit(84600);
     error_reporting(E_ALL & ~E_DEPRECATED);
+
+    $template = 'failoverconf13';
+
     exit();
     
     $isHTML = false;
-    $logPath =  \Yii::getPathOfAlias('application').DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;    
-    $fp = fopen($logPath.'rif203.log',"a+");
+    $logPath =  \Yii::getPathOfAlias('application').DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;
+    $fp = fopen($logPath.$template.'.log',"a+");
     $j = 0;
     
     $criteria = new \CDbCriteria();
     
-    $criteria->with = array('User');
-    $criteria->condition = '"User"."RunetId" = 321';
-    $criteria->limit = 1;
-    
+    $criteria->with = array('User.Settings');
+//    $criteria->condition = '"User"."RunetId" = 12953';
+
     $criteria->addInCondition('"t"."RoleId"', array(1,24));
+    $criteria->addInCondition('"t"."EventId"', array(195,246));
+
+    $criteria->addCondition('"Settings"."UnsubscribeAll" = false');
+
     $criteria->limit = 100;
+    $criteria->order = '"User"."RunetId"';
     $criteria->offset = $step * $criteria->limit;
-    $participants = \event\models\Participant::model()->byEventId(422)->findAll($criteria);
+//    $participants = \event\models\Participant::model()->byEventId(422)->findAll($criteria);
+    $participants = \event\models\Participant::model()->findAll($criteria);
     if (!empty($participants))
     {
       $userIdList = array();
@@ -33,11 +41,16 @@ class DefaultController extends \application\components\controllers\AdminMainCon
       $criteria->addInCondition('"t"."Id"', $userIdList);
       $criteria->with = array('LinkEmail.Email');
       $users = \user\models\User::model()->findAll($criteria);
+
+//      print \user\models\User::model()->count($criteria);
+//      exit();
+
       foreach ($users as $user)
       {
         // ПИСЬМО
-        $body = $this->renderPartial('send', array('user' => $user, 'regLink' => $this->getRegLink($user)), true);
+        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user)), true);
         $mail = new \ext\mailer\PHPMailer(false);
+        $mail->Mailer = 'mail';
         $mail->ParamOdq = true;
         $mail->ContentType = ($isHTML) ? 'text/html' : 'text/plain';
         $mail->IsHTML($isHTML);
@@ -52,18 +65,26 @@ class DefaultController extends \application\components\controllers\AdminMainCon
         }
         
         if ($j == 300) { sleep(1); $j = 0; }; $j++;
-        fwrite($fp, $user->RunetId.'-'.$email."\n");
-   
+
         if (!filter_var($email, FILTER_VALIDATE_EMAIL))
         {
           continue;
         }
+
+        if (preg_match("/@ashmanov.com/i", $email))
+        {
+          print 'is @Ashmanov<br />';
+          continue;
+        }
+
         $mail->AddAddress($email);
-        $mail->SetFrom('users@rif.ru', 'РИФ+КИБ 2013', false);
+        $mail->SetFrom('calendar@runet-id.com', 'RUNET-ID:// Календарь', false);
         $mail->CharSet = 'utf-8';
-        $mail->Subject = '=?UTF-8?B?'. base64_encode('#rif2013 прибывает по расписанию!') .'?=';
+        $mail->Subject = '=?UTF-8?B?'. base64_encode('FailOver Conference. Как защитить сайт от «падений»') .'?=';
         $mail->Body = $body;
-        $mail->Send();
+//        $mail->Send();
+
+        fwrite($fp, $user->RunetId.'-'.$email."\n");
       }
       fwrite($fp, "\n\n\n" . sizeof($users) . "\n\n\n");
       fclose($fp);
