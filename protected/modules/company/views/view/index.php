1e432b0c4d7fabce9cb1e269c1bd759318b6e17b
Maxim Borzov <max.borzov@gmail.com>
2013-03-26 12:09:25 +0400

    IssueID #462: Исправлены ссылки на профили сотрудников

--- protected/modules/company/views/view/index.php
+++ protected/modules/company/views/view/index.php
@@ -101,7 +101,7 @@
       <div class="row">
         <?foreach ($employmentsEx as $employment):?>
         <div class="employee_ex unit span2">
-          <a href="<?=$this->createUrl('/user/view/index', array('RunetId' => $employment->User->RunetId));?>">
+          <a href="<?=$this->createUrl('/user/view/index', array('runetId' => $employment->User->RunetId));?>">
             <?=\CHtml::image($employment->User->Photo->get90px(), $employment->User->getFullName(), array('width' => 58, 'height' => 58, 'class' => 'photo'));?>
             <p class="name"><?=$employment->User->getFullName();?></p>
           </a>
