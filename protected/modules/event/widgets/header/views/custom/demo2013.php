6338db37051611a899dcab091d99d1e4d3e34ae6
Maxim Borzov <max.borzov@gmail.com>
2013-03-28 18:06:55 +0400

    Closes #585: Сдвинул лого DEMO-2013 влево

--- protected/modules/event/widgets/header/views/custom/demo2013.php
+++ protected/modules/event/widgets/header/views/custom/demo2013.php
@@ -7,9 +7,13 @@ $event = $this->event;
 
 <div class="b-event-promo" style="background: #1074ac;">
   <div class="container" style="background-image: none;  height: 80px;">
-    <img src="/img/event/demo2013/logo.png" alt="DEMO Europe 2013" class="logo">
+    <div class="row">
+      <div class="side left span4">
+        <img src="/img/event/demo2013/logo.png" alt="" class="logo">
+      </div>
+    </div>
     <?if ($this->eventPage):?>
-      <span class="all">
+    <span class="all">
       <a href="<?=Yii::app()->createUrl('/event/list/index');?>">Все мероприятия</a>
     </span>
     <?endif;?>
