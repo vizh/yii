6db9088988601ed5282c6709c563166801c5966a
Maxim Borzov <max.borzov@gmail.com>
2013-04-03 14:23:43 +0400

    Исправлен FROM-адрес в письмах для зарегистрированных пользователей

--- protected/modules/user/models/User.php
+++ protected/modules/user/models/User.php
@@ -336,7 +336,7 @@ class User extends \application\models\translation\ActiveRecord
   {
     $mail = new \ext\mailer\PHPMailer(false);
     $mail->AddAddress($this->Email);
-    $mail->SetFrom('register@'.RUNETID_HOST, \Yii::t('app', 'RUNET-ID: Регистрация'), false);
+    $mail->SetFrom('reg@'.RUNETID_HOST, \Yii::t('app', 'RUNET-ID: Регистрация'), false);
     $mail->CharSet = 'utf-8';
     $mail->Subject = '=?UTF-8?B?'. base64_encode(\Yii::t('app', 'RUNET-ID: Регистрация')) .'?=';
     $mail->IsHTML(true);
