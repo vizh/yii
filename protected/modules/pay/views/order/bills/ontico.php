1f6cda89271d2762ffe66d60bbabe4cf253cfc4f
Nikitin Vitaly <alaris.nik@gmail.com>
2013-04-10 18:32:12 +0400

    Closes #624: РИТ++

---
+++ protected/modules/pay/views/order/bills/ontico.php
@@ -0,0 +1,134 @@
+<?php
+use application\components\utility\Texts;
+
+/**
+ * @var $order \pay\models\Order
+ * @var $billData array
+ * @var $total int
+ * @var $nds int
+ * @var $withSign bool
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<HTML lang=ru xml:lang="ru" xmlns="http://www.w3.org/1999/xhtml">
+<HEAD>
+  <TITLE>Счёт № <?=$order->Id;?></TITLE>
+  <META content="text/html; charset=UTF-8" http-equiv=Content-Type>
+</HEAD>
+<BODY>
+<DIV id=panel></DIV>
+<DIV id=content>
+  <TABLE cellSpacing="0" cellPadding="5" width="720" border="0">
+    <TBODY>
+    <TR>
+        <TD><b>ООО «Онтико»</b><BR>Адрес: Российская Федерация, 119180, г.Москва, Бродников пер., д.7, стр.1<BR>тел. +7 (495) 646-07-68 доб. 3001</TD>
+      <td align="right"><img src="/img/pay/bill/ontico/logo.png" width="150" height="61"></td>
+
+    </tr>
+    <TR>
+      <TD colspan="2">
+        <P style="TEXT-ALIGN: center"><B>Образец заполнения платежного поручения</B></P>
+        <TABLE style="BORDER-BOTTOM: 1px solid; BORDER-LEFT: 1px solid; WIDTH: 100%; BORDER-TOP: 1px solid; BORDER-RIGHT: 1px solid" cellSpacing="0" cellPadding="3">
+          <TBODY>
+          <TR>
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">ИНН 7706681097</TD>
+
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">КПП 770601001</TD>
+            <TD style="BORDER-BOTTOM: 1px solid">&nbsp;</TD>
+            <TD style="BORDER-BOTTOM: 1px solid">&nbsp;</TD>
+          </TR>
+          <TR>
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid"	colSpan=2>Получатель<BR>ООО «Онтико»</TD>
+            <TD	style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid"><BR>Сч.&nbsp;№</TD>
+
+            <TD style="BORDER-BOTTOM: 1px solid"><BR>40702810500000032617</TD></TR>
+          <TR>
+            <TD style="BORDER-RIGHT: 1px solid" colSpan=2>Банк получателя<BR>ВТБ 24 (ЗАО), г. МОСКВА,</TD>
+            <TD style="BORDER-RIGHT: 1px solid">БИК<BR>Сч.&nbsp;№</TD>
+            <TD>044525716,<BR>30101810100000000716</TD>
+
+          </TR>
+          </TBODY>
+        </TABLE>
+      </TD>
+    </TR>
+    <TR>
+      <TD style="TEXT-ALIGN: center" colspan="2">
+        <DIV style="MARGIN-TOP: 20px; FONT-SIZE: 24px"><B>СЧЕТ № <?=$order->Id;?> от <?=date('d.m.Y', strtotime($order->CreationTime));?></B></DIV>
+
+        (Счет действителен в течение 5-и банковских дней)
+      </TD>
+    </TR>
+    <TR>
+      <TD colspan="2">Заказчик: <?=$order->OrderJuridical->Name;?>,
+        ИНН / КПП: <?=$order->OrderJuridical->INN;?>/<?=$order->OrderJuridical->KPP;?><BR>
+        Плательщик: <?=$order->OrderJuridical->Name;?><BR>
+        Адрес: <?=$order->OrderJuridical->Address;?>		</TD>
+    </TR>
+
+    <TR>
+      <TD colspan="2">
+        <TABLE style="BORDER-BOTTOM: 1px solid; BORDER-LEFT: 1px solid; WIDTH: 100%; BORDER-TOP: 1px solid; BORDER-RIGHT: 1px solid" cellSpacing=0 cellPadding=3>
+          <TBODY>
+          <TR style="TEXT-ALIGN: center">
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">№</TD>
+            <TD	style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">Наименование<BR>товара (услуги)</TD>
+
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">Единица<BR>измерения</TD>
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">Кол-во</TD>
+            <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid">Цена,<br />руб.</TD>
+            <TD style="BORDER-BOTTOM: 1px solid">Сумма,<BR>руб.</TD>
+          </TR>
+
+          <?
+          $i = 1;
+          foreach ($billData as $data):
+            ?>
+            <TR>
+              <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid"><?=$i;?></TD>
+              <TD style="BORDER-BOTTOM: 1px solid; BORDER-RIGHT: 1px solid"><?=$data['Title'];?></TD>
+              <TD style="BORDER-BOTTOM: 1px solid; TEXT-ALIGN: center; BORDER-RIGHT: 1px solid"><?=$data['Unit'];?></TD>
+              <TD style="BORDER-BOTTOM: 1px solid; TEXT-ALIGN: center; BORDER-RIGHT: 1px solid"><?=$data['Count'];?></TD>
+              <TD style="BORDER-BOTTOM: 1px solid; TEXT-ALIGN: center; BORDER-RIGHT: 1px solid" nowrap="nowrap"><?=number_format(round($data['DiscountPrice'] / 1.18, 2, PHP_ROUND_HALF_UP), 2, ',', ' ');?></TD>
+
+              <TD style="BORDER-BOTTOM: 1px solid; TEXT-ALIGN: right" nowrap="nowrap"><?=number_format(round($data['DiscountPrice'] * $data['Count'] / 1.18, 2, PHP_ROUND_HALF_UP), 2, ',', ' ');?></TD>
+            </TR>
+            <?
+            $i++;
+          endforeach;?>
+
+          <TR>
+            <TD style="TEXT-ALIGN: right; FONT-WEIGHT: bold; BORDER-RIGHT: 1px solid" colSpan="4">Итого:<BR>Итого НДС:<BR>Всего к оплате (c учетом НДС):</TD>
+            <TD style="TEXT-ALIGN: right; FONT-WEIGHT: bold" colspan="2"><?=number_format($total - $nds, 2, ',', ' ');?><BR><?=number_format($nds, 2, ',', ' ');?><BR><?=number_format($total, 2, ',', ' ');?></TD>
+
+          </TR>
+          </TBODY>
+        </TABLE>
+      </TD>
+    </TR>
+    <TR>
+      <TD colspan="2">
+        Всего на сумму <?=number_format($total, 0, ',', ' ');?> руб. 00 коп.
+        <DIV><span><?=Texts::mb_ucfirst(mb_strtolower(Texts::NumberToText($total, true)));?></span> рублей 00 копеек</DIV><BR><BR>
+
+      </TD>
+    </TR>
+    <?if ($withSign):?>
+      <TR>
+        <TD colspan="2">
+          <IMG style="BORDER-BOTTOM: medium none; BORDER-LEFT: medium none; BORDER-TOP: medium none; BORDER-RIGHT: medium none; width: 600px;"
+               src="/img/pay/bill/ontico/sign.jpg"
+              />
+        </TD>
+      </TR>
+    <?else:?>
+      <tr>
+        <td colspan="2" style="border-top: 4px solid #000000;">
+          <IMG style="BORDER-BOTTOM: medium none; BORDER-LEFT: medium none; BORDER-TOP: medium none; BORDER-RIGHT: medium none; width: 600px;"
+               src="/img/pay/bill/ontico/nosign.jpg"
+              />
+        </td>
+      </tr>
+    <?endif;?>
+    </TBODY>
+  </TABLE></DIV></BODY></HTML>
