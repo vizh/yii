6689f96cd764a6d8b74494e4f0c1b2b5ae11949d
Nikitin Vitaly <alaris.nik@gmail.com>
2013-04-17 01:32:01 +0400

    Демо оферта

--- protected/messages/en/app.php
+++ protected/messages/en/app.php
@@ -118,6 +118,11 @@ return array(
   'Превышено максимальное количество активаций промо кода' => 'You have reached maximum number of  activation attempts for this coupon',
   'Для промо кода со скидкой 100% не указан товар, на который распространяется скидка' => 'The product for 100% discount is not specified',
   'Данный товар не может быть приобретен этим пользователем. Возможно уже куплен этот или аналогичный товар' => 'This product is not available for selected user',
-  'У пользователя уже активирован промо код с бОльшей скидкой' => 'User already activated coupon for bigger discount'
+  'У пользователя уже активирован промо код с бОльшей скидкой' => 'User already activated coupon for bigger discount',
+
+
+
+  'offer-demo.pdf' => 'offer-demo-en.pdf'
+
 
 );
