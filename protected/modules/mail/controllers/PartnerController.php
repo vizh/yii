880cc08b3fabf76cff4b97d78ad84238c266ed2a
Korotov Andrey <korotov@internetmediaholding.com>
2013-04-16 16:15:00 +0400

    Рассылка РИТ++ 2013

--- protected/modules/mail/controllers/PartnerController.php
+++ protected/modules/mail/controllers/PartnerController.php
@@ -8,7 +8,7 @@ class PartnerController extends \mail\components\MailerController
    */
   protected function getTemplateName()
   {
-    return 'phdays1';
+    return 'RITpp13';
   }
 
   /**
@@ -22,32 +22,43 @@ class PartnerController extends \mail\components\MailerController
   public function actionSend($step = 0)
   {
     return;
+    $test = false;
     $step = \Yii::app()->request->getParam('step', 0);
     set_time_limit(84600);
     error_reporting(E_ALL & ~E_DEPRECATED);
 
-    $builder = new \mail\components\Builder();
-    $builder->addEvent(246);
-    $builder->addEvent(195);
-    $builder->addEvent(120);
-    //$builder->addEvent(411);
-    //$builder->addEvent(246);
-    
-    $criteria = $builder->getCriteria();
+    if (!$test)
+    {
+      $builder = new \mail\components\Builder();
+      $builder->addEvent(246);
+      $builder->addEvent(195);
+      $builder->addEvent(120);
+      $criteria = $builder->getCriteria();
+      $criteria->addCondition('"t"."Email" LIKE :Email', 'OR');
+      $criteria->params['Email'] = '%@ontico.ru';
+    }
+    else
+    {
+      $criteria = new \CDbCriteria();
+      $criteria->addInCondition('"t"."RunetId"', array(321, 454, 122262));
+    }
     $criteria->limit  = $this->getStepCount();
     $criteria->offset = $this->getStepCount() * $step;
 
     $count = \user\models\User::model()->byVisible(true)->count($criteria);
     echo 'Получателей:'. $count.'<br/>';
-
+    
     $users = \user\models\User::model()->byVisible(true)->findAll($criteria);
     $mailer = new \mail\components\Mailer();
     foreach ($users as $user)
     {
-      $mail = new \mail\components\mail\PhDays13();
+      $mail = new \mail\components\mail\RITpp13();
       $mail->user = $user;
       $mailer->send($mail, $user->Email);
-      $this->addLogMessage($user->RunetId.' '.$user->Email);
+      if (!$test)
+      {
+        $this->addLogMessage($user->RunetId.' '.$user->Email);
+      }
     }
     if (!empty($users))
     {
