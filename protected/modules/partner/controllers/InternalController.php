12a0a48590e95ce292383923833255506ca1d00b
Nikitin Vitaly <alaris.nik@gmail.com>
2013-04-23 18:00:13 +0400

    Разрешение конфликтов

--- protected/modules/partner/controllers/InternalController.php
+++ protected/modules/partner/controllers/InternalController.php
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class InternalController extends \partner\components\Controller
-{
-  public function actions()
-  {
-    return array(
-      'hl12badgefix' => '\partner\controllers\internal\Hl12badgefixAction',
-      'hl12badgeinfo' => '\partner\controllers\internal\Hl12badgeinfoAction',
-      'tc12import' => '\partner\controllers\internal\Tc12importAction',
-      'tc12option' => '\partner\controllers\internal\Tc12optionAction',
-      'safor13import' => '\partner\controllers\internal\Safor13importAction',
-      'eaapa2013import' => '\partner\controllers\internal\Eaapa2013importAction',
-      'icomf13addproduct' => '\partner\controllers\internal\Icomf13addproductAction',
-      'techmailru13import' => '\partner\controllers\internal\Techmailru13importAction',
-      'snce13import' => '\partner\controllers\internal\import\Snce13Action',
-      'ritconf13import' => '\partner\controllers\internal\import\Ritconf13Action',
-      'rifvrn13import' => '\partner\controllers\internal\Rifvrn13importAction'
-    );
-  }
-}
-=======
 <?php
 
 class InternalController extends \partner\components\Controller
@@ -39,7 +16,7 @@ class InternalController extends \partner\components\Controller
       'snce13import' => '\partner\controllers\internal\import\Snce13Action',
       'ritconf13import' => '\partner\controllers\internal\import\Ritconf13Action',
       'telekom13import' => '\partner\controllers\internal\import\Telekom13Action',
+      'rifvrn13import' => '\partner\controllers\internal\Rifvrn13importAction',
     );
   }
 }
->>>>>>> Closes #649: Выгрузка сделана
