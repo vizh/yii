4fe80fe517e3ef26049a49e77c5f41cc54fef916
Korotov Andrey <korotov@internetmediaholding.com>
2013-05-17 17:03:06 +0400

    Closes #693: Фикс верстки

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -17,6 +17,9 @@ $.widget("ui.autocomplete", $.ui.autocomplete, {
   options : {
     create: function(event, ui) {
       $(this).autocomplete("widget").addClass("dropdown-menu");
+    },
+    open: function(){
+      $(this).autocomplete('widget').css('z-index', 1000);
     }
   }
 });
