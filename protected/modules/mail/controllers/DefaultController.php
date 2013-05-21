b553f74b348b66d4bccd3173c51b0e427c4b4720
Eroshenko Vitaliy <eroshenkov@mail.ru>
2013-05-21 12:01:01 +0400

    Mailer update

--- protected/modules/mail/controllers/DefaultController.php
+++ protected/modules/mail/controllers/DefaultController.php
@@ -6,7 +6,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
     set_time_limit(84600);
     error_reporting(E_ALL & ~E_DEPRECATED);
 
-    $template = 'spic13-6';
+    $template = 'phdays13-1';
     $isHTML = false;
 
     $logPath = \Yii::getPathOfAlias('application').DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;
@@ -54,8 +54,13 @@ class DefaultController extends \application\components\controllers\AdminMainCon
       'Participants.Role' => array('together' => true),
       'Settings' => array('select' => false)
     );
-    $criteria->addInCondition('"Participants"."EventId"', array(423));
-    $criteria->addInCondition('"Participants"."RoleId"', array(24));
+    $criteria->addInCondition('"Participants"."EventId"', array(497));
+    $criteria->addInCondition('"Participants"."RoleId"', array(1));
+
+    $criteria->addCondition('"t"."CreationTime" > :CreationTime');
+    $criteria->params['CreationTime'] = '2013-05-21 00:00:00';
+    $criteria->addCondition('"t"."Email" NOT LIKE :Email');
+    $criteria->params['Email'] = '%nomail497+%';
 
     $criteria->distinct = true;
     $criteria->addCondition('NOT "Settings"."UnsubscribeAll"');
@@ -81,8 +86,8 @@ class DefaultController extends \application\components\controllers\AdminMainCon
         */
 
         // ПИСЬМО
-//        $body = $this->renderPartial($template, array('user' => $user), true);
-        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user), 'waybillLink' => $this->getWaybillLink($user)), true);
+        $body = $this->renderPartial($template, array('user' => $user), true);
+//        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user), 'waybillLink' => $this->getWaybillLink($user)), true);
 //        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user), 'promo' => $arPromo), true);
 //        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user), 'role' => $user->Participants[0]->Role->Title), true);
 
@@ -110,14 +115,14 @@ class DefaultController extends \application\components\controllers\AdminMainCon
         */
 
         $mail->AddAddress($email);
-        $mail->SetFrom('users@sp-ic.ru', 'СПИК-2013', false);
-//        $mail->SetFrom('users@runet-id.com', '—RUNET—ID—', false);
+//        $mail->SetFrom('users@sp-ic.ru', 'СПИК-2013', false);
+        $mail->SetFrom('users@runet-id.com', '—RUNET—ID—', false);
         $mail->CharSet = 'utf-8';
-        $mail->Subject = '=?UTF-8?B?'. base64_encode('Видеопросмотр СПИК 2013!') .'?=';
+        $mail->Subject = '=?UTF-8?B?'. base64_encode('Вы зарегистрированы на форум PHDays III') .'?=';
         $mail->Body = $body;
 
-//        $mail->AddAttachment($_SERVER['DOCUMENT_ROOT'] . '/files/ext/2013-05-17/PHDays_rus.doc');
-//        $mail->AddAttachment($_SERVER['DOCUMENT_ROOT'] . '/files/ext/2013-05-17/PHDays_eng.doc');
+        $mail->AddAttachment($_SERVER['DOCUMENT_ROOT'] . '/files/ext/2013-05-17/PHDays_rus.doc');
+        $mail->AddAttachment($_SERVER['DOCUMENT_ROOT'] . '/files/ext/2013-05-17/PHDays_eng.doc');
 
 //        $mail->Send();
 
@@ -125,7 +130,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
       }
       fwrite($fp, "\n\n\n" . sizeof($users) . "\n\n\n");
       fclose($fp);
-      echo '<html><head><meta http-equiv="REFRESH" content="0; url='.$this->createUrl('/mail/default/send', array('step' => $step+1)).'"></head><body></body></html>';
+//      echo '<html><head><meta http-equiv="REFRESH" content="0; url='.$this->createUrl('/mail/default/send', array('step' => $step+1)).'"></head><body></body></html>';
     }
     else
     {
