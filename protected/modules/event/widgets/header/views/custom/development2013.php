819c6feba9b3167f910e34e0530a33bce1e9dde2
korotov <korotov@internetmediaholding.com>
2013-05-28 22:56:06 +0400

    Closes #743

---
+++ protected/modules/event/widgets/header/views/custom/development2013.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @var $this \event\widgets\Header
+ */
+$event = $this->event;
+?>
+
+<div class="b-event-promo" style="background: #ffffff;">
+  <div class="container" style="background-image: none;  height: 300px; padding-top: 20px; padding-bottom: 25px;">
+    <div class="row">
+      <div class="span12">
+        <img src="/img/event/development13/banner_940_300.jpg" alt="<?=CHtml::encode($event->Title);?>" >
+      </div>
+    </div>
+    <?if ($this->eventPage):?>
+      <span class="all">
+      <a href="<?=Yii::app()->createUrl('/event/list/index');?>"><?=Yii::t('app', 'Все мероприятия');?></a>
+    </span>
+    <?endif;?>
+  </div>
+</div>
