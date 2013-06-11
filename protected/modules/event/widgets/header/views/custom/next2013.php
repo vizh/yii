18b1543ad10695d8d17dac16fef66cd230d29087
Maxim Borzov <max.borzov@gmail.com>
2013-06-11 17:07:23 +0400

    Closes #799: Обновлена шапка мероприятия "Поколение Next"

---
+++ protected/modules/event/widgets/header/views/custom/next2013.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @var $this \event\widgets\Header
+ */
+$event = $this->event;
+?>
+
+<div class="b-event-promo" style="background-image: url('/img/event/next2013/header.png'); background-position: center;">
+    <div class="container">
+        <div class="row">
+            <div class="side left span4">
+                <div class="logo img-circle">
+                    <img src="<?=$event->getLogo()->get120px();?>" alt="">
+                </div>
+            </div>
+            <div class="details span4 offset4">
+                <h2 class="title" style="font-size: 25px; white-space: nowrap;"><span><?=CHtml::encode($event->Title);?></span></h2>
+                <div class="type">
+                    Конференция
+                </div>
+                <div class="duration">
+                  <span class="datetime">
+                    <span class="date">
+                      <?$this->widget('\event\widgets\Date', array('event' => $event));?>
+                    </span>
+                  </span>
+                </div>
+                <?if ($event->getContactAddress() != null && !empty($event->getContactAddress()->Place)):?>
+                    <div class="location"><?=$event->getContactAddress()->Place;?></div>
+                <?endif;?>
+            </div>
+        </div>
+        <?if ($this->eventPage):?>
+            <span class="all">
+      <a href="<?=Yii::app()->createUrl('/event/list/index');?>"><?=Yii::t('app', 'Все мероприятия');?></a>
+    </span>
+        <?endif;?>
+    </div>
+</div>
