56a61c0575bb3270401db6361ca8e29a1152f0de
Maxim Borzov <max.borzov@gmail.com>
2013-06-14 18:15:47 +0400

    Кастомная шапка  RIW-2013

---
+++ protected/modules/event/widgets/header/views/custom/riw13.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var $this \event\widgets\Header
+ */
+$event = $this->event;
+?>
+
+<div class="b-event-promo">
+    <div style="background: url('/img/event/riw13/head_bg.jpg') no-repeat; height: 260px; background-position: center;"></div>
+</div>
