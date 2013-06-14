f0fd70dbd78537edbd013f23377c4c33be93d329
Korotov Andrey <korotov@internetmediaholding.com>
2013-06-14 18:08:40 +0400

    Closes #767

--- protected/modules/company/views/edit/index.php
+++ protected/modules/company/views/edit/index.php
@@ -8,7 +8,6 @@
   </div>
 </h2>
 
-
 <script type="text/javascript">
   var phones = [];
   <?foreach ($form->Phones as $phone):?>
@@ -40,83 +39,81 @@
   <?endforeach;?>
 </script>
 
-<div class="company-edit">
-  <?=\CHtml::form('', 'POST', array('class' => 'form-horizontal', 'enctype' => 'multipart/form-data'));?>
-  <div class="container">
-    <div class="row">
-      <div class="span12">
-        <?=\CHtml::errorSummary($form, '<div class="alert alert-error">','</div>');?>
-        <?if (\Yii::app()->user->hasFlash('success')):?>
-          <div class="alert alert-success"><?=\Yii::app()->user->getFlash('success');?></div>
-        <?endif;?>
+<div class="container company-edit">
+  <div class="row">
+    <div class="span3">
+      <p>Указаная вами информация будет отображаться в публичном профиле компании. Пожалуйста, убедитесь в ее актуальности и корректности.</p>
+      <p>В случае, если будет выявления попытка публикации недостоверной или заведомо ложной информации, администрация RUNET-ID оставляет за собой право лишить вас доступа к управлению профилем компании или блокировки аккаунта.</p>
+    </div>
+    <div class="span9">
+      <?=\CHtml::errorSummary($form,'<div class="alert alert-error">', '</div>');?>
+      <?if (\Yii::app()->getUser()->hasFlash('success')):?>
+        <div class="alert alert-success"><?=\Yii::app()->getUser()->getFlash('success');?></div>
+      <?endif;?>
         
-        <div class="control-group">
-          <?=\CHtml::activeLabel($form, 'Logo', array('class' => 'control-label'));?>
-          <div class="controls">
-            <?=\CHtml::activeFileField($form, 'Logo');?>
-            <img src="<?=$company->getLogo()->get58px();?>" title="" />
+      <?=\CHtml::form('', 'POST', array('class' => 'b-form', 'enctype' => 'multipart/form-data'));?>
+        <div class="form-row">
+          <?=\CHtml::activeLabel($form, 'Logo');?>
+          <div class="row">
+            <div class="span3">
+              <?=\CHtml::activeFileField($form, 'Logo');?>
+            </div>
+            <div class="span3">
+              <?=\CHtml::image($company->getLogo()->get58px());?>
+            </div>
           </div>
         </div>
-        
-        <div class="control-group">
-          <?=\CHtml::activeLabel($form, 'Name', array('class' => 'control-label'));?>
-          <div class="controls">
-            <?=\CHtml::activeTextField($form, 'Name');?>
-          </div>
+      
+        <div class="form-row">
+          <?=\CHtml::activeLabel($form, 'Name');?>
+          <?=\CHtml::activeTextField($form, 'Name');?>
         </div>
-        <div class="control-group">
-          <?=\CHtml::activeLabel($form, 'FullName', array('class' => 'control-label'));?>
-          <div class="controls">
-            <?=\CHtml::activeTextField($form, 'FullName', array('class' => 'input-block-level'));?>
-          </div>
+      
+        <div class="form-row">
+          <?=\CHtml::activeLabel($form, 'FullName');?>
+          <?=\CHtml::activeTextField($form, 'FullName', array('class' => 'input-block-level'));?>
         </div>
-        
-        <div class="control-group">
-          <?=\CHtml::activeLabel($form, 'FullInfo', array('class' => 'control-label'));?>
-          <div class="controls">
-            <?=\CHtml::activeTextArea($form, 'FullInfo', array('class' => 'input-block-level'));?>
-          </div>
+      
+      
+        <div class="form-row m-bottom_10">
+          <?=\CHtml::activeLabel($form, 'FullInfo');?>
+          <?=\CHtml::activeTextArea($form, 'FullInfo', array('class' => 'input-block-level'));?>
         </div>
-        
-        <div class="control-group">
-          <?=\CHtml::activeLabel($form, 'Site', array('class' => 'control-label'));?>
-          <div class="controls">
-            <?=\CHtml::activeTextField($form, 'Site');?>
-          </div>
+      
+        <div class="form-row">
+          <?=\CHtml::activeLabel($form, 'Site');?>
+          <?=\CHtml::activeTextField($form, 'Site');?>
         </div>
-        
-        <div class="control-group">
-          <?=\CHtml::activeLabel($form, 'Address', array('class' => 'control-label'));?>
+      
+        <div class="form-row">
+          <?=\CHtml::activeLabel($form, 'Address');?>
           <?$this->widget('\contact\widgets\AddressControls', array('form' => $form->Address, 'place' => false));?>
         </div>
-        
-        <div class="control-group phone-items">
-          <?=\CHtml::activeLabel($form, 'Phones', array('class' => 'control-label'));?>
-          <div class="controls m-top_5 controls-add">
-            <a href="#" class="pseudo-link"><?=\Yii::t('app', 'Добавить телефон');?></a>
+      
+        <div class="form-row phone-items m-bottom_30">
+          <?=\CHtml::activeLabel($form, 'Phones');?>
+          <div class="controls-add">
+             <a href="#" class="pseudo-link"><?=\Yii::t('app', 'Добавить телефон');?></a>
           </div>
         </div>
-        
-        <div class="control-group email-items">
-          <?=\CHtml::activeLabel($form, 'Emails', array('class' => 'control-label'));?>
-          <div class="controls m-top_5 controls-add">
+      
+        <div class="form-row email-items">
+          <?=\CHtml::activeLabel($form, 'Emails');?>
+          <div class="controls-add">
             <a href="#" class="pseudo-link"><?=\Yii::t('app', 'Добавить эл.почту');?></a>
           </div>
         </div>
-        
-        <div class="control-group">
-          <div class="controls">
-            <?=\CHtml::submitButton(\Yii::t('app', 'Сохранить'), array('class' => 'btn btn-info'));?>
-          </div>
+      
+        <div class="form-footer">
+          <?=\CHtml::submitButton(\Yii::t('app', 'Сохранить'), array('class' => 'btn btn-info'));?>
         </div>
-      </div>
+      <?=\CHtml::endForm();?>
     </div>
   </div>
-  <?=\CHtml::endForm();?>
 </div>
 
 <script type="text/template" id="phone-item-tpl">
-  <div class="controls m-bottom_5">
+  <div class="controls">
     <span>+</span> <input type="text" name="<?=\CHtml::activeName($form, 'Phones[<%=i%>][CountryCode]');?>" class="input-mini" />
     <span>(</span> <input type="text" name="<?=\CHtml::activeName($form, 'Phones[<%=i%>][CityCode]');?>" class="input-small" /> <span>)</span>
     <input type="text" name="<?=\CHtml::activeName($form, 'Phones[<%=i%>][Phone]');?>" class="input-medium" />
@@ -126,7 +123,7 @@
 </script>
 
 <script type="text/template" id="phone-item-withdata-tpl">
-  <div class="controls <%if(Delete == 1){%>hide<%}%> m-bottom_5">
+  <div class="controls <%if(Delete == 1){%>hide<%}%>">
     <%if(typeof Errors != "undefined"){%>
       <div class="alert alert-error errorSummary"></div>
     <%}%>
@@ -142,7 +139,7 @@
 </script>
 
 <script type="text/template" id="email-item-tpl">
-  <div class="controls m-bottom_5">
+  <div class="controls">
     <input type="text" name="<?=\CHtml::activeName($form, 'Emails[<%=i%>][Email]');?>" placeholder="<?=\Yii::t('app','Адрес');?>"/>
     <input type="text" name="<?=\CHtml::activeName($form, 'Emails[<%=i%>][Title]');?>" class="input-medium" placeholder="<?=\Yii::t('app','Описание');?>"/>
     <a href="#" class="pseudo-link" data-action="remove"><?=\Yii::t('app', 'Удалить');?></a>
@@ -151,7 +148,7 @@
 </script>
 
 <script type="text/template" id="email-item-withdata-tpl">
-  <div class="controls <%if(Delete == 1){%>hide<%}%> m-bottom_5">
+  <div class="controls <%if(Delete == 1){%>hide<%}%>">
     <%if(typeof Errors != "undefined"){%>
       <div class="alert alert-error errorSummary"></div>
     <%}%>
