7bc553693b74f26908898b39531387c25eb0d405
Korotov Andrey <korotov@internetmediaholding.com>
2013-06-18 12:18:31 +0400

    Доступы админки в разделе пользователей

--- config/rules/user.php
+++ config/rules/user.php
@@ -18,6 +18,6 @@ return array(
     'allow',
     'roles' => array('admin'),
     'module' => 'user',
-    'controllers' => array('admin/moderator')
+    'controllers' => array('admin/moderator', 'admin/merge', 'admin/auth')
   ),
 );
