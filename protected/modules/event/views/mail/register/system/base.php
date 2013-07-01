998f48b1377da0b5b02d8c8e1a1a4d6854cb9a46
Korotov Andrey <korotov@internetmediaholding.com>
2013-07-01 14:02:37 +0400

    Изменение в шаблон системной нотификации о регистрации пользователя на мероприятии.

--- protected/modules/event/views/mail/register/system/base.php
+++ protected/modules/event/views/mail/register/system/base.php
@@ -7,7 +7,7 @@
 Данные пользователя:
 RUNET-ID пользователя: <?=$user->RunetId;?>
 
-Имя: <?=$user->getFullName();?>
+Имя: <?=$user->getFullName();?> <?if($user->getEmploymentPrimary() !== null):?>(<?=$user->getEmploymentPrimary()->Company->Name;?>)<?endif;?>
 
 E-Mail: <?=$user->Email;?>
 
