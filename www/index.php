53d012078c9baa095ab027eb461294edc3511642
Nikitin Vitaly <alaris.nik@gmail.com>
2013-07-11 13:28:30 +0400

    Фикс

--- www/index.php
+++ www/index.php
@@ -1,6 +1,4 @@
 <?php
-phpinfo();
-exit;
 
 // отключаем отладку
 $debug = $_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '82.142.129.35' || $_SERVER['REMOTE_ADDR'] == '178.140.224.229' || $_SERVER['REMOTE_ADDR'] == '10.10.4.1';
