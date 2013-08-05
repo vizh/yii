0161dd97226b84218918b594503640028c356768
Nikitin Vitaly <alaris.nik@gmail.com>
2013-08-05 12:54:40 +0400

    Изменен порядок товаров в api

--- protected/modules/api/controllers/pay/ProductAction.php
+++ protected/modules/api/controllers/pay/ProductAction.php
@@ -5,7 +5,9 @@ class ProductAction extends \api\components\Action
 {
   public function run()
   {
-    $products = \pay\models\Product::model()->byEventId($this->getEvent()->Id)->findAll();
+    $products = \pay\models\Product::model()->byEventId($this->getEvent()->Id)->findAll(
+      ['order' => '"t"."Priority" DESC, "t"."Id" ASC']
+    );
     $result = array();
     foreach ($products as $product)
     {
