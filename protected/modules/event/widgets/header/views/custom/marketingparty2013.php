72ab3f4d8c7c05fc3ee8fdce6d22c52c301f9d57
Андрей <korotov@internetmediaholding.com>
2013-09-25 16:39:22 +0400

    Шапка для MarketingParty2013

---
+++ protected/modules/event/widgets/header/views/custom/marketingparty2013.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var $this \event\widgets\Header
+ */
+$event = $this->event;
+?>
+
+<div class="b-event-promo" style="background: url('/img/event/marketingparty2013/header-bg.jpg') center center no-repeat #000; height: 300px;" >
+    <div style="background: url('/img/event/marketingparty2013/header.png') center center no-repeat; height: 300px;"></div>
+</div>
