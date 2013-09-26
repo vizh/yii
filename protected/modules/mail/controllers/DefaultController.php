0d665bda49b9bd933892f7807169b810e3d71f0c
Eroshenko Vitaliy <eroshenkov@mail.ru>
2013-09-26 13:23:00 +0400

    Mailer update

--- protected/modules/mail/controllers/DefaultController.php
+++ protected/modules/mail/controllers/DefaultController.php
@@ -6,8 +6,8 @@ class DefaultController extends \application\components\controllers\AdminMainCon
     set_time_limit(84600);
     error_reporting(E_ALL & ~E_DEPRECATED);
 
-    $template = 'download13-3';
-    $isHTML = false;
+    $template = 'riw13-html-3';
+    $isHTML = true;
 
     $logPath = \Yii::getPathOfAlias('application').DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;
     $fp = fopen($logPath.$template.'.log',"a+");
@@ -66,7 +66,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
       'Settings' => array('select' => false)
     );
 
-    $criteria->addInCondition('"Participants"."EventId"', array(673));
+//    $criteria->addInCondition('"Participants"."EventId"', array(673));
 //    $criteria->addInCondition('"Participants"."RoleId"', array(1));
 
 //    $criteria->addCondition('"Participants"."UserId" IN (SELECT "UserId" FROM "CompetenceResult" WHERE "TestId" = 1)');
@@ -144,7 +144,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
         $mail->SetFrom('users@runet-id.com', '—RUNET—ID—', false);
         $mail->CharSet = 'utf-8';
 //        $mail->Subject = '=?UTF-8?B?'. base64_encode('Конференция iFResh 2013: путевой лист') .'?=';
-        $mail->Subject = '=?UTF-8?B?'. base64_encode('ИТОГИ конференции Право на  DownLoad') .'?=';
+        $mail->Subject = '=?UTF-8?B?'. base64_encode('ИТОГИ конференции Право на DownLoad') .'?=';
         $mail->Body = $body;
 
 //        $mail->AddAttachment($_SERVER['DOCUMENT_ROOT'] . '/files/ext/2013-05-17/PHDays_eng.doc');
