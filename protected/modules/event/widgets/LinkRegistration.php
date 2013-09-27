10b4396c4a597835ae3c29525a6c7f472dd7181a
Nikitin Vitaly <alaris.nik@gmail.com>
2013-09-27 20:26:17 +0400

    Issue #1190: Заметная ссылка на регистрацию

---
+++ protected/modules/event/widgets/LinkRegistration.php
@@ -0,0 +1,39 @@
+<?php
+namespace event\widgets;
+
+/**
+ * Class LinkRegistration
+ * @package event\widgets
+ *
+ * @property string $LinkRegistration
+ */
+class LinkRegistration extends \event\components\Widget
+{
+  public function getAttributeNames()
+  {
+    return ['LinkRegistration'];
+  }
+
+
+  public function run()
+  {
+    $this->render('linkregistration');
+  }
+
+
+  /**
+   * @return string
+   */
+  public function getTitle()
+  {
+    return \Yii::t('app', 'Переход на страницу регистрации на сайт мероприятия');
+  }
+
+  /**
+   * @return string
+   */
+  public function getPosition()
+  {
+    return \event\components\WidgetPosition::Content;
+  }
+}
