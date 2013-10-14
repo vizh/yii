48117e99ae9b5306fa22a43bf808ed043a607f78
Eroshenko Vitaliy <eroshenkov@mail.ru>
2013-10-14 13:16:17 +0400

    Путевой лист RIW - обновление

--- protected/modules/event/views/ticket/riw13.php
+++ protected/modules/event/views/ticket/riw13.php
@@ -65,11 +65,11 @@
 
   .role {
     margin-top: 15px;
+    margin-bottom: 10px;
     display: block;
-    width: 120px;
+    /*width: 140px;*/
     text-align: center;
-    padding-top: 5px;
-    padding-bottom: 5px;
+    padding: 2px 0;
     background-color: #4f4f4f;
     color: #ffffff;
     text-transform: uppercase;
@@ -77,12 +77,23 @@
   .extra_pay {
     margin-top: 5px;
     display: block;
-    width: 120px;
+    /*width: 140px;*/
     text-align: center;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    background-color: #4f4f4f;
+    padding: 2px 0;
+    background-color: #4481A4;
     color: #ffffff;
+  }
+  a.program-btn {
+    background: transparent url("http://runet-id.com/images/mail/riw13/program_bg.png") no-repeat;
+    color: #FFFFFF;
+    display: block;
+    font-size: 12px;
+    height: 26px;
+    line-height: 24px;
+    margin-top: 12px;
+    text-align: center;
+    text-transform: uppercase;
+    width: 192px;
   }
 </style>
 
@@ -112,17 +123,17 @@
                     <table style="width: 100%" cellpadding="0" cellspacing="0">
                       <tr>
                         <td valign="top" style="width: 270px; padding-right: 20px;">
-                          <div style="font-size: 30px; font-weight: bold; line-height: 35px;"><?=$user->getFullName();?></div>
+                          <div style="color: #000; font-size: 30px; font-weight: bold; line-height: 35px;"><?=$user->getFullName();?></div>
                           <div style="font-size: 12px; margin-top: 5px;"><?=$user->getEmploymentPrimary();?></div>
                         </td>
-                        <td valign="top" style="padding-right: 20px;">
+                        <td valign="top" style="padding-right: 20px; width: 205px; font-size: 12px;">
                           <div class="role"><?=$role->Title;?></div>
 
                           <?if (count($orderItems) > 0):?>
-                            <div class="extra_pay"><p>БИЗНЕС-ЛАНЧ*</p></div>
+                            <div class="extra_pay">БИЗНЕС-ЛАНЧ*</div>
                             <?foreach($orderItems as $item):?>
                               <?if ($day = array_search($item->ProductId, array(17 => 1387,18 => 1391,19 => 1392))):?>
-                                <div class="extra_pay" style="float: left; margin-right: 5px; width: auto; padding: 5px;"><?=$day?></div>
+                                <div class="extra_pay" style="float: left; margin-right: 5px; width: auto; padding: 2px 5px;"><?=$day?></div>
                               <?endif;?>
                             <?endforeach;?>
                             <div style="clear: both;"></div>
@@ -143,16 +154,12 @@
                   <td style="padding-top: 30px;">
                     <table style="width: 100%;" cellpadding="0" cellspacing="0">
                       <tr>
-                        <td width="50%" style="background-color: #ececec; padding: 5px;">
-                          <strong>Время работы регистрации</strong><br/>
-                          <span style="font-size: 80%;">
-                            17 октября 09:00-17:00<br/>
-                            18 октября 10:00-17:00<br/>
-                            19 октября 10:00-17:00
-                          </span>
+                        <td width="65%" style="background-color: #ececec; padding: 5px;">
+                          <strong>Время работы стойки регистрации</strong><br/>
+                          <span style="font-size: 80%;">17 октября 09:00-17:00 / 18 октября 10:00-17:00 / 19 октября 10:00-17:00</span>
                         </td>
-                        <td width="50%" align="right">
-                          <a href="http://2013.russianinternetweek.ru/program/" target="_blank" style="display: block; background-image: url('http://runet-id.com/images/mail/download13/program-link_bg.png'); background-repeat: no-repeat; color: #ffffff; text-transform: uppercase; padding: 6px 0; width: 237px; height: 19px; text-align: center;">ПРОГРАММА КОНФЕРЕНЦИИ</a>
+                        <td align="right">
+                          <a href="http://2013.russianinternetweek.ru/program/" target="_blank" class="program-btn">ПРОГРАММА КОНФЕРЕНЦИИ</a>
                         </td>
                       </tr>
                     </table>
@@ -160,7 +167,7 @@
                 </tr>
                 <tr>
                   <td style="text-align: center; padding-top: 20px;">
-                    <div style="background: #EA1E1E; color: #FFFFFF; font-weight: bold; padding: 5px 10px;">Для регистрации необходимо предъявить данный путевой лист, либо документ, удостоверяющий личность</div>
+                    <img src="http://runet-id.com/images/mail/riw13/warning.png" />
                   </td>
                 </tr>
                 <tr>
@@ -175,16 +182,21 @@
                 </tr>
                 <tr>
                   <td style="padding-top: 10px;">
-                    <p><b>Центральный выставочный комплекс «Экспоцентр».</b></p>
+                    <p><b>Центральный выставочный комплекс «Экспоцентр», пав.№3</b></p>
                     <p>Москва, м. Выставочная, Краснопресненская наб., 14</p>
-                    <p>Телефон оргкомитета:<br/>+7 (985) 876-82-41<br/>+7 (985) 876-89-56</p>
+                    <p>Телефон оргкомитета: +7 (985) 876-82-41 / +7 (985) 876-89-56</p>
                   </td>
                 </tr>
                 <tr>
                   <td style="padding-top: 10px; text-align: center;"><img style="border: 1px solid;" src="http://runet-id.com/images/mail/riw13/riw13-map.jpg" border="0"/></td>
                 </tr>
                 <tr>
-                  <td style="text-align: center; padding: 30px;">
+                  <td style="text-align: center; padding: 20px 0px 0; font-size: 11px;">
+                    <p>Подробная информация: <a href="http://riw13.com" target="_blank">www.riw13.com</a> | Ваш Личный кабинет: <a href="http://riw13.com/my/" target="_blank">www.riw13.com/my/</a> | Служба поддержки: <a href="mailto:users@riw13.com">users@riw13.com</a></p>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="text-align: center; padding: 20px;">
                     <img src="http://runet-id.com/images/mail/spic13/ticket/runet-id_logo.gif" />
                   </td>
                 </tr>
