d13c834254ff281d861218b64a015c39112deb18
Eroshenko Vitaliy <eroshenkov@mail.ru>
2013-10-16 18:56:54 +0400

    Mailer update

--- protected/modules/mail/controllers/DefaultController.php
+++ protected/modules/mail/controllers/DefaultController.php
@@ -6,7 +6,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
     set_time_limit(84600);
     error_reporting(E_ALL & ~E_DEPRECATED);
 
-    $template = 'riw13-18';
+    $template = 'marketing13-1';
     $isHTML = false;
 
     $logPath = \Yii::getPathOfAlias('application').DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR;
@@ -34,7 +34,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
 
     // Чтение из файла
 /*
-    $arUsers = file(Yii::getPathOfAlias('webroot') . '/files/ext/2013-10-15/cloudaward13.csv');
+    $arUsers = file(Yii::getPathOfAlias('webroot') . '/files/ext/2013-10-16/alley.csv');
     foreach($arUsers as $eml) $emails[$eml] = trim($eml);
 //    $emails['v.eroshenko@gmail.com'] = 'v.eroshenko@gmail.com';
 //    $emails['borzov@internetmediaholding.com'] = 'borzov@internetmediaholding.com';
@@ -60,15 +60,13 @@ class DefaultController extends \application\components\controllers\AdminMainCon
     */
 
     // Обычная выборка пользователей [по мероприятиям]
-
     $criteria->with = array(
       'Participants' => array('together' => true),
-//      'Participants.Role' => array('together' => true),
+      'Participants.Role',
       'Settings' => array('select' => false)
     );
 
-
-//    $criteria->addInCondition('"Participants"."EventId"', array(425));
+    $criteria->addInCondition('"Participants"."EventId"', array(763));
 //    $criteria->addInCondition('"Participants"."RoleId"', array(1));
 
     /*
@@ -83,7 +81,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
     $criteria->addCondition('NOT "Settings"."UnsubscribeAll"');
     $criteria->addCondition('"t"."Visible"');
 
-    $criteria->addCondition('"Participants"."UserId" NOT IN (SELECT "UserId" FROM "EventParticipant" WHERE "EventId" IN (425))');
+//    $criteria->addCondition('"Participants"."UserId" NOT IN (SELECT "UserId" FROM "EventParticipant" WHERE "EventId" IN (425))');
 
 /*
     $criteria->addCondition('"Participants"."UserId" IN (SELECT "UserId" FROM "UserEmployment" WHERE
@@ -101,18 +99,18 @@ class DefaultController extends \application\components\controllers\AdminMainCon
     ');
 */
 
-    $criteria->addInCondition('"t"."RunetId"', array(12953));
+//    $criteria->addInCondition('"t"."RunetId"', array(12953, 12959, 112087));
 
     echo \user\models\User::model()->count($criteria);
     exit();
 
-    $criteria->limit = 500;
+    $criteria->limit = 400;
     $criteria->order = '"t"."RunetId" ASC';
     $criteria->offset = $step * $criteria->limit;
     $users = \user\models\User::model()->findAll($criteria);
 
     /* Для PK PASS для Яблочников */
-//    $event = \event\models\Event::model()->findByPk(425);
+    $event = \event\models\Event::model()->findByPk(763);
 
     if (!empty($users))
     {
@@ -123,17 +121,15 @@ class DefaultController extends \application\components\controllers\AdminMainCon
 //        exit();
 
 //        /* PK PASS для Яблочников */
-//        $pkPass = new \application\components\utility\PKPassGenerator($event, $user, $user->Participants[0]->Role);
-
+        $pkPass = new \application\components\utility\PKPassGenerator($event, $user, $user->Participants[0]->Role);
 
 //        $arPromo = array();
 //        for($i = 0; $i < 2; $i++) $arPromo[] = $this->getPromo();
 
-
         // ПИСЬМО
-//        $body = $this->renderPartial($template, array('user' => $user), true);
+        $body = $this->renderPartial($template, array('user' => $user), true);
 //        $body = $this->renderPartial($template, array('user' => $user, 'arPromo' => $arPromo), true);
-        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user)), true);
+//        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user)), true);
 //        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user), 'promo' => $this->getPromo()), true);
 //        $body = $this->renderPartial($template, array('user' => $user, 'regLink' => $this->getRegLink($user), 'role' => $user->Participants[0]->Role->Title), true);
 
@@ -148,7 +144,7 @@ class DefaultController extends \application\components\controllers\AdminMainCon
 
         if ($j == 300) { sleep(1); $j = 0; }; $j++;
 
-        if ($j == 1) continue;
+//        if ($j == 1) continue;
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL))
         {
@@ -164,21 +160,21 @@ class DefaultController extends \application\components\controllers\AdminMainCon
         */
 
         $mail->AddAddress($email);
-//        $mail->SetFrom('info@therunet.com', 'Оргкомитет конкурса "Аллея Инноваций"', false);
+//        $mail->SetFrom('info@therunet.com', 'Редакция theRunet', false);
 //        $mail->SetFrom('users@russianinternetweek.ru', 'RIW 2013', false);
 //        $mail->SetFrom('users@russianinternetweek.ru', 'Премия Облака 2013', false);
 //        $mail->SetFrom('narod@premiaruneta.ru', 'Народное голосование Премии Рунета', false);
-        $mail->SetFrom('users@runet-id.com', 'RIW 2013', false);
+        $mail->SetFrom('users@runet-id.com', '—RUNET—ID—', false);
         $mail->CharSet = 'utf-8';
-        $mail->Subject = '=?UTF-8?B?'. base64_encode('РЕГИСТРИРУЙТЕСЬ на Неделю Российского Интернета (RIW) - старт 17 октября') .'?=';
+        $mail->Subject = '=?UTF-8?B?'. base64_encode('Ваш Путевой лист на закрытую вечеринку для маркетинг директоров') .'?=';
         $mail->Body = $body;
 
 //        $mail->AddAttachment($_SERVER['DOCUMENT_ROOT'] . '/files/ext/2013-10-02/marketingparty2013.pdf');
 
         /* PK PASS для Яблочников */
-//        $mail->AddAttachment($pkPass->runAndSave(), 'ticket.pkpass');
+        $mail->AddAttachment($pkPass->runAndSave(), 'ticket.pkpass');
 
-//        $mail->Send();
+        $mail->Send();
 
 //        fwrite($fp, $email . "\n");
         fwrite($fp, $user->RunetId . ' - '. $email . "\n");
@@ -222,10 +218,10 @@ class DefaultController extends \application\components\controllers\AdminMainCon
   {
     $coupon = new \pay\models\Coupon();
     $coupon->EventId = 425;
-    $coupon->Discount = '1';
+    $coupon->Discount = '0.25';
     $coupon->ProductId = 1309;
     $coupon->Code = $coupon->generateCode();
-    $coupon->Recipient = 'Выдано участнику Аллеи Инноваций';
+    $coupon->Recipient = 'Выдано участнику прошлых мероприятий RIW и РИФ';
     $coupon->save();
     return $coupon->Code;
   }
