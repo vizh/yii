36adac3dbf3f1b10c00ea701b4b20c02605f5c73
Nikitin Vitaly <alaris.nik@gmail.com>
2013-11-05 19:42:08 +0400

    Исправлен генератор путевых листов для многочастевых мероприятий

--- protected/modules/event/controllers/TicketController.php
+++ protected/modules/event/controllers/TicketController.php
@@ -2,32 +2,47 @@
 class TicketController extends application\components\controllers\PublicMainController
 {
   public $layout = '//layouts/ticket';
+
+  /** @var \event\models\Event */
+  protected $event = null;
   
   public function actionIndex($eventIdName, $runetId, $hash)
   {
-    $event = \event\models\Event::model()->byIdName($eventIdName)->find();
+    $this->event = \event\models\Event::model()->byIdName($eventIdName)->find();
     $user  = \user\models\User::model()->byRunetId($runetId)->find();
     
-    if ($event == null || $user == null)
-      throw new \CHttpException(404);
-    
-    $participant = \event\models\Participant::model()->byUserId($user->Id)->byEventId($event->Id)->find();
-    if ($participant == null || $participant->getHash() !== $hash)
+    if ($this->event == null || $user == null)
       throw new \CHttpException(404);
 
     $orderItems = \pay\models\OrderItem::model()
         ->byOwnerId($user->Id)->byChangedOwnerId(null)->byChangedOwnerId($user->Id, false)
-        ->byEventId($event->Id)->byPaid(true)->findAll();
-    
-    $path = \Yii::getPathOfAlias('event.views.ticket.'.$event->IdName).'.php';
-    $view = file_exists($path) ? $event->IdName : 'index';
-    $this->setPageTitle(\Yii::t('app', 'Ваш билет на мероприятие «{event}»', ['{event}' => $event->Title]));
+        ->byEventId($this->event->Id)->byPaid(true)->findAll();
+
+    $params = ['event' => $this->event, 'user' => $user, 'orderItems' => $orderItems];
+    $model = \event\models\Participant::model()->byUserId($user->Id)->byEventId($this->event->Id);
+    if (count($this->event->Parts) == 0)
+    {
+      $participant = $model->find();
+      if ($participant == null || $participant->getHash() !== $hash)
+        throw new \CHttpException(404);
+      $params['role'] = $participant->Role;
+    }
+    else
+    {
+      $participants = $model->findAll();
+      if (count($participants) == 0 || $participants[0]->getHash() !== $hash)
+        throw new \CHttpException(404);
+      $params['participants '] = $participants;
+    }
+    $this->renderTicket($params);
+  }
+
+  protected function renderTicket($params)
+  {
+    $path = \Yii::getPathOfAlias('event.views.ticket.'.$this->event->IdName).'.php';
+    $view = file_exists($path) ? $this->event->IdName : 'index';
+    $this->setPageTitle(\Yii::t('app', 'Ваш билет на мероприятие «{event}»', ['{event}' => $this->event->Title]));
     \Yii::app()->getClientScript()->reset();
-    $this->render($view, [
-      'event' => $event,
-      'role' => $participant->Role,
-      'user' => $user,
-      'orderItems' => $orderItems
-    ]);
+    $this->render($view, $params);
   }
 }
