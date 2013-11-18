34d43fb0f26afc92d71a34f37a34e1e5b4cc1d99
Андрей <korotov@internetmediaholding.com>
2013-11-18 16:12:55 +0400

    Регистрационные письма RBP2013

---
+++ protected/modules/event/views/mail/register/rbp13.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @var \user\models\User $user
+ * @var \event\models\Role $role
+ * 
+ */
+?>
+
+Здравствуйте, <?=$user->getShortName();?>!
+
+Благодарим Вас, за регистрацию и оплату участия в: RUSSIAN BUSINESS PARTY - Вечеринка в стиле Bond Party
+Статус: <?=$role->Title;?>
+
+
+Время и место проведения
+21 ноября 2013
+четверг
+начало в 20:00
+Бар-Ресторан Типография
+Россия, Москва
+3-я Ямского Поля улица 24
+
+Мы с социальных сетях:
+https://www.facebook.com/russianbusinessparty
+http://vk.com/russianbusinessparty
+https://www.youtube.com/user/russianbusinessparty
+
+До встречи на мероприятии!
+Контакты: maxim.kolesnichenko@gmail.com
+v+7 (926) 343-17-18
