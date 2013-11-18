1b206746f4a6be462d0b204ec1515491c7f089c4
Андрей <korotov@internetmediaholding.com>
2013-11-18 18:21:44 +0400

    Письма регистрации для  fpsb13

---
+++ protected/modules/event/components/handlers/register/Fpsb13.php
@@ -0,0 +1,15 @@
+<?php
+namespace event\components\handlers\register;
+
+class Rbp13 extends Base
+{ 
+  public function getSubject()
+  {
+    return 'Благодарим Вас, за регистрацию в RUSSIAN BUSINESS PARTY - Вечеринка в стиле Bond Party';
+  }
+  
+  public function getBody()
+  {
+    return $this->renderBody('event.views.mail.register.rbp13', ['user' => $this->user, 'role' => $this->role]);
+  }
+}
