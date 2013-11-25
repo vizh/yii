ebad20d47632683e3c527bf3903a8c91ea564de4
Korotov Andrey <andrey.korotov@yandex.ru>
2013-11-25 16:06:05 +0400

    Шапка devcon2014

---
+++ protected/modules/event/widgets/header/views/custom/devcon14.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var $this \event\widgets\Header
+ */
+$event = $this->event;
+?>
+<?if (\Yii::app()->getController()->getModule()->getId() == 'pay'):?>
+  <style type="text/css">
+    header#header {
+      display: none;
+    }
+  </style>
+<?endif;?>
+
+<div class="b-event-promo" style="background: #ffffff;">
+  <a href="http://msdevcon.cloudapp.net/register/" target="_blank"><img src="/img/event/devcon14/bg.png" border="0" /></a>
+</div>
