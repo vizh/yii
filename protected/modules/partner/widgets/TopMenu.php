e2ef7889b0a9e4ed3abd47fccb74fb62bf47b612
Nikitin Vitaly <alaris.nik@gmail.com>
2013-11-25 17:20:53 +0400

    Фикс бага с топ меню в партнерке

--- protected/modules/partner/widgets/TopMenu.php
+++ protected/modules/partner/widgets/TopMenu.php
@@ -6,58 +6,60 @@ class TopMenu extends \CWidget
 
   public function run()
   {
-    $menu = array();
-    $menu[] = array(
+    $menu = [];
+    $menu[] = [
       'Title' => 'Главная',
       'Url' => \Yii::app()->createUrl('/partner/main/index'),
       'Access' => $this->checkAccess('main', 'index'),
       'Active' => $this->isActive('main')
-    );
-    $menu[] = array(
+    ];
+    $menu[] = [
       'Title' => 'Регистрации',
       'Url' => \Yii::app()->createUrl('/partner/ruvents/index'),
       'Access' => $this->checkAccess('ruvents', 'index'),
       'Active' => $this->isActive('ruvents')
-    );
-    $menu[] = array(
+    ];
+    $menu[] = [
       'Title' => 'Участники',
       'Url' => \Yii::app()->createUrl('/partner/user/index'),
       'Access' => $this->checkAccess('user', 'index'),
       'Active' => $this->isActive('user')
-    );
-    $menu[] = array(
+    ];
+    $menu[] = [
       'Title' => 'Промо-коды',
       'Url' => \Yii::app()->createUrl('/partner/coupon/index'),
       'Access' => $this->checkAccess('coupon', 'index'),
       'Active' => $this->isActive('coupon')
-    );
-    $menu[] = array(
+    ];
+    $menu[] = [
       'Title' => 'Счета',
       'Url' => \Yii::app()->createUrl('/partner/order/index'),
       'Access' => $this->checkAccess('order', 'index'),
       'Active' => $this->isActive('order')
-    );
-    $menu[] = array(
+    ];
+    $menu[] = [
       'Title' => 'Заказы',
       'Url' => \Yii::app()->createUrl('/partner/orderitem/index'),
       'Access' => $this->checkAccess('orderitem', 'index'),
       'Active' => $this->isActive('orderitem')
-    );
-    $menu[] = array(
+    ];
+    $menu[] = [
       'Title' => 'Программа',
       'Url' => \Yii::app()->createUrl('/partner/program/index'),
       'Access' => $this->checkAccess('program', 'index'),
       'Active' => $this->isActive('program')
-    );
-    $menu[] = array(
-      'Title' => 'Компетенции',
-      'Url' => \Yii::app()->createUrl('/partner/competence/index'),
-      'Access' => \competence\models\Test::model()->byEventId(\Yii::app()->partner->getAccount()->EventId)->exists(),
-      'Active' => $this->isActive('competence')
-    );
-    
+    ];
+    if (!\Yii::app()->partner->isGuest)
+    {
+      $menu[] = [
+        'Title' => 'Компетенции',
+        'Url' => \Yii::app()->createUrl('/partner/competence/index'),
+        'Access' => \competence\models\Test::model()->byEventId(\Yii::app()->partner->getAccount()->EventId)->exists(),
+        'Active' => $this->isActive('competence')
+      ];
+    }
 
-    $this->render('topMenu', array('menu' => $menu));
+    $this->render('topMenu', ['menu' => $menu]);
   }
 
   protected function checkAccess($controller, $action)
