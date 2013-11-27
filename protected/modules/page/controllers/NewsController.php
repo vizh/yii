3b192e2930627c4ff8d41c0b7b2d862cae0dec0a
Maxim Borzov <max.borzov@gmail.com>
2013-11-27 14:46:27 +0400

    Closes #1520: Публикация новости на главной странице

---
+++ protected/modules/page/controllers/NewsController.php
@@ -0,0 +1,9 @@
+<?php
+class NewsController extends \application\components\controllers\PublicMainController
+{
+  public function actionPayonline_And_RunetId_2013()
+  {
+    $this->bodyId = 'about-page';
+    $this->render('2013-11-payonline-and-runetid');
+  }
+}
