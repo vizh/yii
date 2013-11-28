d13387a1384de45547ea303184c6e2d0e0b4fcc0
Maxim Borzov <max.borzov@gmail.com>
2013-11-28 22:56:15 +0400

    Правки в шапке itogi2013

--- protected/modules/event/widgets/header/views/custom/itogi2013.php
+++ protected/modules/event/widgets/header/views/custom/itogi2013.php
@@ -5,8 +5,8 @@
 $event = $this->event;
 ?>
 
-<div class="b-event-promo" style="height: 350px; background: url('/img/event/itogi2013/bg.jpg') no-repeat scroll center 0 #ffffff;">
-  <div class="container" style="background-image: none; height: 250px;">
+<div class="b-event-promo" style="height: 250px; background: url('/img/event/itogi2013/bg.jpg') no-repeat scroll center 0 #ffffff;">
+  <div class="container" style="background-image: none; height: 125px;">
     <?if ($this->eventPage):?>
       <span class="all">
       <a href="<?=Yii::app()->createUrl('/event/list/index');?>"><?=Yii::t('app', 'Все мероприятия');?></a>
