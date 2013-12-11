e2081aa1fafe0888df06c3ccdbd9bf279a51701c
Nikitin Vitaly <alaris.nik@gmail.com>
2013-12-11 09:41:15 +0400

    Удален модуль Convert

--- protected/modules/convert/ConvertModule.php
+++
@@ -1,5 +0,0 @@
-<?php
-class ConvertModule extends \CWebModule
-{
-  
-}
--- protected/modules/convert/views/default/index.php
+++
@@ -1,4 +0,0 @@
-<div class="container b-convert">
-  <h2>DB converter</h2>
-  <input type="button" value="Начать" />
-</div>
