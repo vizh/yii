319e158512f4362a08746193ed95d4c16ce7e4a3
Nikitin Vitaly <alaris.nik@gmail.com>
2013-12-27 16:12:20 +0400

    Виджет-карта для DO

---
+++ protected/modules/event/widgets/fullwidth/MapDigitalOctober.php
@@ -0,0 +1,38 @@
+<?php
+namespace event\widgets\fullwidth;
+
+class MapDigitalOctober extends \event\components\Widget
+{
+  public function init()
+  {
+    \Yii::app()->clientScript->registerScriptFile('http://maps.google.com/maps/api/js?sensor=true');
+    parent::init();
+  }
+
+
+  public function run()
+  {
+    $this->render('mapdigitaloctober', []);
+  }
+
+  public function getIsHasDefaultResources()
+  {
+    return true;
+  }
+
+  /**
+   * @return string
+   */
+  public function getTitle()
+  {
+    return \Yii::t('app', 'Карта Digital October');
+  }
+
+  /**
+   * @return string
+   */
+  public function getPosition()
+  {
+    return \event\components\WidgetPosition::Content;
+  }
+}
---
+++ protected/modules/event/widgets/fullwidth/views/mapdigitaloctober.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var $this \event\widgets\fullwidth\MapDigitalOctober
+ */
+?>
+<div class="map">
+  <nav class="t-center">
+    <a href="javascript: void(null);" class="map-road" id="kropotkinskaya">От метро «Кропоткинская»</a>
+    <a href="javascript: void(null);" class="map-road" id="polyanka">От метро «Полянка»</a>
+    <a href="javascript: void(null);" class="map-road" id="tretyakovskaya">От метро «Третьяковская»</a>
+  </nav>
+  <div id="map" style="height: 450px;margin-bottom: 100px;width: 100%; max-width: 100%;"></div>
+</div>
