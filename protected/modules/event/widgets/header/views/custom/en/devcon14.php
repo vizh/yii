a6ac2bfd914adcacccec64041882cf2d45e294ac
Nikitin Vitaly <alaris.nik@gmail.com>
2014-01-31 13:49:42 +0400

    Локализация платежной страницы devcon

---
+++ protected/modules/event/widgets/header/views/custom/en/devcon14.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var $this \event\widgets\Header
+ */
+$event = $this->event;
+?>
+<?if (\Yii::app()->getController()->getModule()->getId() == 'pay'):?>
+  <style type="text/css">
+    header#header {
+      display: none;
+    }
+  </style>
+<?endif;?>
+
+<div class="b-event-promo" style="background: #ffffff;">
+  <a href="http://www.msdevcon.ru/register/" target="_blank"><img src="/img/event/devcon14/bg-en.png" border="0" /></a>
+</div>
