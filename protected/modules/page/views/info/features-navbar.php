04a4645ddc70debe9ee8800d6ca0fb8e7e9ab37e
Nikitin Vitaly <alaris.nik@gmail.com>
2014-02-12 13:29:12 +0400

    Issue #1705: Промо-страница RUNET-ID

---
+++ protected/modules/page/views/info/features-navbar.php
@@ -0,0 +1,9 @@
+<div class="navbar-features">
+  <div class="container">
+    <ul>
+      <li><a href="">Шаг 1: Создание</a></li>
+      <li><a href="">Шаг 2: Продвижение</a></li>
+      <li><a href="">Шаг 3: Управление</a></li>
+    </ul>
+  </div>
+</div>
---
+++ protected/modules/page/views/info/features.php
@@ -0,0 +1,101 @@
+<div class="container info">
+  <h1>Успешное мероприятие в 3 шага</h1>
+
+  <p><nobr>RUNET-ID</nobr> является уникальным сервисом, позволяющим не&nbsp;только анонсировать событие, но&nbsp;также организовать <nobr>онлайн-регистрацию</nobr> участников мероприятия, приём оплат за&nbsp;участие и&nbsp;за&nbsp;дополнительные услуги, <nobr>оффлайн-регистрацию</nobr> участников на&nbsp;самом мероприятии.</p>
+
+  <p>Сегодня на&nbsp;портале <nobr>RUNET-ID</nobr> зарегистрированно около 200&nbsp;000 профессионалов IT&nbsp;и&nbsp;<nobr>медиа-отрасли</nobr>, активно принимающих участие в&nbsp;профильных мероприятиях, посещающих российские (московские и&nbsp;региональные) и&nbsp;западные конференции. И&nbsp;многие из&nbsp;зарегистрированных в&nbsp;<nobr>RUNET-ID</nobr> пользователей могут заинтересоваться участием в&nbsp;вашем проекте.</p>
+</div>
+
+<div class="container info">
+  <div class="step">
+    <span class="green"></span>
+    Шаг 1
+  </div>
+
+  <h2>Создайте мероприятие в календаре</h2>
+
+  <p class="text-center"><a href="<?=Yii::app()->createUrl('/event/create/index')?>">Заполните простую форму</a> для создание заявки на&nbsp;добавление мероприятия в&nbsp;<nobr>IT-календарь</nobr>. Наши<br>специалисты оперативно проверят ее&nbsp;и&nbsp;свяжутся с&nbsp;вами для уточнения деталей, если это будет<br>необходимо. При регистрации мероприятия вы&nbsp;можете сразу заказать и&nbsp;получить консультацию<br>по&nbsp;другим услугам нашего сервиса:</p>
+
+
+
+  <ul class="f-adv-thesises green">
+    <li class="active"><b><i class="create-1"></i></b>Онлайн регистрация</li>
+    <li><b><i class="create-2"></i></b>Информационная поддержкая</li>
+    <li><b><i class="create-3"></i></b>Видео трансляция</li>
+    <li><b><i class="create-4"></i></b>Офлайн регистрация</li>
+  </ul>
+
+</div>
+
+<script type="text/javascript">
+  $(function(){
+    $('#sliderCreate').bind('slid', function(){
+
+    });
+  });
+</script>
+
+<div class="f-slider f-create">
+  <div class="container">
+
+    <div id="sliderCreate" class="carousel slide">
+      <!-- Carousel items -->
+      <div class="carousel-inner">
+        <div class="active item">
+          <h3>Некоторый заголовок</h3>
+          <p>Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. Пачка контента. </p>
+        </div>
+        <div class="item">item 2</div>
+        <div class="item">item 3</div>
+      </div>
+      <!-- Carousel nav -->
+      <a class="carousel-control left" href="#sliderCreate" data-slide="prev">&lsaquo;</a>
+      <a class="carousel-control right" href="#sliderCreate" data-slide="next">&rsaquo;</a>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="container info">
+  <div class="step">
+    <span class="purple"></span>
+    Шаг 2
+  </div>
+
+  <h2>Продвигайте мероприятие</h2>
+
+  <p class="text-center">На этом этапе у вас уже есть страница мероприятия в календаре, позволяющая регистрировать<br>участников на мероприятия — расскажите о ней! Вам обязательно следует использовать социальные<br>сети и собственные каналы для привлечения участников, в дополнение к ним можете воспользоваться<br>услугами нашего сервиса:</p>
+
+
+  <ul class="f-adv-thesises purple">
+    <li class="active"><b><i class="promo-1"></i></b>Рассылка</li>
+    <li><b><i class="promo-2"></i></b>Приоритетное размещение</li>
+    <li><b><i class="promo-3"></i></b>Сайт мероприятия</li>
+    <li><b><i class="promo-4"></i></b>Социальные сети</li>
+  </ul>
+
+</div>
+
+
+
+<div class="container info">
+  <div class="step">
+    <span class="blue"></span>
+    Шаг 3
+  </div>
+
+  <h2>Управляйте мероприятием</h2>
+
+  <p class="text-center">RUNET-ID позволяет упростить и автоматизировать многие задачи для организатора мероприятий.<br>Наш подход позволяет добиться максимальной гибкости во всех вопросах, связанных с<br>управлением мероприятием. Организуя регистрацию через наш сервис, вы получаете<br>неограниченные возможности:</p>
+
+  <ul class="f-adv-thesises blue">
+    <li class="active"><b><i class="control-1"></i></b>Типы участия</li>
+    <li><b><i class="control-2"></i></b>Ценовая политика</li>
+    <li><b><i class="control-3"></i></b>Промо-коды</li>
+    <li><b><i class="control-4"></i></b>Статистика</li>
+    <li><b><i class="control-5"></i></b>Способы оплаты</li>
+    <li><b><i class="control-6"></i></b>Электронные билеты</li>
+  </ul>
+
+</div>
