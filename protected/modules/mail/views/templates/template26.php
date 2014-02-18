d05ac174b406fc4584c6368a13197c39148ff7da
Eroshenko Vitaliy <eroshenkov@mail.ru>
2014-02-18 16:57:02 +0400

    Рассылка text/plain CSF14 по всем кроме виртуалов

---
+++ protected/modules/mail/views/templates/template26.php
@@ -0,0 +1,18 @@
+<p><img alt="" src="http://runet-id.com/images/mail/mblt14/header.png" style="width: 630px; height: 164px;" /></p>
+
+<h2><?=$user->getShortName();?>, здравствуйте!</h2>
+
+<p>Приглашаем вас принять участие в ежегодной международной мобильной конференции <strong>#MBLT14</strong>, которая состоится 23 мая в Digital October!</p>
+
+<p>Конференция пройдет уже в третий раз и соберет более 40 мировых экспертов и 700 участников из США, Европы, Азии и России. Уже подтвердили свое участие представители <strong>Deutsche Telekom, Qualcomm, Siri, Samsung, Facebook, Opera, Aviasales, SoundCloud, Livemap, KwameCorp</strong> и многих других компаний.</p>
+
+<p>В этом году на #MBLT14 пристальное внимание будет уделено носимым гаджетам. Браслеты, очки, кольца, мотоциклетные шлемы с дополненной реальностью перестают быть инновациями будущего и становятся частью нашей повседневной жизни. Кроме этого, на конференции будут рассмотрены самые горячие и актуальные темы в секциях Travel, Business, Social и Payment.</p>
+
+<p><a href="<?=$user->getFastauthUrl('/event/mblt14/');?>" style="display: block; text-decoration: none; background: #FF7370; color: #FFFFFF; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 12px; text-align: center; width: 300px;">Регистрация уже открыта!</a></p>
+
+<p>Стоимость билета до 31 апреля &mdash; 8 000 рублей. С 1 мая &mdash; 11 000 рублей. При одновременной покупке двух и более билетов на участие &mdash; скидка 25%.</p>
+
+<p>Организаторы мероприятия: e-Legion и РАЭК<br />
+Генеральные партнеры: Aviasales, Digital October, Camerjam, Linko<br />
+Генеральный Отель-партнер: Mercure<br />
+Генеральный Такси-партнер: Uber</p>
