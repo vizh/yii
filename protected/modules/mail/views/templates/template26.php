ddc9df4c87e33ae1e30655a06c6788199a39e362
Eroshenko Vitaliy <eroshenkov@mail.ru>
2014-02-19 16:22:50 +0400

    Рассылка MBLT14

--- protected/modules/mail/views/templates/template26.php
+++ protected/modules/mail/views/templates/template26.php
@@ -4,15 +4,17 @@
 
 <p>Приглашаем вас принять участие в ежегодной международной мобильной конференции <strong>#MBLT14</strong>, которая состоится 23 мая в Digital October!</p>
 
+<p><a href="http://mblt.ru/ru" style="display: block; text-decoration: none; background: #3C3F41; color: #FFFFFF; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 12px; text-align: center; width: 250px;">Официальный сайт</a></p>
+
 <p>Конференция пройдет уже в третий раз и соберет более 40 мировых экспертов и 700 участников из США, Европы, Азии и России. Уже подтвердили свое участие представители <strong>Deutsche Telekom, Qualcomm, Siri, Samsung, Facebook, Opera, Aviasales, SoundCloud, Livemap, KwameCorp</strong> и многих других компаний.</p>
 
 <p>В этом году на #MBLT14 пристальное внимание будет уделено носимым гаджетам. Браслеты, очки, кольца, мотоциклетные шлемы с дополненной реальностью перестают быть инновациями будущего и становятся частью нашей повседневной жизни. Кроме этого, на конференции будут рассмотрены самые горячие и актуальные темы в секциях Travel, Business, Social и Payment.</p>
 
-<p><a href="<?=$user->getFastauthUrl('/event/mblt14/');?>" style="display: block; text-decoration: none; background: #FF7370; color: #FFFFFF; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 12px; text-align: center; width: 300px;">Регистрация уже открыта!</a></p>
+<p><a href="<?=$user->getFastauthUrl('/event/mblt14/');?>" style="display: block; text-decoration: none; background: #FF7370; color: #FFFFFF; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 12px; text-align: center; width: 250px;">Регистрация</a></p>
 
 <p>Стоимость билета до 31 апреля &mdash; 8 000 рублей. С 1 мая &mdash; 11 000 рублей. При одновременной покупке двух и более билетов на участие &mdash; скидка 25%.</p>
 
-<p>Организаторы мероприятия: e-Legion и РАЭК<br />
-Генеральные партнеры: Aviasales, Digital October, Camerjam, Linko<br />
-Генеральный Отель-партнер: Mercure<br />
-Генеральный Такси-партнер: Uber</p>
+<p>Организаторы мероприятия: <a href="http://e-legion.ru">e-Legion</a> и <a href="http://raec.ru">РАЭК</a><br />
+Генеральные партнеры: <a href="http://aviasales.ru">Aviasales</a>, <a href="http://digitaloctober.ru">Digital October</a>, <a href="http://camerjam.com">Camerjam</a>, <a href="http://linko.io">Linko</a><br />
+Генеральный Отель-партнер: <a href="http://mercure.com/ru/russia/index.shtml">Mercure</a><br />
+Генеральный Такси-партнер: <a href="http://uber.com/">Uber</a></p>
