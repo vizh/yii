791cafcee80b3bfcdb841366ce2fe3e8fc1279e1
Maxim Borzov <max.borzov@gmail.com>
2014-03-18 17:40:12 +0400

    Рассылка по всему RUNET-ID (анонс концерта 20-летия Рунета)

---
+++ protected/modules/mail/views/templates/template45.php
@@ -0,0 +1,15 @@
+<p><strong>7 апреля 2014 </strong>года Рунет празднует свой <nobr>20-летний</nobr> юбилей. В&nbsp;этот день в&nbsp;<strong>Arena Moscow</strong> состоится <strong>яркое шоу и&nbsp;концерт</strong>.</p>
+
+<p>Хэдлайнеры вечера: <strong>Ляпис Трубецкой</strong>, <strong>Браво</strong>, <strong>Сансара</strong>, <strong>Вася Обломов</strong> и&nbsp;другие.</p>
+
+<p><nobr>20-летие</nobr> Рунета&nbsp;&mdash; прекрасный повод отметить этот праздник в&nbsp;кругу друзей и&nbsp;хороших знакомых, в&nbsp;вечернем формате и&nbsp;под музыку любимых исполнителей.</p>
+
+<p>В&nbsp;программе вечера: работа <nobr>welcome-зоны</nobr>, подарки от&nbsp;партнеров, юбилейное шоу, концерт, новые знакомства и&nbsp;общение с&nbsp;друзьями, море позитива.</p>
+
+<p>Пока основные моменты мероприятия держатся в&nbsp;секрете, известно только, что выбор музыкантов не&nbsp;случаен. У&nbsp;каждого из&nbsp;них есть своя особая история, связанная с&nbsp;развитием их&nbsp;карьеры в&nbsp;интернете и&nbsp;благодаря интернету.</p>
+
+<p><a href="<?=$user->getFastauthUrl('http://runet-id.com/event/20therunet/');?>" style="display: block; text-decoration: none; background: #D85939; color: #FFFFFF; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 12px; text-align: center; width: 300px;">КУПИТЬ БИЛЕТ<span style="display: block; font-size: 60%;">(стоимость 2000 рублей)</span></a></p>
+
+<p style="text-align: center"><strong>ВНИМАНИЕ: Количество мест ограничено!</strong></p>
+
+<p>Билеты на концертное шоу также можно приобрести в&nbsp;кассе <a href="http://www.arenagroup.ru/timetable/443">Arena Moscow</a>, на&nbsp;сайте <a href="http://redkassa.ru/events/70337-bilety_na_koncert_20_let_online/">Redkassa</a>, на&nbsp;сайте <a href="http://www.nashe.ru/">Нашего Радио</a> и&nbsp;на&nbsp;<nobr><a href="<?=$user->getFastauthUrl('http://runet-id.com/event/20therunet/');?>">RUNET&mdash;ID</a></nobr>.</p>
