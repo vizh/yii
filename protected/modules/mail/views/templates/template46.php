4a4653adde4ced5422a4dbe8b13138aa5ce930fa
Maxim Borzov <max.borzov@gmail.com>
2014-03-31 15:55:25 +0400

    Всяские разные рассылки за последнее время

---
+++ protected/modules/mail/views/templates/template46.php
@@ -0,0 +1,17 @@
+<p><?=$user->getShortName();?>, приглашаем на&nbsp;конференцию <a href="http://events.techdays.ru/AppSummit/2014-03/">AppSummit</a>!</p>
+
+<p>Всем членам РАЭК доступен билет на&nbsp;конференцию бесплатно!</p>
+
+<p>Специальный промокод для бесплатного участия в&nbsp;конференции:</p>
+
+<p><span style="display: block; text-decoration: none; background: #EEEEEE; border: 2px dashed #ffffff; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 12px; text-align: center; width: 350px;">APPSUMMIT14_RAEC</span></p>
+
+<p>Мобильные приложения вошли в&nbsp;жизнь почти каждого из&nbsp;нас, изменяя традиции и&nbsp;делая жизнь проще и&nbsp;интереснее. Многим они уже принесли успех, <nobr>кому-то</nobr> приоткрыли дорогу к&nbsp;высокой эффективности бизнеса.</p>
+
+<p>25&nbsp;марта в&nbsp;Москве мы&nbsp;вместе с&nbsp;вами будем обсуждать мобильные платформы и&nbsp;рынок приложений для корпоративного и&nbsp;личного использования, для дела и&nbsp;развлечений. Место встречи: <a href="http://digitaloctober.ru/">Digital October</a>, Москва.</p>
+
+<p>В&nbsp;программе примут участие представители компаний Microsoft, Google, AppAnnie, S7 Group, <nobr>ТКС-Банк</nobr>, МТС, телеканал Дождь, Вымпелком,Mail. Ru. Group,Yandex,JoyBits,Game Insight и&nbsp;многие другие!</p>
+
+<p>Вечером специально для участников мероприятия состоится закрытый показ фильма &laquo;Стартап&raquo;!</p>
+
+<p><a href="http://events.techdays.ru/AppSummit/2014-03/" style="display: block; text-decoration: none; background: #D85939; color: #FFFFFF; font-family: Arial,Verdana,sans-serif; font-size: 20px; margin: 0 auto; padding: 15px 12px; text-align: center; width: 350px;">ПОДРОБНЕЕ О КОНФЕРЕНЦИИ</a></p>
