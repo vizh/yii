16e851e931f56abd6e1d204a51fd3d49416a4f59
Korotov Andrey <andrey.korotov@yandex.ru>
2014-08-01 18:29:51 +0400

    Closes #2158: Экосистемы Рунета

--- protected/modules/page/views/info/ecosystems.php
+++ protected/modules/page/views/info/ecosystems.php
@@ -12,23 +12,22 @@
         </div>
     </div>
 </h2>
-<div class="container">
-  <div class="row">
-      <div class="span1">
-
-      </div>
-      <div class="span11">
-          <div clas="row">
-              <?foreach ($ecosystems as $ecosystem):?>
-                  <div class="ecosystem span5">
-                      <h2><?=$ecosystem->En;?></h2>
-                      <p><?=$ecosystem->Title;?></p>
-                      <?if (!empty($ecosystem->Description)):?>
-                          <p class="muted"><?=$ecosystem->Description;?></p>
-                      <?endif;?>
-                  </div>
-              <?endforeach;?>
+<div class="ecosystem-page">
+    <div class="container">
+      <div class="row">
+          <div class="span11 offset1">
+              <div clas="row">
+                  <?foreach ($ecosystems as $ecosystem):?>
+                      <div class="ecosystem span5">
+                          <h2><?=$ecosystem->En;?></h2>
+                          <p><?=$ecosystem->Title;?></p>
+                          <?if (!empty($ecosystem->Description)):?>
+                              <p class="muted"><?=$ecosystem->Description;?></p>
+                          <?endif;?>
+                      </div>
+                  <?endforeach;?>
+              </div>
           </div>
       </div>
-  </div>
+    </div>
 </div>
