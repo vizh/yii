997c3223696a2c05b6d1b2a48abc906e37cbb41c
Korotov Andrey <andrey.korotov@yandex.ru>
2014-08-01 18:23:53 +0400

    Closes #2158: Экосистемы Рунета

--- protected/modules/page/views/info/ecosystems.php
+++ protected/modules/page/views/info/ecosystems.php
@@ -3,16 +3,32 @@
  * @var \application\models\ProfessionalInterest[] $ecosystems
  */
 ?>
+<h2 class="b-header_large light">
+    <div class="line"></div>
+    <div class="container">
+        <div class="title">
+            <span class="backing runet">RUNET</span>
+            <span class="backing text"><?=\Yii::t('app', 'Экосистемы Рунета');?></span>
+        </div>
+    </div>
+</h2>
 <div class="container">
   <div class="row">
-    <?foreach ($ecosystems as $ecosystem):?>
-      <div class="ecosystem span5">
-        <h2><?=$ecosystem->En;?></h2>
-        <p><?=$ecosystem->Title;?></p>
-        <?if (!empty($ecosystem->Description)):?>
-          <p class="muted"><?=$ecosystem->Description;?></p>
-        <?endif;?>
+      <div class="span1">
+
       </div>
-    <?endforeach;?>
+      <div class="span11">
+          <div clas="row">
+              <?foreach ($ecosystems as $ecosystem):?>
+                  <div class="ecosystem span5">
+                      <h2><?=$ecosystem->En;?></h2>
+                      <p><?=$ecosystem->Title;?></p>
+                      <?if (!empty($ecosystem->Description)):?>
+                          <p class="muted"><?=$ecosystem->Description;?></p>
+                      <?endif;?>
+                  </div>
+              <?endforeach;?>
+          </div>
+      </div>
   </div>
 </div>
