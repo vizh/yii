e695601b907814b3861d86f0979db1bc6a559068
Korotov Andrey <andrey.korotov@yandex.ru>
2014-09-18 13:33:34 +0400

    Товары типа Callback

--- protected/modules/pay/controllers/cabinet/IndexAction.php
+++ protected/modules/pay/controllers/cabinet/IndexAction.php
@@ -3,130 +3,140 @@ namespace pay\controllers\cabinet;
 
 class IndexAction extends \pay\components\Action
 {
-  public function run($eventIdName)
-  {
-    $request = \Yii::app()->getRequest();
-    $this->getController()->setPageTitle('Оплата  / ' .$this->getEvent()->Title . ' / RUNET-ID');
-
-    \partner\models\PartnerCallback::start($this->getEvent());
-    if ($this->getUser() != null)
+    public function run($eventIdName)
     {
-      \partner\models\PartnerCallback::registration($this->getEvent(), $this->getUser());
-    }
+        $request = \Yii::app()->getRequest();
+        $this->getController()->setPageTitle('Оплата  / ' .$this->getEvent()->Title . ' / RUNET-ID');
 
-    $finder = \pay\components\collection\Finder::create($this->getEvent()->Id, $this->getUser()->Id);
-    $unpaidItems = new \stdClass();
-    $unpaidItems->all = [];
-    $unpaidItems->tickets = [];
-    foreach ($finder->getUnpaidFreeCollection() as $item)
-    {
-      $key = $item->getOrderItem()->Product->ManagerName == 'Ticket' ? 'tickets' : 'all';
-      if (!isset($unpaidItems->{$key}[$item->getOrderItem()->ProductId]))
-      {
-        $unpaidItems->{$key}[$item->getOrderItem()->ProductId] = [];
-      }
-      $unpaidItems->{$key}[$item->getOrderItem()->ProductId][] = $item;
-    }
-
-    $formAdditionalAttributes = $this->getAddtionalAttributesForm($finder);
-    if (!$formAdditionalAttributes->getIsEmpty())
-    {
-      $formAdditionalAttributes->attributes = $request->getParam(get_class($formAdditionalAttributes));
-      if ($request->getIsPostRequest() && $formAdditionalAttributes->validate())
-      {
-        $this->processAddtionalAttributesForm($formAdditionalAttributes);
-      }
-    }
-
-
-
-    $allPaidCollections = array_merge($finder->getPaidOrderCollections(), $finder->getPaidFreeCollections());
-
-    $hasRecentPaidItems = false;
-    foreach ($allPaidCollections as $collection)
-    {
-      foreach ($collection as $item)
-      {
-        /** @var $item \pay\components\OrderItemCollectable */
-        if ($item->getOrderItem()->PaidTime > date('Y-m-d H:i:s', time() - 10*60*60))
+        \partner\models\PartnerCallback::start($this->getEvent());
+        if ($this->getUser() != null)
         {
-          $hasRecentPaidItems = true;
-          break;
-        }
-      }
-      if ($hasRecentPaidItems)
-        break;
-    }
-
-
-
-    $this->getController()->render('index', array(
-      'finder' => $finder,
-      'unpaidItems' => $unpaidItems,
-      'hasRecentPaidItems' => $hasRecentPaidItems,
-      'account' => $this->getAccount(),
-      'formAdditionalAttributes' => $formAdditionalAttributes
-    ));
-  }
-
-  /**
-   * @param \pay\components\collection\Finder $finder
-   * @return \pay\models\forms\AddtionalAttributes
-   */
-  private function getAddtionalAttributesForm(\pay\components\collection\Finder $finder)
-  {
-    $title = null;
-    $attributes = [];
-    $values = [];
-    foreach ($finder->getUnpaidFreeCollection() as $item)
-    {
-      $product = $item->getOrderItem()->Product;
-      foreach ($product->getAdditionalAttributes() as $attr)
-      {
-        $attributes[$attr->Name] = $attr;
-        $value = \pay\models\EventUserAdditionalAttribute::model()->byEventId($this->getEvent()->Id)->byUserId($this->getUser()->Id)->byName($attr->Name)->find();
-        if ($value !== null)
-        {
-          $values[$value->Name] = $value->Value;
+            \partner\models\PartnerCallback::registration($this->getEvent(), $this->getUser());
         }
 
-        if (!empty($product->AdditionalAttributesTitle))
+        $finder = \pay\components\collection\Finder::create($this->getEvent()->Id, $this->getUser()->Id);
+        $unpaidItems = new \stdClass();
+        $unpaidItems->all = [];
+        $unpaidItems->tickets = [];
+        $unpaidItems->callbacks = [];
+
+        foreach ($finder->getUnpaidFreeCollection() as $item)
         {
-          $title = $product->AdditionalAttributesTitle;
+            switch ($item->getOrderItem()->Product->ManagerName) {
+                case 'Ticket': $key = 'tickets';
+                    break;
+                case 'Callback': $key = 'callbacks';
+                    break;
+                default: $key = 'all';
+            }
+
+            if (!isset($unpaidItems->{$key}[$item->getOrderItem()->ProductId]))
+            {
+                $unpaidItems->{$key}[$item->getOrderItem()->ProductId] = [];
+            }
+            $unpaidItems->{$key}[$item->getOrderItem()->ProductId][] = $item;
         }
-      }
+
+        $formAdditionalAttributes = $this->getAddtionalAttributesForm($finder);
+        if (!$formAdditionalAttributes->getIsEmpty())
+        {
+            $formAdditionalAttributes->attributes = $request->getParam(get_class($formAdditionalAttributes));
+            if ($request->getIsPostRequest() && $formAdditionalAttributes->validate())
+            {
+                $this->processAddtionalAttributesForm($formAdditionalAttributes);
+            }
+        }
+
+
+
+        $allPaidCollections = array_merge($finder->getPaidOrderCollections(), $finder->getPaidFreeCollections());
+
+        $hasRecentPaidItems = false;
+        foreach ($allPaidCollections as $collection)
+        {
+            foreach ($collection as $item)
+            {
+                /** @var $item \pay\components\OrderItemCollectable */
+                if ($item->getOrderItem()->PaidTime > date('Y-m-d H:i:s', time() - 10*60*60))
+                {
+                    $hasRecentPaidItems = true;
+                    break;
+                }
+            }
+            if ($hasRecentPaidItems)
+                break;
+        }
+
+
+
+        $this->getController()->render('index', array(
+            'finder' => $finder,
+            'unpaidItems' => $unpaidItems,
+            'hasRecentPaidItems' => $hasRecentPaidItems,
+            'account' => $this->getAccount(),
+            'formAdditionalAttributes' => $formAdditionalAttributes
+        ));
     }
 
-    usort($attributes, function ($a, $b) {
-      if ($a->Order == $b->Order) {
-        return 0;
-      }
-      return ($a->Order < $b->Order) ? -1 : 1;
-    });
-
-    $form = new \pay\models\forms\AddtionalAttributes($attributes, $values);
-    $form->FormTitle = $title;
-    return $form;
-  }
-
-  /**
-   * @param \pay\models\forms\AddtionalAttributes $form
-   */
-  private function processAddtionalAttributesForm(\pay\models\forms\AddtionalAttributes $form)
-  {
-    foreach ($form->attributeNames() as $name)
+    /**
+     * @param \pay\components\collection\Finder $finder
+     * @return \pay\models\forms\AddtionalAttributes
+     */
+    private function getAddtionalAttributesForm(\pay\components\collection\Finder $finder)
     {
-      $attribute = \pay\models\EventUserAdditionalAttribute::model()->byEventId($this->getEvent()->Id)->byUserId($this->getUser()->Id)->byName($name)->find();
-      if ($attribute == null)
-      {
-        $attribute = new \pay\models\EventUserAdditionalAttribute();
-        $attribute->EventId = $this->getEvent()->Id;
-        $attribute->UserId = $this->getUser()->Id;
-        $attribute->Name = $name;
-      }
-      $attribute->Value = $form->$name;
-      $attribute->save();
+        $title = null;
+        $attributes = [];
+        $values = [];
+        foreach ($finder->getUnpaidFreeCollection() as $item)
+        {
+            $product = $item->getOrderItem()->Product;
+            foreach ($product->getAdditionalAttributes() as $attr)
+            {
+                $attributes[$attr->Name] = $attr;
+                $value = \pay\models\EventUserAdditionalAttribute::model()->byEventId($this->getEvent()->Id)->byUserId($this->getUser()->Id)->byName($attr->Name)->find();
+                if ($value !== null)
+                {
+                    $values[$value->Name] = $value->Value;
+                }
+
+                if (!empty($product->AdditionalAttributesTitle))
+                {
+                    $title = $product->AdditionalAttributesTitle;
+                }
+            }
+        }
+
+        usort($attributes, function ($a, $b) {
+            if ($a->Order == $b->Order) {
+                return 0;
+            }
+            return ($a->Order < $b->Order) ? -1 : 1;
+        });
+
+        $form = new \pay\models\forms\AddtionalAttributes($attributes, $values);
+        $form->FormTitle = $title;
+        return $form;
     }
-    $this->getController()->redirect($form->SuccessUrl);
-  }
+
+    /**
+     * @param \pay\models\forms\AddtionalAttributes $form
+     */
+    private function processAddtionalAttributesForm(\pay\models\forms\AddtionalAttributes $form)
+    {
+        foreach ($form->attributeNames() as $name)
+        {
+            $attribute = \pay\models\EventUserAdditionalAttribute::model()->byEventId($this->getEvent()->Id)->byUserId($this->getUser()->Id)->byName($name)->find();
+            if ($attribute == null)
+            {
+                $attribute = new \pay\models\EventUserAdditionalAttribute();
+                $attribute->EventId = $this->getEvent()->Id;
+                $attribute->UserId = $this->getUser()->Id;
+                $attribute->Name = $name;
+            }
+            $attribute->Value = $form->$name;
+            $attribute->save();
+        }
+        $this->getController()->redirect($form->SuccessUrl);
+    }
+
 }
--- protected/modules/pay/views/cabinet/index/unpaidItems.php
+++ protected/modules/pay/views/cabinet/index/unpaidItems.php
@@ -10,141 +10,171 @@
 $total = 0;
 ?>
 
-<?if (sizeof($unpaidItems->all) > 0 || sizeof($unpaidItems->tickets) > 0):?>
+<?if (sizeof($unpaidItems->all) > 0 || sizeof($unpaidItems->tickets) > 0 || sizeof($unpaidItems->callbacks) > 0):?>
 
-  <table class="table thead-actual">
-    <thead>
-    <tr>
-      <th><?=\Yii::t('app', 'Тип билета');?></th>
-      <th class="col-width t-right"><?=\Yii::t('app', 'Цена');?></th>
-      <th class="col-width t-right"><?=\Yii::t('app', 'Кол-во');?></th>
-      <th class="col-width t-right last-child"><?=\Yii::t('app', 'Сумма');?></th>
-    </tr>
-    </thead>
-  </table>
-
-  <?foreach ($unpaidItems->all as $items):?>
-    <?
-    /** @var $items \pay\components\OrderItemCollectable[] */
-    $product = $items[0]->getOrderItem()->Product;
-    ?>
-    <table class="table">
-      <thead>
-      <tr>
-        <th colspan="2"><h4 class="title"><?=$product->Title;?> <i class="icon-chevron-up"></i></h4></th>
-        <th class="col-width t-right"><span class="number"><?=$product->getPrice();?></span> <?=Yii::t('app', 'руб.');?></th>
-        <th class="col-width t-right"><b class="number"><?=sizeof($items);?></b></th>
-        <th class="col-width t-right last-child"><b class="number"><?=$product->getPrice()*sizeof($items);?></b> <?=Yii::t('app', 'руб.');?></th>
-      </tr>
-      </thead>
-      <tbody>
-      <?foreach ($items as $item):?>
-        <?$total += $item->getPriceDiscount();?>
+    <table class="table thead-actual">
+        <thead>
         <tr>
-          <td style="padding-left: 10px; width: 15px;">
-            <?= \CHtml::beginForm(array('/pay/cabinet/deleteitem', 'orderItemId' => $item->getOrderItem()->Id), 'post', array('class' => 'button-only')); ?>
-              <?= \CHtml::htmlButton('<i class="icon-trash"></i>', array('type' => 'submit')); ?>
-            <?= \CHtml::endForm(); ?>
-          </td>
-          <td>
-            <?=$item->getOrderItem()->Owner->getFullName();?>
-          </td>
-          <td colspan="3" class="t-right muted last-child">
-            <?if ($item->getPriceDiscount() < $item->getOrderItem()->getPrice()):?>
-              <?if (!$item->getIsGroupDiscount()):?>
-                <?if ($item->getOrderItem()->getCouponActivation() !== null):?>
-                  <?=\Yii::t('app', 'Промо код');?> <?=$item->getOrderItem()->getCouponActivation()->Coupon->Code;?>
-                <?elseif ($item->getOrderItem()->Owner->hasLoyaltyDiscount()):?>
-                  <?=\Yii::t('app', 'RUNET-ID CARD');?>
-                <?endif;?>
-              <?else:?>
-                  <?=\Yii::t('app', 'Групповая скидка');?>:
-              <?endif;?>
-              <b class="number">-<?=$item->getOrderItem()->getPrice() - $item->getPriceDiscount();?></b> <?=Yii::t('app', 'руб.');?>
-            <?elseif ($item->getOrderItem()->Product->ManagerName == 'RoomProductManager'):?>
-              <?$dateFormatter = Yii::app()->getLocale()->getDateFormatter();?>
-              с <?=$dateFormatter->format('dd MMMM' , strtotime($item->getOrderItem()->getItemAttribute('DateIn')));?> по <?=$dateFormatter->format('dd MMMM' , strtotime($item->getOrderItem()->getItemAttribute('DateOut')));?> за  <b class="number"><?=$item->getPriceDiscount();?></b> <?=Yii::t('app', 'руб.');?>
-            <?endif;?>
-          </td>
+            <th><?=\Yii::t('app', 'Тип билета');?></th>
+            <th class="col-width t-right"><?=\Yii::t('app', 'Цена');?></th>
+            <th class="col-width t-right"><?=\Yii::t('app', 'Кол-во');?></th>
+            <th class="col-width t-right last-child"><?=\Yii::t('app', 'Сумма');?></th>
         </tr>
-      <?endforeach;?>
-      </tbody>
+        </thead>
     </table>
-  <?endforeach;?>
 
-  <?foreach ($unpaidItems->tickets as $items):?>
-    <table class="table">
-      <thead>
-        <tr>
-          <th colspan="5"><h4 class="title"><?=\Yii::t('app', 'Билеты');?> <i class="icon-chevron-up"></i></h4></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?foreach ($items as $item):?>
-          <?$total += $item->getPriceDiscount();?>
-          <tr>
-            <td style="padding-left: 10px; width: 15px;">
-              <?= \CHtml::beginForm(array('/pay/cabinet/deleteitem', 'orderItemId' => $item->getOrderItem()->Id), 'post', array('class' => 'button-only')); ?>
-              <?= \CHtml::htmlButton('<i class="icon-trash"></i>', array('type' => 'submit')); ?>
-              <?= \CHtml::endForm(); ?>
-            </td>
-            <td><?=$item->getOrderItem()->Product->getManager()->getTitle($item->getOrderItem());?></td>
-            <td class="col-width t-right"><?=$item->getOrderItem()->Product->getPrice();?> <?=Yii::t('app', 'руб.');?></td>
-            <td class="col-width t-right"><?=$item->getOrderItem()->getItemAttribute('Count');?></td>
-            <td class="col-width t-right last-child"><b class="number"><?=$item->getOrderItem()->getPrice();?>  <?=Yii::t('app', 'руб.');?></b></td>
-          </tr>
-        <?endforeach;?>
-      </tbody>
-    </table>
-  <?endforeach;?>
+    <?foreach ($unpaidItems->all as $items):?>
+        <?
+        /** @var $items \pay\components\OrderItemCollectable[] */
+        $product = $items[0]->getOrderItem()->Product;
+        ?>
+        <table class="table">
+            <thead>
+            <tr>
+                <th colspan="2"><h4 class="title"><?=$product->Title;?> <i class="icon-chevron-up"></i></h4></th>
+                <th class="col-width t-right"><span class="number"><?=$product->getPrice();?></span> <?=Yii::t('app', 'руб.');?></th>
+                <th class="col-width t-right"><b class="number"><?=sizeof($items);?></b></th>
+                <th class="col-width t-right last-child"><b class="number"><?=$product->getPrice()*sizeof($items);?></b> <?=Yii::t('app', 'руб.');?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?foreach ($items as $item):?>
+                <?$total += $item->getPriceDiscount();?>
+                <tr>
+                    <td style="padding-left: 10px; width: 15px;">
+                        <?= \CHtml::beginForm(array('/pay/cabinet/deleteitem', 'orderItemId' => $item->getOrderItem()->Id), 'post', array('class' => 'button-only')); ?>
+                        <?= \CHtml::htmlButton('<i class="icon-trash"></i>', array('type' => 'submit')); ?>
+                        <?= \CHtml::endForm(); ?>
+                    </td>
+                    <td>
+                        <?=$item->getOrderItem()->Owner->getFullName();?>
+                    </td>
+                    <td colspan="3" class="t-right muted last-child">
+                        <?if ($item->getPriceDiscount() < $item->getOrderItem()->getPrice()):?>
+                            <?if (!$item->getIsGroupDiscount()):?>
+                                <?if ($item->getOrderItem()->getCouponActivation() !== null):?>
+                                    <?=\Yii::t('app', 'Промо код');?> <?=$item->getOrderItem()->getCouponActivation()->Coupon->Code;?>
+                                <?elseif ($item->getOrderItem()->Owner->hasLoyaltyDiscount()):?>
+                                    <?=\Yii::t('app', 'RUNET-ID CARD');?>
+                                <?endif;?>
+                            <?else:?>
+                                <?=\Yii::t('app', 'Групповая скидка');?>:
+                            <?endif;?>
+                            <b class="number">-<?=$item->getOrderItem()->getPrice() - $item->getPriceDiscount();?></b> <?=Yii::t('app', 'руб.');?>
+                        <?elseif ($item->getOrderItem()->Product->ManagerName == 'RoomProductManager'):?>
+                            <?$dateFormatter = Yii::app()->getLocale()->getDateFormatter();?>
+                            с <?=$dateFormatter->format('dd MMMM' , strtotime($item->getOrderItem()->getItemAttribute('DateIn')));?> по <?=$dateFormatter->format('dd MMMM' , strtotime($item->getOrderItem()->getItemAttribute('DateOut')));?> за  <b class="number"><?=$item->getPriceDiscount();?></b> <?=Yii::t('app', 'руб.');?>
+                        <?endif;?>
+                    </td>
+                </tr>
+            <?endforeach;?>
+            </tbody>
+        </table>
+    <?endforeach;?>
 
-  <div class="total">
-    <span><?=\Yii::t('app', 'Итого');?>:</span> <b class="number"><?=\Yii::app()->numberFormatter->format('#,##0.00', $total);?></b> <?=Yii::t('app', 'руб.');?>
-  </div>
+    <?foreach ($unpaidItems->callbacks as $items):?>
+        <?
+        /** @var $items \pay\components\OrderItemCollectable[] */
+        $product = $items[0]->getOrderItem()->Product;
+        ?>
+        <table class="table">
+            <thead>
+            <tr>
+                <th colspan="5"><h4 class="title"><?=$product->Title;?> <i class="icon-chevron-up"></i></h4></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?foreach ($items as $item):?>
+                <?$total += $item->getPriceDiscount();?>
+                <tr>
+                    <td style="padding-left: 10px; width: 15px;">
+                        <?= \CHtml::beginForm(array('/pay/cabinet/deleteitem', 'orderItemId' => $item->getOrderItem()->Id), 'post', array('class' => 'button-only')); ?>
+                        <?= \CHtml::htmlButton('<i class="icon-trash"></i>', array('type' => 'submit')); ?>
+                        <?= \CHtml::endForm(); ?>
+                    </td>
+                    <td><?=$item->getOrderItem()->Product->getManager()->getTitle($item->getOrderItem());?></td>
+                    <td class="col-width t-right"><?=$item->getOrderItem()->getPrice();?> <?=Yii::t('app', 'руб.');?></td>
+                    <td class="col-width t-right"><?=$item->getOrderItem()->Product->getManager()->getCount($item->getOrderItem());?></td>
+                    <td class="col-width t-right last-child"><b class="number"><?=$item->getOrderItem()->getPrice();?>  <?=Yii::t('app', 'руб.');?></b></td>
+                </tr>
+            <?endforeach;?>
+            </tbody>
+        </table>
+    <?endforeach;?>
 
-  <div style="width: 500px; margin: 0 auto; margin-bottom: 40px;">
-    <?if (!$formAdditionalAttributes->getIsEmpty()):?>
-      <div class="well m-bottom_30">
-        <h4><?=($formAdditionalAttributes->FormTitle !== null ? $formAdditionalAttributes->FormTitle : \Yii::t('app', 'Дополнительные данные'));?></h4>
-        <?=\CHtml::errorSummary($formAdditionalAttributes, '<div class="alert alert-error">', '</div>');?>
-        <?=\CHtml::form('','POST',['class' => 'additional-attributes']);?>
-        <?foreach($formAdditionalAttributes->attributeNames() as $attr):?>
-          <div class="control-group">
-            <?=\CHtml::activeLabel($formAdditionalAttributes, $attr, ['class' => 'control-label']);?>
-            <div class="controls">
-              <?=$formAdditionalAttributes->getHtmlActiveField($attr);?>
+    <?foreach ($unpaidItems->tickets as $items):?>
+        <table class="table">
+            <thead>
+            <tr>
+                <th colspan="5"><h4 class="title"><?=\Yii::t('app', 'Билеты');?> <i class="icon-chevron-up"></i></h4></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?foreach ($items as $item):?>
+                <?$total += $item->getPriceDiscount();?>
+                <tr>
+                    <td style="padding-left: 10px; width: 15px;">
+                        <?= \CHtml::beginForm(array('/pay/cabinet/deleteitem', 'orderItemId' => $item->getOrderItem()->Id), 'post', array('class' => 'button-only')); ?>
+                        <?= \CHtml::htmlButton('<i class="icon-trash"></i>', array('type' => 'submit')); ?>
+                        <?= \CHtml::endForm(); ?>
+                    </td>
+                    <td><?=$item->getOrderItem()->Product->getManager()->getTitle($item->getOrderItem());?></td>
+                    <td class="col-width t-right"><?=$item->getOrderItem()->Product->getPrice();?> <?=Yii::t('app', 'руб.');?></td>
+                    <td class="col-width t-right"><?=$item->getOrderItem()->getItemAttribute('Count');?></td>
+                    <td class="col-width t-right last-child"><b class="number"><?=$item->getOrderItem()->getPrice();?>  <?=Yii::t('app', 'руб.');?></b></td>
+                </tr>
+            <?endforeach;?>
+            </tbody>
+        </table>
+    <?endforeach;?>
+
+    <div class="total">
+        <span><?=\Yii::t('app', 'Итого');?>:</span> <b class="number"><?=\Yii::app()->numberFormatter->format('#,##0.00', $total);?></b> <?=Yii::t('app', 'руб.');?>
+    </div>
+
+    <div style="width: 500px; margin: 0 auto; margin-bottom: 40px;">
+        <?if (!$formAdditionalAttributes->getIsEmpty()):?>
+            <div class="well m-bottom_30">
+                <h4><?=($formAdditionalAttributes->FormTitle !== null ? $formAdditionalAttributes->FormTitle : \Yii::t('app', 'Дополнительные данные'));?></h4>
+                <?=\CHtml::errorSummary($formAdditionalAttributes, '<div class="alert alert-error">', '</div>');?>
+                <?=\CHtml::form('','POST',['class' => 'additional-attributes']);?>
+                <?foreach($formAdditionalAttributes->attributeNames() as $attr):?>
+                    <div class="control-group">
+                        <?=\CHtml::activeLabel($formAdditionalAttributes, $attr, ['class' => 'control-label']);?>
+                        <div class="controls">
+                            <?=$formAdditionalAttributes->getHtmlActiveField($attr);?>
+                        </div>
+                    </div>
+                <?endforeach;?>
+                <?=\CHtml::activeHiddenField($formAdditionalAttributes, 'SuccessUrl');?>
+                <?=\CHtml::endForm();?>
             </div>
-          </div>
-        <?endforeach;?>
-        <?=\CHtml::activeHiddenField($formAdditionalAttributes, 'SuccessUrl');?>
-        <?=\CHtml::endForm();?>
-      </div>
-    <?endif;?>
+        <?endif;?>
 
-    <label class="checkbox">
-      <input type="checkbox" name="agreeOffer" value="1"/><?=\Yii::t('app', 'Я согласен с условиями <a target="_blank" href="{url}">договора-оферты</a> и готов перейти к оплате', array('{url}' => $this->createUrl('/pay/cabinet/offer')));?>
-    </label>
-  </div>
+        <label class="checkbox">
+            <input type="checkbox" name="agreeOffer" value="1"/><?=\Yii::t('app', 'Я согласен с условиями <a target="_blank" href="{url}">договора-оферты</a> и готов перейти к оплате', array('{url}' => $this->createUrl('/pay/cabinet/offer')));?>
+        </label>
+    </div>
 
-  <?$this->renderPartial('index/payments', ['account' => $account, 'total' => $total]);?>
+    <?$this->renderPartial('index/payments', ['account' => $account, 'total' => $total]);?>
 <?else:?>
 
-  <style type="text/css">
-    .event-register .alert {
-      margin: 0 40px 40px;
-    }
-  </style>
-  <?if (!$hasRecentPaidItems):?>
-    <div class="alert alert-error"><?=\Yii::t('app', 'У вас нет товаров для оплаты.');?></div>
-  <?else:?>
-    <div class="alert alert-success"><?=\Yii::t('app', 'Вы недавно оплатили участие или активировали промо-код. Список оплаченных товаров можно посмотреть ниже.');?></div>
-  <?endif;?>
+    <style type="text/css">
+        .event-register .alert {
+            margin: 0 40px 40px;
+        }
+    </style>
+    <?if (!$hasRecentPaidItems):?>
+        <div class="alert alert-error"><?=\Yii::t('app', 'У вас нет товаров для оплаты.');?></div>
+    <?else:?>
+        <div class="alert alert-success"><?=\Yii::t('app', 'Вы недавно оплатили участие или активировали промо-код. Список оплаченных товаров можно посмотреть ниже.');?></div>
+    <?endif;?>
 
-  <div class="nav-buttons">
-    <a href="<?=$account->ReturnUrl===null ? $this->createUrl('/pay/cabinet/register') : $account->ReturnUrl;?>" class="btn btn-large">
-      <i class="icon-circle-arrow-left"></i>
-      <?=\Yii::t('app', 'Назад');?>
-    </a>
-  </div>
+    <div class="nav-buttons">
+        <a href="<?=$account->ReturnUrl===null ? $this->createUrl('/pay/cabinet/register') : $account->ReturnUrl;?>" class="btn btn-large">
+            <i class="icon-circle-arrow-left"></i>
+            <?=\Yii::t('app', 'Назад');?>
+        </a>
+    </div>
 
 <?endif;?>
