bc2bf774be92f7cdd3f837ea9e7abd4dcd19a191
Korotov Andrey <andrey.korotov@yandex.ru>
2014-09-22 10:28:08 +0400

    Выод сообщения если счет удален

--- protected/modules/pay/controllers/OrderController.php
+++ protected/modules/pay/controllers/OrderController.php
@@ -2,32 +2,34 @@
 
 class OrderController extends \application\components\controllers\MainController
 {
-  public $layout = '/layouts/bill';
+    public $layout = '/layouts/bill';
 
-  public function actionIndex($orderId, $hash = null, $clear = null)
-  {
-    /** @var $order \pay\models\Order */
-    $order = \pay\models\Order::model()->findByPk($orderId);
-    if ($order === null || (!\pay\models\OrderType::getIsBank($order->Type)))
-      throw new \CHttpException(404);
-
-    $checkHash = $order->checkHash($hash);
-    if (!$checkHash && (\Yii::app()->user->getCurrentUser() === null || \Yii::app()->user->getCurrentUser()->Id != $order->PayerId))
+    public function actionIndex($orderId, $hash = null, $clear = null)
     {
-      throw new \CHttpException(404);
+        /** @var $order \pay\models\Order */
+        $order = \pay\models\Order::model()->findByPk($orderId);
+        if ($order === null || (!\pay\models\OrderType::getIsBank($order->Type)))
+            throw new \CHttpException(404);
+
+        $checkHash = $order->checkHash($hash);
+        if (!$checkHash && (\Yii::app()->user->getCurrentUser() === null || \Yii::app()->user->getCurrentUser()->Id != $order->PayerId))
+        {
+            throw new \CHttpException(404);
+        }
+
+        if ($clear === null && $order->Deleted) {
+            $this->renderText(\Yii::t('app', 'Данный счет был удален. Вы можете выставить новый или восстановить этот счет обратившись по адресу {email}.', ['{email}' => \CHtml::mailto('fin@runet-id.com')]));
+            \Yii::app()->end();
+        }
+
+        $this->setPageTitle('Счёт № ' . $order->Number);
+        $this->render($order->getViewName(), [
+            'order' => $order,
+            'billData' => $order->getBillData()->Data,
+            'total' => $order->getBillData()->Total,
+            'nds' => $order->getBillData()->Nds,
+            'withSign' => $clear===null,
+            'template' => $order->getViewTemplate()
+        ]);
     }
-
-    if ($clear === null && $order->Deleted)
-      throw new \CHttpException(404);
-
-    $this->setPageTitle('Счёт № ' . $order->Number);
-    $this->render($order->getViewName(), [
-      'order' => $order,
-      'billData' => $order->getBillData()->Data,
-      'total' => $order->getBillData()->Total,
-      'nds' => $order->getBillData()->Nds,
-      'withSign' => $clear===null,
-      'template' => $order->getViewTemplate()
-    ]);
-  }
 }
