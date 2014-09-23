f53fcc5aecf8704211999858c8808b5f62cfbff3
Korotov Andrey <andrey.korotov@yandex.ru>
2014-09-23 18:35:36 +0400

    Дополнение к 2332

--- protected/modules/event/views/mail/register/parts/calendar.php
+++ protected/modules/event/views/mail/register/parts/calendar.php
@@ -6,5 +6,5 @@ use event\models\Event;
 ?>
 <img src="http://runet-id.com/images/mail/icons/google_calendar.png" />
 <a href="<?=\Yii::app()->createAbsoluteUrl('/event/view/share', ['targetService' => 'Google', 'idName' => $event->IdName])?>" style="vertical-align: top;line-height: 15px;">Добавить в Google Calendar</a><br/>
-<img src="http://runet-id.com/images/mail/ical.png" />
+<img src="http://runet-id.com/images/mail/icons/ical.png" />
 <a href="<?=\Yii::app()->createAbsoluteUrl('/event/view/share', ['targetService' => 'iCal', 'idName' => $event->IdName])?>"  style="vertical-align: top;line-height: 15px;">Добавить в iCalendar</a>
