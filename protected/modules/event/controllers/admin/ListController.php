9e54b8de34539f7f1ca492e14be5e00316a7ad25
Korotov Andrey <andrey.korotov@yandex.ru>
2014-09-24 12:18:32 +0400

    Closes #2201: Список мероприятий в административном интерфейсе

--- protected/modules/event/controllers/admin/ListController.php
+++ protected/modules/event/controllers/admin/ListController.php
@@ -1,77 +1,75 @@
 <?php
+use application\components\utility\Paginator;
+use \event\models\Event;
+
 class ListController extends \application\components\controllers\AdminMainController
 {
-  public function actionIndex()
-  {
-    $this->processAction();
-
-    $criteria = new \CDbCriteria();
-    $criteria->order = '"t"."Id" DESC';
-    $criteria->addCondition('NOT "t"."Deleted"');
-
-    $searchQuery = \Yii::app()->request->getParam('Query', null);
-    if (!empty($searchQuery))
+    public function actionIndex()
     {
-      if (is_numeric($searchQuery))
-      {
-        $criteria->addCondition('"t"."Id" = :Query');
-        $criteria->params['Query'] = $searchQuery;
-      }
-      else 
-      {
-        $criteria->addCondition('"t"."IdName" ILIKE :Query OR "t"."Title" ILIKE :Query');
-        $criteria->params['Query'] = '%'.$searchQuery.'%';
-      }
-    }
-    
-    $approved = \Yii::app()->request->getParam('Approved', null);
-    if ($approved !== null)
-    {
-      $criteria->addCondition('"t"."External" AND "t"."Approved" = :Approved');
-      $criteria->params['Approved'] = $approved;
-    }
-    else
-    {
-      $criteria->addCondition('"t"."Visible" OR NOT "t"."External"');
-    }
-    
-    $eventCountAll = \event\models\Event::model()->count($criteria);
-    $paginator = new \application\components\utility\Paginator($eventCountAll);
-    $paginator->perPage = \Yii::app()->params['AdminEventPerPage'];
-    $criteria->mergeWith($paginator->getCriteria());
-    $events = \event\models\Event::model()->findAll($criteria);
-    $this->render('index', array(
-      'events' => $events, 
-      'paginator' => $paginator,
-    ));
-  }
+        $this->processAction();
 
-  private function processAction()
-  {
-    $request = \Yii::app()->getRequest();
-    $action = $request->getParam('Action');
-    if ($action == null)
-      return;
+        $criteria = new \CDbCriteria();
+        $criteria->order = '"t"."Id" DESC';
+        $criteria->with  = ['Type','Attributes','LinkProfessionalInterests'];
 
-    $event = \event\models\Event::model()->findByPk($request->getParam('EventId'));
-    if ($event == null)
-      return;
-
-    $backUrl = $request->getParam('BackUrl');
-    if ($backUrl == null)
-      return;
-
-    switch ($action)
-    {
-      case 'Delete':
-        if ($event->getCanBeRemoved())
+        $searchQuery = \Yii::app()->getRequest()->getParam('Query', null);
+        if (!empty($searchQuery))
         {
-          $event->Deleted = true;
-          $event->DeletionTime = date('Y-m-d H:i:s');
-          $event->save();
+            if (is_numeric($searchQuery))
+            {
+                $criteria->addCondition('"t"."Id" = :Query');
+                $criteria->params['Query'] = $searchQuery;
+            }
+            else
+            {
+                $criteria->addCondition('"t"."IdName" ILIKE :Query OR "t"."Title" ILIKE :Query');
+                $criteria->params['Query'] = '%'.$searchQuery.'%';
+            }
         }
+
+        $approved = \Yii::app()->request->getParam('Approved', null);
+        if ($approved !== null)
+        {
+            $criteria->addCondition('"t"."External" AND "t"."Approved" = :Approved');
+            $criteria->params['Approved'] = $approved;
+        }
+
+        $paginator = new Paginator(Event::model()->count($criteria));
+        $paginator->perPage = \Yii::app()->params['AdminEventPerPage'];
+        $criteria->mergeWith($paginator->getCriteria());
+        $events = Event::model()->findAll($criteria);
+        $this->render('index', array(
+            'events' => $events,
+            'paginator' => $paginator,
+        ));
     }
 
-    $this->redirect($backUrl);
-  }
+    private function processAction()
+    {
+        $request = \Yii::app()->getRequest();
+        $action = $request->getParam('Action');
+        if ($action == null)
+            return;
+
+        $event = \event\models\Event::model()->findByPk($request->getParam('EventId'));
+        if ($event == null)
+            return;
+
+        $backUrl = $request->getParam('BackUrl');
+        if ($backUrl == null)
+            return;
+
+        switch ($action)
+        {
+            case 'Delete':
+                if ($event->getCanBeRemoved())
+                {
+                    $event->Deleted = true;
+                    $event->DeletionTime = date('Y-m-d H:i:s');
+                    $event->save();
+                }
+        }
+
+        $this->redirect($backUrl);
+    }
 }
--- protected/modules/event/views/admin/list/index.php
+++ protected/modules/event/views/admin/list/index.php
@@ -1,53 +1,75 @@
 <?/**
  * @var \event\models\Event[] $events
  */
+use application\components\utility\Texts;
+use event\models\Approved;
+
 ?>
 
 
 <div class="row-fluid">
-  <div class="btn-toolbar clearfix">
-    <?=\CHtml::form($this->createUrl('/event/admin/list/index'), 'GET', array('class' => 'form-inline pull-left'));?>
-      <?=\CHtml::textField('Query', \Yii::app()->request->getParam('Query', ''), array('placeholder' => \Yii::t('app', 'ID или навзание мероприятия'), 'style' => 'margin-right:5px'));?>
-      <?=\CHtml::submitButton(\Yii::t('app', 'Искать'), array('class' => 'btn'));?>
-    <?=\CHtml::endForm();?>
+    <div class="btn-toolbar clearfix">
+        <?=\CHtml::form($this->createUrl('/event/admin/list/index'), 'GET', array('class' => 'form-inline pull-left'));?>
+        <?=\CHtml::textField('Query', \Yii::app()->request->getParam('Query', ''), array('placeholder' => \Yii::t('app', 'ID или навзание мероприятия'), 'style' => 'margin-right:5px'));?>
+        <?=\CHtml::submitButton(\Yii::t('app', 'Искать'), array('class' => 'btn'));?>
+        <?=\CHtml::endForm();?>
 
-    <a href="<?=$this->createUrl('/event/admin/edit/index');?>" class="btn btn-success pull-right"><?=\Yii::t('app', 'Добавить мероприятие');?></a>
-  </div>
-  <div class="well">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Код</th>
-          <th>Лого</th>
-          <th>Название</th>
-          <th>Даты проведения</th>
-          <th style="width: 26px;"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?foreach ($events as $event):?>
-          <?/** @var \event\models\Event $event; */?>
-          <tr>
-            <td><strong><?=$event->Id;?></strong></td>
-            <td><?=$event->IdName;?></td>
-            <td><?=\CHtml::image($event->getLogo()->getMini());?></td>
-            <td><a href="<?=$this->createUrl('/event/admin/edit/index', array('eventId' => $event->Id));?>"><?=$event->Title;?></a></td>
-            <td><?$this->widget('event\widgets\Date', array('event' => $event));?></td>
-            <td>
-              <div class="btn-group">
-                <?if ($event->getCanBeRemoved()):?>
-                  <a href="<?=$this->createUrl('/event/admin/list/index', ['EventId' => $event->Id, 'Action' => 'Delete', 'BackUrl' => \Yii::app()->getRequest()->getRequestUri()]);?>" class="btn btn-danger"><i class="icon-remove icon-white"></i></a>
-                <?endif;?>
-                <a href="<?=$this->createUrl('/event/admin/edit/index', ['eventId' => $event->Id]);?>" class="btn"><i class="icon-edit"></i> <?=\Yii::t('app', 'Редактировать');?></a>
-              </div>
-            </td>
-          </tr>
-        <?endforeach;?>
-      </tbody>
-    </table>
-  </div>
-  <?$this->widget('application\widgets\Paginator', array(
-    'paginator' => $paginator
-  ));?>
+        <a href="<?=$this->createUrl('/event/admin/edit/index');?>" class="btn btn-success pull-right"><?=\Yii::t('app', 'Добавить мероприятие');?></a>
+    </div>
+    <div class="well">
+        <table class="table events">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Код</th>
+                <th>Лого</th>
+                <th>Название</th>
+                <th>Тип</th>
+                <th>Опции</th>
+                <th>Даты проведения</th>
+                <th style="width: 26px;"></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?foreach ($events as $event):?>
+                <?/** @var \event\models\Event $event; */?>
+                <?
+                $class = null;
+                if  ($event->Visible)
+                    $class = 'success';
+                elseif ($event->External && $event->Approved == Approved::None)
+                    $class = 'muted';
+                elseif ($event->External && $event->Approved == Approved::No)
+                    $class = 'error';
+                ?>
+
+                <tr <?if (!empty($class)):?>class="<?=$class;?><?endif;?>">
+                    <td><strong><?=$event->Id;?></strong></td>
+                    <td><?=Texts::cropText($event->IdName,50);?></td>
+                    <td><?=\CHtml::image($event->getLogo()->getMini());?></td>
+                    <td><a href="<?=$this->createUrl('/event/admin/edit/index', array('eventId' => $event->Id));?>"><?=$event->Title;?></a></td>
+                    <td><label class="label"><?=$event->Type->Title;?></label></td>
+                    <td class="icons">
+                        <i class="icon-eye-open <?if (!$event->ShowOnMain):?>muted<?endif;?>" title="Опубликовано на главной"></i>
+                        <i class="icon-fullscreen <?if (!isset($event->Top) || $event->Top == 0):?>muted<?endif;?>" title="Используется выделение в большой блок"></i>
+                        <i class="icon-list <?if (empty($event->LinkProfessionalInterests)):?>muted<?endif;?>" title="Проставлены проф. интересы"></i>
+                    </td>
+                    <td><?$this->widget('event\widgets\Date', array('event' => $event));?></td>
+                    <td class="buttons">
+                        <div class="btn-group">
+                            <?if ($event->getCanBeRemoved()):?>
+                                <a href="<?=$this->createUrl('/event/admin/list/index', ['EventId' => $event->Id, 'Action' => 'Delete', 'BackUrl' => \Yii::app()->getRequest()->getRequestUri()]);?>" class="btn btn-danger"><i class="icon-remove icon-white"></i></a>
+                            <?endif;?>
+                            <a href="<?=$this->createUrl('/event/admin/edit/index', ['eventId' => $event->Id]);?>" class="btn"><i class="icon-edit"></i></a>
+                            <a href="<?=$this->createUrl('/event/view/index', ['idName' => $event->IdName]);?>" class="btn" target="_blank"><i class="icon-share"></i></a>
+                        </div>
+                    </td>
+                </tr>
+            <?endforeach;?>
+            </tbody>
+        </table>
+    </div>
+    <?$this->widget('application\widgets\Paginator', array(
+        'paginator' => $paginator
+    ));?>
 </div>
