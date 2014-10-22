0e4e98b5a6bcf752862e3247e85418f9b2179f21
Nikitin Vitaly <alaris.nik@gmail.com>
2014-10-22 12:40:00 +0400

    Убраны костыли для проверки участия с помощью телефона на конкретном мероприятии.

--- protected/modules/ruvents/controllers/product/CreateProductGetAction.php
+++ protected/modules/ruvents/controllers/product/CreateProductGetAction.php
@@ -4,40 +4,24 @@ namespace ruvents\controllers\product;
 
 class CreateProductGetAction extends \ruvents\components\Action
 {
-  public function run($runetId, $productId)
-  {
-    $user = \user\models\User::model()->byRunetId($runetId)->byEventId($this->getEvent()->Id)->find();
-    if ($user == null)
-      throw new \ruvents\components\Exception(202,[$runetId]);
+    public function run($runetId, $productId)
+    {
+        $user = \user\models\User::model()->byRunetId($runetId)->byEventId($this->getEvent()->Id)->find();
+        if ($user == null)
+            throw new \ruvents\components\Exception(202,[$runetId]);
 
-    $product = \pay\models\Product::model()->byEventId($this->getEvent()->Id)->findByPk($productId);
-    if ($product == null)
-      throw new \ruvents\components\Exception(401,[$productId]);
+        $product = \pay\models\Product::model()->byEventId($this->getEvent()->Id)->findByPk($productId);
+        if ($product == null)
+            throw new \ruvents\components\Exception(401,[$productId]);
 
-//    $get = \pay\models\ProductGet::model()->byUserId($user->Id)->byProductId($product->Id)->find();
-//    if ($get == null)
-//    {
-      $get = new \pay\models\ProductGet();
-      $get->UserId = $user->Id;
-      $get->ProductId = $product->Id;
-      $get->save();
-      $get->refresh();
-    //}
 
-      if ($productId == 3036) {
-          $badge = new \ruvents\models\Badge();
-          $badge->OperatorId = 1534;
-          $badge->EventId = $this->getEvent()->Id;
-          $badge->UserId = $user->Id;
+        $get = new \pay\models\ProductGet();
+        $get->UserId = $user->Id;
+        $get->ProductId = $product->Id;
+        $get->save();
+        $get->refresh();
 
-          $participant = \event\models\Participant::model()->byEventId($this->getEvent()->Id)->byUserId($user->Id)->find();
-          if ($participant === null) {
-              throw new \ruvents\components\Exception(304);
-          }
-          $badge->RoleId = $participant->RoleId;
-          $badge->save();
-      }
 
-    $this->renderJson(['Success' => true, 'CreationTime' => $get->CreationTime]);
-  }
+        $this->renderJson(['Success' => true, 'CreationTime' => $get->CreationTime]);
+    }
 } 
--- protected/modules/ruvents/controllers/product/FastPaidItemsAction.php
+++ protected/modules/ruvents/controllers/product/FastPaidItemsAction.php
@@ -14,11 +14,6 @@ class FastPaidItemsAction extends \ruvents\components\Action
 {
     public function run($productId, $fromTime = null)
     {
-        if ($productId == 3036) {
-            $this->renderALDUsers($fromTime);
-            return;
-        }
-
         $criteria = new \CDbCriteria();
         $criteria->with = [
             'Owner',
@@ -51,36 +46,6 @@ class FastPaidItemsAction extends \ruvents\components\Action
         $data = new \stdClass();
         $data->Owners = $result;
         $data->MaxCreationTime = $maxCreationTime;
-
-        $this->renderJson($data);
-    }
-
-    private function renderALDUsers($fromTime = null)
-    {
-        $criteria = new \CDbCriteria();
-        if (!empty($fromTime)) {
-            $datetime = \DateTime::createFromFormat('Y-m-d H:i:s', $fromTime);
-            if ($datetime === false)
-                throw new \ruvents\components\Exception(321);
-
-            $criteria->addCondition('"t"."UpdateTime" >= :Time');
-            $criteria->params['Time'] = $datetime->format('Y-m-d H:i:s');
-        }
-
-        $participants = Participant::model()->byEventId(1370)->findAll($criteria);
-        $result = [];
-        $maxUpdateTime = '1970-01-01 00:00:00';
-        foreach ($participants as $participant)
-        {
-            $result[] = $participant->User->RunetId;
-
-            if ($participant->UpdateTime > $maxUpdateTime)
-                $maxUpdateTime = $participant->UpdateTime;
-        }
-
-        $data = new \stdClass();
-        $data->Owners = $result;
-        $data->MaxCreationTime = $maxUpdateTime;
 
         $this->renderJson($data);
     }
