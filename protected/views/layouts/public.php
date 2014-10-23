e0568ad39461ece8d007e92f15d27efb41ed2813
Maxim Borzov <max.borzov@gmail.com>
2014-10-23 13:54:09 +0400

    Удален код Google Tag Manager

--- protected/views/layouts/public.php
+++ protected/views/layouts/public.php
@@ -21,9 +21,6 @@
   <![endif]-->
 </head>
 <body id="<?php echo $this->bodyId;?>">
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KP565L" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KP565L');</script>
-<!-- End Google Tag Manager -->
 <noscript>JavaScript disabled</noscript>
 <header id="header" role="banner">
   <div class="navbar navbar-fixed-top navbar-inverse">
