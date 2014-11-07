dcb5f78426d7291dfed1e56fbe91e33d5e3cb54e
Korotov Andrey <andrey.korotov@yandex.ru>
2014-11-07 13:58:14 +0300

    Closes #2447: Баг отписки при совпадении email

--- protected/modules/user/controllers/UnsubscribeController.php
+++ protected/modules/user/controllers/UnsubscribeController.php
@@ -2,16 +2,25 @@
 
 class UnsubscribeController extends \application\components\controllers\PublicMainController
 {
-  public function actionIndex($email, $hash)
-  {
-    $user = \user\models\User::model()->byEmail($email)->byVisible(true)->find();
-    if ($user == null)
-      throw new CHttpException(500, 'Не найден пользователь с email: ' . $email);
-    if ($user->getHash() != $hash)
-      throw new \CHttpException(500, 'Не корректный код отписки пользователя. Пришедший код: ' . $hash . ' RUNET-ID пользователя: ' . $user->RunetId);
-    $user->Settings->UnsubscribeAll = true;
-    $user->Settings->save();
-    $this->setPageTitle(\Yii::t('app', 'Подписка успешно отменена'));
-    $this->render('index');
-  }
+    public function actionIndex($email, $hash)
+    {
+        $users = \user\models\User::model()->byEmail($email)->findAll();
+        if (empty($users)) {
+            throw new CHttpException(500, 'Не найден пользователь с email: ' . $email);
+        }
+
+        /** @var \user\models\User $user */
+        foreach ($users as $user)
+        {
+            if ($user->getHash() == $hash) {
+                $user->Settings->UnsubscribeAll = true;
+                $user->Settings->save();
+                $this->setPageTitle(\Yii::t('app', 'Подписка успешно отменена'));
+                $this->render('index');
+                return;
+            }
+        }
+
+        throw new \CHttpException(500, 'Не корректный код отписки пользователя. Пришедший код: ' . $hash . ' Email пользователя: ' . $email);
+    }
 }
