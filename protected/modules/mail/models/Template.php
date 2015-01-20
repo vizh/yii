d064b588b48e71f35a133a7ff2721824fdc83bf2
Korotov Andrey <andrey.korotov@yandex.ru>
2015-01-20 11:01:31 +0300

    Возврат MandrilMailer для рассылок

--- protected/modules/mail/models/Template.php
+++ protected/modules/mail/models/Template.php
@@ -1,6 +1,7 @@
 <?php
 namespace mail\models;
 use event\models\Event;
+use mail\components\mailers\MandrillMailer;
 use mail\components\mailers\PhpMailer;
 
 /**
@@ -254,7 +255,7 @@ class Template extends \CActiveRecord
     {
         if ($this->mailer == null)
         {
-            $this->mailer = new PhpMailer();
+            $this->mailer = new MandrillMailer();
         }
         return $this->mailer;
     }
