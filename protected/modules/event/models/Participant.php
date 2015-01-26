32876007a66cc59e96531d5ba79329a9ab23a6fd
Vitaliy Nikitin <alaris.nik@gmail.com>
2015-01-26 16:49:59 +0300

    Правильные отступы в Participant, для дальнейшей работы с классом.

--- protected/modules/event/models/Participant.php
+++ protected/modules/event/models/Participant.php
@@ -21,80 +21,80 @@ namespace event\models;
  */
 class Participant extends \CActiveRecord
 {
-  /** @var int */
-  public $CountForCriteria;
+    /** @var int */
+    public $CountForCriteria;
 
-  /**
-   * @param string $className
-   * @return Participant
-   */
-  public static function model($className=__CLASS__)
-  {    
-    return parent::model($className);
-  }
-  
-  public function tableName()
-  {
-    return 'EventParticipant';
-  }
-  
-  public function primaryKey()
-  {
-    return 'Id';
-  }
-  
-  public function relations()
-  {
-    return array(
-      'Event' => array(self::BELONGS_TO, '\event\models\Event', 'EventId'),
-      'Role' => array(self::BELONGS_TO, '\event\models\Role', 'RoleId'),
-      'User' => array(self::BELONGS_TO, '\user\models\User', 'UserId'),
-      'Part' => array(self::BELONGS_TO, '\event\models\Part', 'PartId')
-    );
-  }
+    /**
+     * @param string $className
+     * @return Participant
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
-  /**
-   * @param int $userId
-   * @param bool $useAnd
-   * @return Participant
-   */
-  public function byUserId($userId, $useAnd = true)
-  {
-    $criteria = new \CDbCriteria();
-    $criteria->condition = '"t"."UserId" = :UserId';
-    $criteria->params = array(':UserId' => $userId);
-    $this->getDbCriteria()->mergeWith($criteria, $useAnd);
-    return $this;
-  }
+    public function tableName()
+    {
+        return 'EventParticipant';
+    }
 
-  /**
-   * @param int $eventId
-   * @param bool $useAnd
-   * @return Participant
-   */
-  public function byEventId($eventId, $useAnd = true)
-  {
-    $criteria = new \CDbCriteria();
-    $criteria->condition = '"t"."EventId" = :EventId';
-    $criteria->params = array(':EventId' => $eventId);
-    $this->getDbCriteria()->mergeWith($criteria, $useAnd);
-    return $this;
-  }
+    public function primaryKey()
+    {
+        return 'Id';
+    }
 
-  /**
-   * @param int $roleId
-   * @param bool $useAnd
-   *
-   * @return Participant
-   */
-  public function byRoleId($roleId, $useAnd = true)
-  {
-    $criteria = new \CDbCriteria();
-    $criteria->condition = '"t"."RoleId" = :RoleId';
-    $criteria->params = [':RoleId' => $roleId];
-    $this->getDbCriteria()->mergeWith($criteria, $useAnd);
-    return $this;
-  }
+    public function relations()
+    {
+        return array(
+            'Event' => array(self::BELONGS_TO, '\event\models\Event', 'EventId'),
+            'Role' => array(self::BELONGS_TO, '\event\models\Role', 'RoleId'),
+            'User' => array(self::BELONGS_TO, '\user\models\User', 'UserId'),
+            'Part' => array(self::BELONGS_TO, '\event\models\Part', 'PartId')
+        );
+    }
+
+    /**
+     * @param int $userId
+     * @param bool $useAnd
+     * @return Participant
+     */
+    public function byUserId($userId, $useAnd = true)
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->condition = '"t"."UserId" = :UserId';
+        $criteria->params = array(':UserId' => $userId);
+        $this->getDbCriteria()->mergeWith($criteria, $useAnd);
+        return $this;
+    }
+
+    /**
+     * @param int $eventId
+     * @param bool $useAnd
+     * @return Participant
+     */
+    public function byEventId($eventId, $useAnd = true)
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->condition = '"t"."EventId" = :EventId';
+        $criteria->params = array(':EventId' => $eventId);
+        $this->getDbCriteria()->mergeWith($criteria, $useAnd);
+        return $this;
+    }
+
+    /**
+     * @param int $roleId
+     * @param bool $useAnd
+     *
+     * @return Participant
+     */
+    public function byRoleId($roleId, $useAnd = true)
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->condition = '"t"."RoleId" = :RoleId';
+        $criteria->params = [':RoleId' => $roleId];
+        $this->getDbCriteria()->mergeWith($criteria, $useAnd);
+        return $this;
+    }
 
     /**
      * @param int|int[]|null $partId
@@ -116,47 +116,47 @@ class Participant extends \CActiveRecord
         return $this;
     }
 
-  /**
-   * @param $role Role
-   * @param bool $usePriority
-   * @return bool
-   */
-  public function UpdateRole($role, $usePriority = false)
-  {
-    if (!$usePriority || $this->Role->Priority <= $role->Priority)
+    /**
+     * @param $role Role
+     * @param bool $usePriority
+     * @return bool
+     */
+    public function UpdateRole($role, $usePriority = false)
     {
-      $oldRole = $this->Role;
+        if (!$usePriority || $this->Role->Priority <= $role->Priority)
+        {
+            $oldRole = $this->Role;
 
-      $this->RoleId = $role->RoleId;
-      $this->UpdateTime = time();
-      $this->save();
+            $this->RoleId = $role->RoleId;
+            $this->UpdateTime = time();
+            $this->save();
 
-      /** @var $partnerAccount \partner\models\Account */
-      $partnerAccount = \partner\models\Account::model()->byEventId($this->EventId)->find();
-      if (!empty($partnerAccount))
-      {
-        $partnerAccount->GetNotifier()->NotifyRoleChange($this->User, $oldRole, $role);
-      }
+            /** @var $partnerAccount \partner\models\Account */
+            $partnerAccount = \partner\models\Account::model()->byEventId($this->EventId)->find();
+            if (!empty($partnerAccount))
+            {
+                $partnerAccount->GetNotifier()->NotifyRoleChange($this->User, $oldRole, $role);
+            }
 
-      return true;
+            return true;
+        }
+
+        return false;
     }
 
-    return false;
-  }
-  
-  
-  private $hashSalt = 'aHQR/agr(pSEt/t.EFS.BT/!';
-  public function getHash()
-  {
-    return substr(md5($this->EventId.$this->hashSalt.$this->UserId), 2, 25);
-  }
-  
-  public function getTicketUrl()
-  {
-    return \Yii::app()->createAbsoluteUrl('/event/ticket/index', [
-      'eventIdName' => $this->Event->IdName,
-      'runetId' => $this->User->RunetId,
-      'hash' => $this->getHash()
-    ]);
-  }
+
+    private $hashSalt = 'aHQR/agr(pSEt/t.EFS.BT/!';
+    public function getHash()
+    {
+        return substr(md5($this->EventId.$this->hashSalt.$this->UserId), 2, 25);
+    }
+
+    public function getTicketUrl()
+    {
+        return \Yii::app()->createAbsoluteUrl('/event/ticket/index', [
+            'eventIdName' => $this->Event->IdName,
+            'runetId' => $this->User->RunetId,
+            'hash' => $this->getHash()
+        ]);
+    }
 }
