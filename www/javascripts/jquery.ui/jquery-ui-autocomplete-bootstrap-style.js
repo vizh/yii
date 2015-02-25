617e79e81af619277bd98645ea1cc7a2319742ff
Korotov Andrey <andrey.korotov@yandex.ru>
2015-02-25 17:04:01 +0300

    Правки автокомплита по chrome

--- www/javascripts/jquery.ui/jquery-ui-autocomplete-bootstrap-style.js
+++ www/javascripts/jquery.ui/jquery-ui-autocomplete-bootstrap-style.js
@@ -1,7 +1,7 @@
 $.widget("ui.autocomplete", $.ui.autocomplete, {
   options : {
     create: function(event, ui) {
-      $(this).autocomplete("widget").addClass("dropdown-menu");
+      $(this).autocomplete("widget").addClass("ui-autocomplete-bootstrap");
     },
     open: function(){
       $(this).autocomplete('widget').css('z-index', 1000);
