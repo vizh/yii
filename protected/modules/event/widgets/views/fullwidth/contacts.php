00f8cff3200ca7e3b2c1103c1ee3dd4f7cf3dea1
Korotov Andrey <andrey.korotov@yandex.ru>
2015-03-03 18:00:18 +0300

    Адрес в виджет контактов

--- protected/modules/event/widgets/views/fullwidth/contacts.php
+++ protected/modules/event/widgets/views/fullwidth/contacts.php
@@ -6,15 +6,18 @@
 ?>
 
 <div class="contacts">
-  <header>
-    <h3 class="title"><?=\Yii::t('app', 'Контакты');?></h3>
-  </header>
-  <article>
-    <?foreach ($phones as $phone):?>
-      <span class="phone"><?=$phone;?></span>
-    <?endforeach;?>
-    <?foreach ($this->event->LinkEmails as $link):?>
-      <a href="mailto: <?=$link->Email->Email;?>" class="email"><?=$link->Email->Email;?></a>
-    <?endforeach;?>
-  </article>
+    <header>
+        <h3 class="title"><?=\Yii::t('app', 'Контакты');?></h3>
+    </header>
+    <article>
+        <?if ($this->event->getContactAddress() !== null):?>
+            <p class="address"><?=$this->event->getContactAddress();?></p>
+        <?endif;?>
+        <?foreach ($phones as $phone):?>
+            <span class="phone"><?=$phone;?></span>
+        <?endforeach;?>
+        <?foreach ($this->event->LinkEmails as $link):?>
+            <a href="mailto: <?=$link->Email->Email;?>" class="email"><?=$link->Email->Email;?></a>
+        <?endforeach;?>
+    </article>
 </div>
