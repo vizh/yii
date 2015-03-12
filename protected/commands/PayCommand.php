23b8f7b2aca0d02a1b143e89aeccf71267af3f2b
Korotov Andrey <andrey.korotov@yandex.ru>
2015-03-12 12:33:51 +0300

    Удаление просроченных бронирований для РИФ 15

--- protected/commands/PayCommand.php
+++ protected/commands/PayCommand.php
@@ -3,39 +3,74 @@ use application\components\console\BaseConsoleCommand;
 use mail\components\mailers\MandrillMailer;
 use pay\models\Order;
 use pay\models\OrderItem;
+use pay\models\OrderType;
 
 class PayCommand extends BaseConsoleCommand
-{   
-  public function actionJuridicalOrderNotPaidNotify()
-  {
-    $date = date('Y-m-d', time() - (4*24*60*60));
-    $criteria = new \CDbCriteria();
-    $criteria->addCondition('"t"."CreationTime" >= :MinTime AND "t"."CreationTime" <= :MaxTime');
-    $criteria->params['MinTime'] = $date.' 00:00:00';
-    $criteria->params['MaxTime'] = $date.' 23:59:59';
-
-    $orders = Order::model()->byBankTransfer(true)->byDeleted(false)->byPaid(false)->findAll($criteria);
-    $mailer = new MandrillMailer();
-    foreach ($orders as $order)
+{
+    public function actionJuridicalOrderNotPaidNotify()
     {
-      $language = $order->Payer->Language != null ? $order->Payer->Language : 'ru';
-      Yii::app()->setLanguage($language);
-      $class = \Yii::getExistClass('\pay\components\handlers\orderjuridical\notify\notpaid', ucfirst($order->Event->IdName), 'Base'); 
-      $mail = new $class($mailer, $order);
-      $mail->send();
-    }
-    return 0;
-  }
+        $date = date('Y-m-d', time() - (4*24*60*60));
+        $criteria = new \CDbCriteria();
+        $criteria->addCondition('"t"."CreationTime" >= :MinTime AND "t"."CreationTime" <= :MaxTime');
+        $criteria->params['MinTime'] = $date.' 00:00:00';
+        $criteria->params['MaxTime'] = $date.' 23:59:59';
 
-  public function actionClearPhysicalBook()
-  {
-    $orderItems = OrderItem::model()->byEventId(\Yii::app()->params['AdminBookingEventId'])->byPaid(false)->byBooked(false)->byDeleted(false)->findAll();
-
-    foreach ($orderItems as $item)
-    {
-      $item->delete();
+        $orders = Order::model()->byBankTransfer(true)->byDeleted(false)->byPaid(false)->findAll($criteria);
+        $mailer = new MandrillMailer();
+        foreach ($orders as $order)
+        {
+            $language = $order->Payer->Language != null ? $order->Payer->Language : 'ru';
+            Yii::app()->setLanguage($language);
+            $class = \Yii::getExistClass('\pay\components\handlers\orderjuridical\notify\notpaid', ucfirst($order->Event->IdName), 'Base');
+            $mail = new $class($mailer, $order);
+            $mail->send();
+        }
+        return 0;
     }
 
-    return 0;
-  }
+    public function actionClearPhysicalBook()
+    {
+        $orderItems = OrderItem::model()->byEventId(\Yii::app()->params['AdminBookingEventId'])->byPaid(false)->byBooked(false)->byDeleted(false)->findAll();
+
+        foreach ($orderItems as $item)
+        {
+            $item->delete();
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * Удаление забронированных номеров для рифа15, если это физический счет - заказ удаляется, если счет юр. - удаляется счет или заказ из счета
+     * @throws CDbException
+     */
+    public function actionClearRif15Book()
+    {
+        $orderItems = OrderItem::model()->byEventId(\Yii::app()->params['AdminBookingEventId'])->byPaid(false)->byBooked(false)->byDeleted(false)->findAll();
+        foreach ($orderItems as $orderItem) {
+            $orderLinks = $orderItem->OrderLinks(['with' => ['Order']]);
+            /** @var \pay\models\OrderLinkOrderItem $orderLink */
+            foreach ($orderLinks as $orderLink) {
+                $order = $orderLink->Order;
+                if (OrderType::getIsLong($order->Type) && !$order->Deleted) {
+                    $count = 0;
+                    foreach ($order->ItemLinks as $itemLink) {
+                        if ($itemLink->OrderItem->Id != $orderItem->Id && !$itemLink->OrderItem->Paid && !$itemLink->OrderItem->Deleted) {
+                            $count++;
+                        }
+                    }
+
+                    if ($count === 0) {
+                        $order->delete();
+                    } else {
+                        $orderLink->delete();
+                    }
+                }
+            }
+            $orderItem->deleteHard();
+        }
+
+
+    }
 }
