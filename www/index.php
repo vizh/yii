1ac94ac37ec64325930b4d88e261aa9b5d088a51
Максим Махнюк <star.absorber@gmail.com>
2015-03-20 14:15:54 +0500

    Рефакторинг определения режима боевой / отладка.

--- www/index.php
+++ www/index.php
@@ -1,8 +1,6 @@
 <?php
 // отключаем отладку
-$debug = $_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '82.142.129.35' || $_SERVER['REMOTE_ADDR'] == '178.140.224.229' || $_SERVER['REMOTE_ADDR'] == '10.10.4.1' || $_SERVER['REMOTE_ADDR'] == '::1';
-
-define('YII_DEBUG', $debug);
+define('YII_DEBUG', in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '82.142.129.35', '178.140.224.229', '10.10.4.1', '::1']));
 define('YII_TRACE_LEVEL',3);
 
 $yii=dirname(__FILE__).'/../protected/Yii.php';
