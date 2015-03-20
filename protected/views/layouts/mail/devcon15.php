f3a34f441eaad27273a42249694018d110033217
Dmitry Arnautov <arnautov.dima@gmail.com>
2015-03-20 15:00:15 +0300

    Исправления в рассылках для MS

--- protected/views/layouts/mail/devcon15.php
+++ protected/views/layouts/mail/devcon15.php
@@ -1,51 +1,53 @@
-<?$this->beginContent('/layouts/mail');?>
+<? $this->beginContent('/layouts/mail'); ?>
 
-<table style="width: 100%;">
-    <tr>
-        <td>&nbsp;</td>
-        <td width="700">
+    <table style="width: 100%;">
+        <tr>
+            <td>&nbsp;</td>
+            <td width="700">
 
-            <table cellpadding="0" cellspacing="0" border="0" width="700" align="left" style="border: 1px solid #efefef; font-family:Segoe UI,Tahoma,Arial,Helvetica,Sans-Serif; font-size:13px;">
-                <tr>
-                    <td height="140">
-                        <a href="http://www.msdevcon.ru/"><img src="http://runet-id.com/img/mail/2015/devcon2015-700x140.jpg" /></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <table cellpadding="10" cellspacing="10" border="0" width="100%">
-                            <tr>
-                                <td style="font-size: 13px;">
+                <table cellpadding="0" cellspacing="0" border="0" width="700" align="left"
+                       style="border: 1px solid #efefef; font-family:Segoe UI,Tahoma,Arial,Helvetica,Sans-Serif; font-size:14px;">
+                    <tr>
+                        <td height="140">
+                            <a href="http://www.msdevcon.ru/"><img
+                                    src="http://runet-id.com/img/mail/2015/devcon2015-700x140.jpg"/></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table cellpadding="10" cellspacing="10" border="0" width="100%">
+                                <tr>
+                                    <td style="font-size: 14px;">
 
-                                    <?=$content;?>
+                                        <?= $content; ?>
 
-                                    <br/><br/>
+                                        <br/><br/>
 
-                                    <p style="font-family: Segoe UI, Tahoma, Helvetica, sans-serif; font-size: 14px;">
-                                        С уважением,<br/>
-                                        организаторы конференции DevCon 2015<br/>
-                                        _______________________________________<br/>
-                                        <a href="http://www.msdevcon.ru">www.msdevcon.ru</a><br/>
-                                        <a href="mailto:ms@devcon2015.ru">ms@devcon2015.ru</a><br/>
-                                        +7 (929) 694-00-99<br/>
-                                        Режим работы: будни 9:00 – 18:00<br/>
-                                        #msdevcon
-                                    </p>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                <tr>
-                    <td height="80">
-                        <img src="http://runet-id.com/img/event/devcon14/email-footer.gif" />
-                    </td>
-                </tr>
-            </table>
+                                        <p style="font-family: Segoe UI, Tahoma,Arial,Helvetica, sans-serif; font-size: 14px;">
+                                            С уважением,<br/>
+                                            организаторы конференции DevCon 2015<br/>
+                                            _______________________________________<br/>
+                                            <a href="http://www.msdevcon.ru">www.msdevcon.ru</a><br/>
+                                            <a href="mailto:ms@devcon2015.ru">ms@devcon2015.ru</a><br/>
+                                            +7 (929) 694-00-99<br/>
+                                            Режим работы: будни 9:00 – 18:00<br/>
+                                            #msdevcon
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td height="80">
+                            <img src="http://runet-id.com/img/event/devcon14/email-footer.gif"/>
+                        </td>
+                    </tr>
+                </table>
 
-        </td>
-        <td>&nbsp;</td>
-    </tr>
-</table>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
 
-<?=$this->endContent();?>
+<?= $this->endContent(); ?>
--- protected/views/layouts/mail/msdevtour.php
+++ protected/views/layouts/mail/msdevtour.php
@@ -1,43 +1,48 @@
-<?$this->beginContent('/layouts/mail');?>
+<? $this->beginContent('/layouts/mail'); ?>
 
-<table style="width: 100%;">
-    <tr>
-        <td>&nbsp;</td>
-        <td width="700">
+    <table style="width: 100%;">
+        <tr>
+            <td>&nbsp;</td>
+            <td width="700">
 
-            <table cellpadding="0" cellspacing="0" border="0" width="700" align="left" style="border: 1px solid #efefef; font-family:Segoe UI,Tahoma,Arial,Helvetica,Sans-Serif; font-size:13px;">
-                <tr>
-                    <td height="140">
-                        <a href="http://events.techdays.ru/msdevtour/"><img src="http://runet-id.com/img/mail/2015/msdevtour.png" /></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <table cellpadding="10" cellspacing="10" border="0" width="100%">
-                            <tr>
-                                <td style="font-size: 13px;">
+                <table cellpadding="0" cellspacing="0" border="0" width="700" align="left"
+                       style="border: 1px solid #efefef; font-family:Segoe UI,Tahoma,Arial,Helvetica,Sans-Serif; font-size:14px;">
+                    <tr>
+                        <td height="140">
+                            <a href="http://events.techdays.ru/msdevtour/"><img width="700"
+                                                                                src="http://runet-id.com/img/mail/2015/msdevtour.png"/></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table cellpadding="10" cellspacing="10" border="0" width="100%">
+                                <tr>
+                                    <td style="font-size: 14px;">
 
-                                    <?=$content;?>
+                                        <?= $content; ?>
 
-                                    <br/><br/>
-                                    <a href="#" style="color: #C8F687;text-decoration: none;user-select: none;background: #81BB2A;padding: .7em 1.5em;outline: none; ">Регистрация</a>
-                                    <br/>
-                                    <p style="font-family: Segoe UI, Tahoma, Helvetica, sans-serif; font-size: 14px;">
-                                        До встречи на мероприятии!<br/>
-                                        <br/>
-                                        С уважением,<br/>
-                                        Команда Microsoft Developer Tour<br/>
-                                    </p>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
+                                        <br/><br/>
 
-        </td>
-        <td>&nbsp;</td>
-    </tr>
-</table>
+                                        <p style="font-family: Segoe UI, Tahoma,Arial, Helvetica, sans-serif; font-size: 14px;">
+                                            <br/>
+                                            С уважением,<br/>
+                                            Команда организаторов конференции Microsoft Developer Tour<br/>
+                                            ----------------------<br/>
+                                            <a href="http://events.techdays.ru/msdevtour/">msdevtour.ru</a>
+                                            <br/>
+                                            <a href="https://twitter.com/search?f=realtime&q=%23msdevtour%20&src=typd">#msdevtour</a>
+                                            <br/>
+                                        </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
 
-<?=$this->endContent();?>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+
+<?= $this->endContent(); ?>
