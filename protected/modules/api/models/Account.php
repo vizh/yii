ea6f7e89494cb43e3462c1617af71a89eee23904
Korotov Andrey <andrey.korotov@yandex.ru>
2015-05-06 16:16:32 +0300

    Роль API без доступа к контактам пользователя

--- protected/modules/api/models/Account.php
+++ protected/modules/api/models/Account.php
@@ -19,132 +19,138 @@ namespace api\models;
  */
 class Account extends \CActiveRecord
 {
+    const ROLE_OWN = 'own';
+    const ROLE_PARTNER = 'partner';
+    const ROLE_MICROSOFT = 'microsoft';
+    const ROLE_MBLT = 'mblt';
+    const ROLE_MOBILE = 'mobile';
+    const ROLE_PARTNER_WOC = 'partner_woc';
 
     const SelfId = 1;
 
-  /**
-   * @param string $className
-   *
-   * @return Account
-   */
-  public static function model($className=__CLASS__)
-  {
-    return parent::model($className);
-  }
-
-  public function tableName()
-  {
-    return 'ApiAccount';
-  }
-
-  public function primaryKey()
-  {
-    return 'Id';
-  }
-
-  public function relations()
-  {
-    return array(
-      'Domains' => array(self::HAS_MANY, '\api\models\Domain', 'AccountId'),
-      'Ips' => array(self::HAS_MANY, '\api\models\Ip', 'AccountId'),
-      'Event' => [self::BELONGS_TO, '\event\models\Event', 'EventId']
-    );
-  }
-
-  /**
-   * @param string $key
-   * @param bool $useAnd
-   *
-   * @return Account
-   */
-  public function byKey($key, $useAnd = true)
-  {
-    $criteria = new \CDbCriteria();
-    $criteria->condition = '"t"."Key" = :Key';
-    $criteria->params = [':Key' => $key];
-    $this->getDbCriteria()->mergeWith($criteria, $useAnd);
-    return $this;
-  }
-
-  public function byEventId($eventId, $useAnd = true)
-  {
-    $criteria = new \CDbCriteria();
-    $criteria->condition = '"t"."EventId" = :EventId';
-    $criteria->params = ['EventId' => $eventId];
-    $this->getDbCriteria()->mergeWith($criteria, $useAnd);
-    return $this;
-  }
-
-  protected $_dataBuilder = null;
-
-  /**
-   * @return \api\components\builders\Builder
-   */
-  public function getDataBuilder()
-  {
-    if ($this->_dataBuilder === null)
+    /**
+     * @param string $className
+     *
+     * @return Account
+     */
+    public static function model($className=__CLASS__)
     {
-      $version = \Yii::app()->getRequest()->getParam('v', null);
-      $timestamp = strtotime($version);
-      if ($timestamp === false)
-      {
-        $this->_dataBuilder = new \api\components\builders\Builder($this);
-      }
-      else
-      {
-        $this->_dataBuilder = $this->getVersioningBuilder($timestamp);
-      }
+        return parent::model($className);
     }
 
-    return $this->_dataBuilder;
-  }
-
-  /**
-   * @param int $timestamp
-   * @return \api\components\builders\Builder
-   */
-  protected function getVersioningBuilder($timestamp)
-  {
-    return new \api\components\builders\Builder($this);
-  }
-
-  public function checkIp($ip)
-  {
-    //todo: fix it
-    return true;
-  }
-
-  /**
-   * @param string $hash
-   * @param int $timestamp
-   * @return bool
-   */
-  public function checkHash($hash, $timestamp)
-  {
-    $checkHash = $this->getHash($timestamp);
-    if ($hash === $checkHash || strstr($hash, $checkHash) !== false)
+    public function tableName()
     {
-      return true;
+        return 'ApiAccount';
     }
-    return false;
-  }
 
-  public function checkReferer($referer, $hash)
-  {
-    if ($hash !== $this->getRefererHash($referer))
+    public function primaryKey()
     {
-      return false;
+        return 'Id';
     }
-    foreach ($this->Domains as $domain)
+
+    public function relations()
     {
-      $pattern = '/^' . $domain->Domain . '$/i';
-      if (preg_match($pattern, $referer) === 1)
-      {
+        return array(
+            'Domains' => array(self::HAS_MANY, '\api\models\Domain', 'AccountId'),
+            'Ips' => array(self::HAS_MANY, '\api\models\Ip', 'AccountId'),
+            'Event' => [self::BELONGS_TO, '\event\models\Event', 'EventId']
+        );
+    }
+
+    /**
+     * @param string $key
+     * @param bool $useAnd
+     *
+     * @return Account
+     */
+    public function byKey($key, $useAnd = true)
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->condition = '"t"."Key" = :Key';
+        $criteria->params = [':Key' => $key];
+        $this->getDbCriteria()->mergeWith($criteria, $useAnd);
+        return $this;
+    }
+
+    public function byEventId($eventId, $useAnd = true)
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->condition = '"t"."EventId" = :EventId';
+        $criteria->params = ['EventId' => $eventId];
+        $this->getDbCriteria()->mergeWith($criteria, $useAnd);
+        return $this;
+    }
+
+    protected $_dataBuilder = null;
+
+    /**
+     * @return \api\components\builders\Builder
+     */
+    public function getDataBuilder()
+    {
+        if ($this->_dataBuilder === null)
+        {
+            $version = \Yii::app()->getRequest()->getParam('v', null);
+            $timestamp = strtotime($version);
+            if ($timestamp === false)
+            {
+                $this->_dataBuilder = new \api\components\builders\Builder($this);
+            }
+            else
+            {
+                $this->_dataBuilder = $this->getVersioningBuilder($timestamp);
+            }
+        }
+
+        return $this->_dataBuilder;
+    }
+
+    /**
+     * @param int $timestamp
+     * @return \api\components\builders\Builder
+     */
+    protected function getVersioningBuilder($timestamp)
+    {
+        return new \api\components\builders\Builder($this);
+    }
+
+    public function checkIp($ip)
+    {
+        //todo: fix it
         return true;
-      }
     }
-    return false;
-  }
+
+    /**
+     * @param string $hash
+     * @param int $timestamp
+     * @return bool
+     */
+    public function checkHash($hash, $timestamp)
+    {
+        $checkHash = $this->getHash($timestamp);
+        if ($hash === $checkHash || strstr($hash, $checkHash) !== false)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public function checkReferer($referer, $hash)
+    {
+        if ($hash !== $this->getRefererHash($referer))
+        {
+            return false;
+        }
+        foreach ($this->Domains as $domain)
+        {
+            $pattern = '/^' . $domain->Domain . '$/i';
+            if (preg_match($pattern, $referer) === 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     /**
      * @param string $url
@@ -184,24 +190,40 @@ class Account extends \CActiveRecord
         return false;
     }
 
-  public function getRefererHash($referer)
-  {
-    return substr(md5($this->Key . $referer . $this->Secret . 'nPOg9ODiyos4HJIYS9FGGJ7qw'), 0, 16);
-  }
+    public function getRefererHash($referer)
+    {
+        return substr(md5($this->Key . $referer . $this->Secret . 'nPOg9ODiyos4HJIYS9FGGJ7qw'), 0, 16);
+    }
 
-  /**
-   * @param int $timestamp
-   * @return string
-   */
-  private function getHash($timestamp)
-  {
-    if ($timestamp === null)
+    /**
+     * @param int $timestamp
+     * @return string
+     */
+    private function getHash($timestamp)
     {
-      return md5($this->Key . $this->Secret);
+        if ($timestamp === null)
+        {
+            return md5($this->Key . $this->Secret);
+        }
+        else
+        {
+            return substr(md5($this->Key . $timestamp . $this->Secret), 0, 16);
+        }
     }
-    else
+
+    /**
+     * Лейблы для типов ролей
+     * @return array
+     */
+    public static function getRoleLabels()
     {
-      return substr(md5($this->Key . $timestamp . $this->Secret), 0, 16);
+        return [
+            self::ROLE_OWN => 'Собственное мероприятие',
+            self::ROLE_PARTNER => 'Партнерское мероприятие',
+            self::ROLE_PARTNER_WOC => 'Партнерское мероприятие без доступа к контактной информации',
+            self::ROLE_MICROSOFT => 'Мероприятие Microsoft',
+            self::ROLE_MBLT => 'Мероприятие MBLT',
+            self::ROLE_MOBILE => 'Мобильное приложение',
+        ];
     }
-  }
 }
