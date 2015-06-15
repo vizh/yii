26cef9fc2468090fec8fc0d8bb7ffba4b09ec701
Fedor B Gorsky <fedor@support-pc.org>
2015-06-15 18:19:06 +0300

    IssueId #2122 : Добавлена капча при регистрации

--- protected/modules/oauth/components/form/RegisterForm.php
+++ protected/modules/oauth/components/form/RegisterForm.php
@@ -1,100 +1,134 @@
 <?php
 namespace oauth\components\form;
 
+use api\models\Account;
+use contact\models\forms\Address;
+use user\models\User;
+
 class RegisterForm extends \CFormModel
 {
-  private $account = null;
+    private $account = null;
 
-  public $LastName;
-  public $FirstName;
-  public $FatherName;
-  public $Email;
-  public $CompanyId;
-  public $Company;
-  public $Address;
-  public $Phone;
+    public $LastName;
+    public $FirstName;
+    public $FatherName;
+    public $Email;
+    public $CompanyId;
+    public $Company;
+    public $Address;
+    public $Phone;
 
-  public function __construct(\api\models\Account $account)
-  {
-    parent::__construct('');
-    $this->account = $account;
-    $this->Address = new \contact\models\forms\Address();
-  }
+    public $RecaptchSecret = '6LerUwgTAAAAANSAzVD3IlpODDgpaH9NgN6gl46h';
 
-
-  public function attributeLabels()
-  {
-    return array(
-      'LastName' => \Yii::t('app', 'Фамилия'),
-      'FirstName' => \Yii::t('app', 'Имя'),
-      'FatherName' => \Yii::t('app', 'Отчество'),
-      'Email' => \Yii::t('app', 'Электронная почта'),
-      'Company' => \Yii::t('app', 'Компания'),
-      'City' => \Yii::t('app', 'Город'),
-      'Phone' => \Yii::t('app', 'Номер телефона')
-    );
-  }
-
-  public function afterValidate()
-  {
-    $this->Address->attributes = \Yii::app()->getRequest()->getParam(get_class($this->Address));
-    if (!$this->Address->validate())
+    public function __construct(Account $account)
     {
-      foreach ($this->Address->getErrors() as $messages)
-      {
-        $this->addError('Address', $messages[0]);
-      }
+        parent::__construct('');
+        $this->account = $account;
+        $this->Address = new Address();
     }
-  }
 
-  public function rules()
-  {
-    return [
-      ['Phone', 'filter', 'filter' => '\application\components\utility\Texts::getOnlyNumbers'],
-      ['FirstName, LastName, Email, Phone', 'required'],
-      ['Email', 'email'],
-      ['Email', 'uniqueEmailValidate'],
-      ['FatherName, CompanyId, Company', 'safe'],
-      ['Phone', 'unique', 'className' => '\user\models\User', 'attributeName' => 'PrimaryPhone', 'criteria' => ['condition' => '"t"."Visible"']]
-    ];
-  }
-  
-  public function uniqueEmailValidate($attribute, $params)
-  {
-    $value = trim($this->$attribute);
-    if (!empty($value))
+    /**
+     * @return array
+     */
+    public function attributeLabels()
     {
-      if (!\user\models\User::model()->byEmail($value)->byVisible(true)->exists())
-      {
-        return true;
-      }
-      $this->addError($attribute, 'Пользователь с таким Email уже существует.');
+        return array(
+            'LastName' => \Yii::t('app', 'Фамилия'),
+            'FirstName' => \Yii::t('app', 'Имя'),
+            'FatherName' => \Yii::t('app', 'Отчество'),
+            'Email' => \Yii::t('app', 'Электронная почта'),
+            'Company' => \Yii::t('app', 'Компания'),
+            'City' => \Yii::t('app', 'Город'),
+            'Phone' => \Yii::t('app', 'Номер телефона')
+        );
     }
-    return false;
-  }
-  
-  protected function beforeValidate()
-  {
-    $purifier = new \CHtmlPurifier();
-    $purifier->options = array(
-      'HTML.AllowedElements' => array()  
-    );
-    $attributes = $this->attributes;
-    foreach ($this->attributes as $field => $value)
-    {
-      if ($field == 'Address' || $field == 'Phone')
-        continue;
-      $attributes[$field] = $purifier->purify($value);
-    }
-    $this->attributes = $attributes;
-    return parent::beforeValidate();
-  }
 
-  /**
-   * @return \api\models\Account|null
-   */
-  public function getAccount()
-  {
-    return $this->account;
-  }
+    public function afterValidate()
+    {
+        $this->Address->attributes = \Yii::app()->getRequest()->getParam(get_class($this->Address));
+        if (!$this->Address->validate()) {
+            foreach ($this->Address->getErrors() as $messages) {
+                $this->addError('Address', $messages[0]);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            ['Phone', 'filter', 'filter' => '\application\components\utility\Texts::getOnlyNumbers'],
+            ['FirstName, LastName, Email, Phone', 'required'],
+            ['Email', 'email'],
+            ['Email', 'uniqueEmailValidate'],
+            ['FatherName, CompanyId, Company', 'safe'],
+            ['Phone', 'unique', 'className' => '\user\models\User', 'attributeName' => 'PrimaryPhone', 'criteria' => ['condition' => '"t"."Visible"']]
+        ];
+    }
+
+    public function uniqueEmailValidate($attribute, $params)
+    {
+        $value = trim($this->$attribute);
+        if (!empty($value)) {
+            if (!User::model()->byEmail($value)->byVisible(true)->exists()) {
+                return true;
+            }
+            $this->addError($attribute, 'Пользователь с таким Email уже существует.');
+        }
+        return false;
+    }
+
+    protected function beforeValidate()
+    {
+        $response = \Yii::app()->request->getPost('g-recaptcha-response');
+        if ($response == ''){
+            $this->addError('Captcha', 'Поставьте галочку "Я не робот"');
+        } else {
+            $postdata = http_build_query(
+                [
+                    'secret' => $this->RecaptchSecret,
+                    'response' => $response
+                ]
+            );
+            $opts = ['http' =>
+                [
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/x-www-form-urlencoded',
+                    'content' => $postdata
+                ]
+            ];
+            $context = stream_context_create($opts);
+            $captchaResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+            if (!$captchaResponse) {
+                $this->addError('Captcha', 'Нет связи с сервером капчи');
+            } else {
+                $result = json_decode($captchaResponse);
+                if (!$result->success) {
+                    $this->addError('Captcha', 'Поставьте галочку "Я не робот"');
+                }
+            }
+        }
+        $purifier = new \CHtmlPurifier();
+        $purifier->options = array(
+            'HTML.AllowedElements' => array()
+        );
+        $attributes = $this->attributes;
+        foreach ($this->attributes as $field => $value) {
+            if ($field == 'Address' || $field == 'Phone')
+                continue;
+            $attributes[$field] = $purifier->purify($value);
+        }
+        $this->attributes = $attributes;
+        return parent::beforeValidate();
+    }
+
+    /**
+     * @return \api\models\Account|null
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
 }
--- protected/modules/oauth/views/main/register.php
+++ protected/modules/oauth/views/main/register.php
@@ -6,45 +6,76 @@
 ?>
 
 <?=CHtml::beginForm([], 'post', ['id'=>'authForm']);?>
-  <fieldset>
+<fieldset>
     <legend><?=Yii::t('app', 'Регистрация');?></legend>
 
     <?=CHtml::errorSummary($model, '<div class="alert alert-danger">', '</div>');?>
     <?if ($socialProxy !== null && $socialProxy->isHasAccess()):?>
-      <div class="alert alert-warning">
-        Не найдена связь с аккаунтом социальной сети <strong><?=$socialProxy->getSocialTitle();?></strong>. Она будет добавлена после регистрации или <a href="<?=$this->createUrl('/oauth/main/auth');?>">авторизации</a> в RUNET-ID.
-      </div>
+        <div class="alert alert-warning">
+            Не найдена связь с аккаунтом социальной сети <strong><?=$socialProxy->getSocialTitle();?></strong>. Она будет добавлена после регистрации или <a href="<?=$this->createUrl('/oauth/main/auth');?>">авторизации</a> в RUNET-ID.
+        </div>
     <?endif;?>
-
     <p><?=Yii::t('app', 'Вы&nbsp;можете одновременно получить RUNET-ID и&nbsp;зарегистрироваться на&nbsp;мероприятие, заполнив форму:');?></p>
-    <div class="form-group <?=$model->hasErrors('LastName') ? 'error' : '';?>">
-      <?=CHtml::activeTextField($model, 'LastName', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('LastName')));?>
+
+
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group <?=$model->hasErrors('LastName') ? 'error' : '';?>">
+                <?=CHtml::activeTextField($model, 'LastName', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('LastName')));?>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group <?=$model->hasErrors('FirstName') ? 'error' : '';?>">
+                <?=CHtml::activeTextField($model, 'FirstName', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('FirstName')));?>
+            </div>
+        </div>
     </div>
-    <div class="form-group <?=$model->hasErrors('FirstName') ? 'error' : '';?>">
-      <?=CHtml::activeTextField($model, 'FirstName', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('FirstName')));?>
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group">
+                <?=CHtml::activeTextField($model, 'FatherName', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('FatherName')));?>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group">
+                <p class="form-text">Отчество - не обязательное поле</p>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-      <?=CHtml::activeTextField($model, 'FatherName', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('FatherName')));?>
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group <?=$model->hasErrors('Email') ? 'error' : '';?>">
+                <?=CHtml::activeTextField($model, 'Email', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('Email')));?>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group <?=$model->hasErrors('Phone') ? 'error' : '';?>">
+                <?=CHtml::activeTextField($model, 'Phone', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('Phone')));?>
+            </div>
+        </div>
     </div>
-    <div class="form-group <?=$model->hasErrors('Email') ? 'error' : '';?>">
-      <?=CHtml::activeTextField($model, 'Email', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('Email')));?>
-    </div>
-    <div class="form-group">
-      <?=CHtml::activeTextField($model, 'Company', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('Company')));?>
-    </div>
-    <div class="form-group <?if ($model->Address->hasErrors()):?>error<?endif;?>">
-      <?$this->widget('\contact\widgets\AddressControls', array('form' => $model->Address, 'address' => false, 'place' => false, 'inputClass' => 'form-control'));?>
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group">
+                <?=CHtml::activeTextField($model, 'Company', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('Company')));?>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="form-group <?if ($model->Address->hasErrors()):?>error<?endif;?>">
+                <?$this->widget('\contact\widgets\AddressControls', array('form' => $model->Address, 'address' => false, 'place' => false, 'inputClass' => 'form-control'));?>
+            </div>
+        </div>
     </div>
 
-    <div class="form-group <?=$model->hasErrors('Phone') ? 'error' : '';?>">
-      <?=CHtml::activeTextField($model, 'Phone', array('class' => 'form-control', 'placeholder' => $model->getAttributeLabel('Phone')));?>
+    <div class="form-group <?=$model->hasErrors('Captcha') ? 'error' : '';?>">
+        <div class="g-recaptcha" data-sitekey="6LerUwgTAAAAALjQMLIb1H9zUKHVIGYuK3af5QHj"></div>
     </div>
 
     <p class="muted agreement">Нажимая кнопку «<?=Yii::t('app', 'Зарегистрироваться');?>», я принимаю условия <a target="_blank"
-            href="<?=Yii::app()->createUrl('/page/info/agreement');?>">Пользовательского соглашения</a> и даю своё согласие RUNET-ID на обработку моих персональных данных, в соответствии с Федеральным законом от 27.07.2006 года №152-ФЗ «О персональных данных»</p>
+                                                                                                                 href="<?=Yii::app()->createUrl('/page/info/agreement');?>">Пользовательского соглашения</a> и даю своё согласие RUNET-ID на обработку моих персональных данных, в соответствии с Федеральным законом от 27.07.2006 года №152-ФЗ «О персональных данных»</p>
 
     <button type="submit" class="btn btn-large btn-block btn-primary"><i class="icon-ok-sign icon-white"></i>&nbsp;<?=Yii::t('app', 'Зарегистрироваться');?></button>
-  </fieldset>
+</fieldset>
 <?=CHtml::endForm();?>
 <hr>
 <p class="text-center"><?=\Yii::t('app', 'Если вы&nbsp;уже получали RUNET-ID&nbsp;&mdash; <a href="{url}">авторизуйтесь</a>.', array('{url}' => $this->createUrl('/oauth/main/auth')));?></p>
