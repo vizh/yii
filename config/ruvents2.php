22872ef85e6f30c0294bc88ddd7595b04b9d3d3b
Максим Махнюк <star.absorber@gmail.com>
2015-08-11 03:46:59 +0500

    Api / Выровнен массив правил маршрутизации для наглядности. Уменьшено кол-во конкатенаций при построении правил.

--- config/ruvents2.php
+++ config/ruvents2.php
@@ -1,4 +1,7 @@
 <?php
+
+define('RUVENTS_API_HOST', 'http://ruvents2.'.RUNETID_HOST);
+
 return [
     'modules' => ['ruvents2'],
     'components' => [
@@ -8,54 +11,28 @@ return [
         ],
         'urlManager' => [
             'rules' => [
-                ['ruvents2/event/index', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/event', 'verb' => 'GET'],
-                ['ruvents2/event/<action>', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/event/<action:\w+>', 'verb' => 'GET'],
-
-                ['ruvents2/participants/fields', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/participants/fields', 'verb' => 'GET'],
-                ['ruvents2/participants/list', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/participants', 'verb' => 'GET'],
-                ['ruvents2/participants/create', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/participants', 'verb' => 'POST'],
-                ['ruvents2/participants/edit', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/participants/<runetId:\d+>', 'verb' => 'PUT'],
-                ['ruvents2/participants/delete', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/participants/<runetId:\d+>', 'verb' => 'DELETE'],
-
-                ['ruvents2/users/list', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/users', 'verb' => 'GET'],
-
-                ['ruvents2/badges/list', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/badges', 'verb' => 'GET'],
-                ['ruvents2/badges/create', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/badges', 'verb' => 'POST'],
-
-                ['ruvents2/positions/list', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/positions', 'verb' => 'GET'],
-
-
-                ['ruvents2/products/checks', 'pattern' => 'http://ruvents2.' . RUNETID_HOST . '/products/<id:\d+>/checks', 'verb' => 'GET'],
-                ['ruvents2/products/check', 'pattern' => 'http://ruvents2.' . RUNETID_HOST . '/products/<id:\d+>/checks', 'verb' => 'POST'],
-
-                ['ruvents2/halls/checks', 'pattern' => 'http://ruvents2.' . RUNETID_HOST . '/halls/<id:\d+>/checks', 'verb' => 'GET'],
-                ['ruvents2/halls/check', 'pattern' => 'http://ruvents2.' . RUNETID_HOST . '/halls/<id:\d+>/checks', 'verb' => 'POST'],
-
-                ['ruvents2/utility/ping', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/utility/ping', 'verb' => 'GET'],
-
+                ['ruvents2/event/index',           'pattern' => RUVENTS_API_HOST.'/event',                      'verb' => 'GET'   ],
+                ['ruvents2/event/<action>',        'pattern' => RUVENTS_API_HOST.'/event/<action:\w+>',         'verb' => 'GET'   ],
+                ['ruvents2/participants/fields',   'pattern' => RUVENTS_API_HOST.'/participants/fields',        'verb' => 'GET'   ],
+                ['ruvents2/participants/list',     'pattern' => RUVENTS_API_HOST.'/participants',               'verb' => 'GET'   ],
+                ['ruvents2/participants/register', 'pattern' => RUVENTS_API_HOST.'/participants',               'verb' => 'PUT'   ],
+                ['ruvents2/participants/create',   'pattern' => RUVENTS_API_HOST.'/participants',               'verb' => 'POST'  ],
+                ['ruvents2/participants/edit',     'pattern' => RUVENTS_API_HOST.'/participants/<runetId:\d+>', 'verb' => 'PUT'   ],
+                ['ruvents2/participants/delete',   'pattern' => RUVENTS_API_HOST.'/participants/<runetId:\d+>', 'verb' => 'DELETE'],
+                ['ruvents2/users/list',            'pattern' => RUVENTS_API_HOST.'/users',                      'verb' => 'GET'   ],
+                ['ruvents2/badges/list',           'pattern' => RUVENTS_API_HOST.'/badges',                     'verb' => 'GET'   ],
+                ['ruvents2/badges/create',         'pattern' => RUVENTS_API_HOST.'/badges',                     'verb' => 'POST'  ],
+                ['ruvents2/positions/list',        'pattern' => RUVENTS_API_HOST.'/positions',                  'verb' => 'GET'   ],
+                ['ruvents2/products/checks',       'pattern' => RUVENTS_API_HOST.'/products/<id:\d+>/checks',   'verb' => 'GET'   ],
+                ['ruvents2/products/check',        'pattern' => RUVENTS_API_HOST.'/products/<id:\d+>/checks',   'verb' => 'POST'  ],
+                ['ruvents2/halls/checks',          'pattern' => RUVENTS_API_HOST.'/halls/<id:\d+>/checks',      'verb' => 'GET'   ],
+                ['ruvents2/halls/check',           'pattern' => RUVENTS_API_HOST.'/halls/<id:\d+>/checks',      'verb' => 'POST'  ],
+                ['ruvents2/utility/ping',          'pattern' => RUVENTS_API_HOST.'/utility/ping',               'verb' => 'GET'   ],
                 /* Хак, для большого удобства работы клиента */
-                ['ruvents2/participants/edit', 'pattern' => 'http://ruvents2.'. RUNETID_HOST.'/participants', 'verb' => 'PUT'],
 
-                'http://ruvents2.'. RUNETID_HOST.'/<controller:\w+>/' => 'ruvents2/not/found',
-                'http://ruvents2.'. RUNETID_HOST.'/<controller:\w+>/<action:\w+>' => 'ruvents2/not/found',
+                RUVENTS_API_HOST.'/<controller:\w+>/' => 'ruvents2/not/found',
+                RUVENTS_API_HOST.'/<controller:\w+>/<action:\w+>' => 'ruvents2/not/found',
             ]
         ]
     ]
 ];
-
-/*
- * Routes example
- *
-
-    array('api/<controller>/index', 'pattern'=>'api/<controller:\w+>', 'verb'=>'GET'),
-    array('api/<controller>/create', 'pattern'=>'api/<controller:\w+>', 'verb'=>'POST'),
-    array('api/<controller>/view', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'GET'),
-    array('api/<controller>/update', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'PUT, POST'),
-    array('api/<controller>/delete', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'DELETE'),
-
-
-
-
-    'http://ruvents2.'. RUNETID_HOST.'/<controller:\w+>/<action:\w+>' => 'ruvents2/<controller>/<action>',
- *
- */
