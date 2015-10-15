1a0eb00f60b9ce594069dd5e8f23dfa7501f3eda
Maxim Borzov <max.borzov@gmail.com>
2015-10-16 00:40:03 +0300

    Генератор промо-кодов на мероприятия с доступ по приглашению

--- protected/modules/main/controllers/TestController.php
+++ protected/modules/main/controllers/TestController.php
@@ -6,6 +6,31 @@ use pay\models\CouponLinkProduct;
 
 class TestController extends \CController
 {
+
+    public function actionInvites()
+    {
+        $eventId = 1999;
+
+        if ($eventId) {
+
+            for ($i = 0; $i <= 48; $i++) {
+
+                $text = new \application\components\utility\Texts();
+                $code = $text->getUniqString($eventId);
+
+                $invite = new \event\models\Invite();
+                $invite->EventId = $eventId;
+                $invite->RoleId = 167;
+                $invite->Code = $code;
+                $invite->save();
+
+                print $code . '<br>';
+
+            }
+
+        }
+    }
+
     public function actionInfo()
     {
 
