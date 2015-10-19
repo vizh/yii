c9a4353daf5afa5360f23661d8115d07c5532f2e
Korotov Andrey <andrey.korotov@yandex.ru>
2015-10-19 13:22:42 +0300

    Уведомления организаторов для nspk

---
+++ protected/modules/event/components/handlers/register/system/Nspk15.php
@@ -0,0 +1,16 @@
+<?php
+namespace event\components\handlers\register\system;
+
+
+class Nspk15 extends Base
+{
+    public function getFrom()
+    {
+        return 'info@runet-id.com';
+    }
+
+    public function getTo()
+    {
+        return 'orgcenter@nspk.ru';
+    }
+} 
--- protected/modules/event/views/mail/register/system/base.php
+++ protected/modules/event/views/mail/register/system/base.php
@@ -1,16 +1,14 @@
-На мероприятие <?=$event->Title;?> успешно зарегистрирован новый пользователь.
+<p>На мероприятие <?=$event->Title;?> успешно зарегистрирован новый пользователь.</p>
 
-Роль:
-<?=$role->Title;?>
-
-
-Данные пользователя:
-RUNET-ID пользователя: <?=$user->RunetId;?>
-
-Имя: <?=$user->getFullName();?> <?if($user->getEmploymentPrimary() !== null):?>(<?=$user->getEmploymentPrimary()->Company->Name;?>)<?endif;?>
-
-E-Mail: <?=$user->Email;?>
+<p><strong>Роль:</strong><br/>
+<?=$role->Title;?></p>
 
 
---
-Письмо сгенерировано автоматически.
+<p><strong>Данные пользователя:</strong><br/>
+RUNET-ID пользователя: <?=$user->RunetId;?><br/>
+Имя: <?=$user->getFullName();?> <?if($user->getEmploymentPrimary() !== null):?>(<?=$user->getEmploymentPrimary()->Company->Name;?>)<?endif;?><br/>
+E-Mail: <?=$user->Email;?></br>
+</p>
+
+<p>--<br/>
+Письмо сгенерировано автоматически.</p>
