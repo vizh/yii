f40120270ffe80fc4d5d7e1d3b43a293e9eef9c0
Korotov Andrey <andrey.korotov@yandex.ru>
2015-12-09 15:54:43 +0300

    Issue #3273: Члены РАЭК / Редактирование карточки организации

--- protected/modules/company/assets/js/admin/main/edit.js
+++ protected/modules/company/assets/js/admin/main/edit.js
@@ -1,7 +1,7 @@
 $(function () {
     $('input[id*="_JoinTime"]').datepicker();
-    $('input[id*="_AllowVote"]:radio').change(function() {
-        $('input[id*="_AllowVote"]:radio').not(this).prop('checked', false);
+    $('input[id*="_AllowVote"]:checkbox').change(function() {
+        $('input[id*="_AllowVote"]:checkbox').not(this).prop('checked', false);
     });
 
     CKEDITOR.replace('company\\models\\forms\\admin\\Company[Info]', {
