1bb3dd3c8dcfafe9c8eee5877fc3e8b1f457574f
Korotov Andrey <andrey.korotov@yandex.ru>
2015-12-17 13:33:36 +0300

    Closes #3367: API RUVENTS / Механизм отметки посетителей при входе в зоны мониторинга на мероприятиях

--- protected/modules/ruvents/controllers/UserController.php
+++ protected/modules/ruvents/controllers/UserController.php
@@ -2,14 +2,14 @@
 
 class UserController extends \ruvents\components\Controller
 {
-
     public function actions()
     {
         return [
             'create' => 'ruvents\controllers\user\CreateAction',
             'edit' => 'ruvents\controllers\user\EditAction',
             'search' => 'ruvents\controllers\user\SearchAction',
-            'auth' => 'ruvents\controllers\user\AuthAction'
+            'auth' => 'ruvents\controllers\user\AuthAction',
+            'visit' => 'ruvents\controllers\user\VisitAction'
         ];
     }
 }
