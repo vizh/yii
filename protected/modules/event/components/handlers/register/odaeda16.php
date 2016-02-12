7b0cdc14ca43f0959f1c452fd2eb84652effc3d4
Fedor B Gorsky <fedor@support-pc.org>
2016-02-12 13:00:19 +0300

    Odaeda16 Переопределение почтового сообщения

---
+++ protected/modules/event/components/handlers/register/odaeda16.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fgorsky
+ * Date: 15.04.15
+ * Time: 16:02
+ */
+
+namespace event\components\handlers\register;
+
+
+use mail\models\Layout;
+
+class odaeda16 extends Base
+{
+
+
+    protected function getRepeat()
+    {
+        return false;
+    }
+
+    public function getAttachments()
+    {
+        return [];
+    }
+
+    public function getLayoutName()
+    {
+        return Layout::OneColumn;
+    }
+
+    public function getFromName()
+    {
+        return 'Платежная система «Мир»';
+    }
+
+    public function showFooter()
+    {
+        return false;
+    }
+
+    public function showUnsubscribeLink()
+    {
+        return false;
+    }
+
+    public function getFrom()
+    {
+        return 'no-reply@mironline.ru';
+    }
+
+
+}
