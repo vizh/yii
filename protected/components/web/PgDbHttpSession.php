a554042f6971d449043d67addbee81723ce60fbf
Maxim Palenov <MAX_ELEK@mail.ru>
2016-03-02 16:12:07 +0300

    Cleared code

--- protected/components/web/PgDbHttpSession.php
+++ protected/components/web/PgDbHttpSession.php
@@ -1,15 +1,15 @@
 <?php
 namespace application\components\web;
 
+/**
+ * Class PgDbHttpSession Implementation of the database session storage for the PostgreSQL
+ */
 class PgDbHttpSession extends \CDbHttpSession
 {
-//  public function readSession($id)
-//  {
-//    echo $id;
-//    $data = parent::readSession($id);
-//    var_dump($data);
-//    return $data;
-//  }
+    /**
+     * Adds a regeneration session identifier ability
+     * @inheritdoc
+     */
     public function open()
     {
         parent::open();
@@ -19,10 +19,14 @@ class PgDbHttpSession extends \CDbHttpSession
         }
     }
 
-
+    /**
+     * Escapes data for the session
+     * @inheritdoc
+     */
     public function writeSession($id, $data)
     {
         $data = str_replace('\\', '\\\\', $data);
+
         return parent::writeSession($id, $data);
     }
 }
