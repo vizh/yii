be8b9deec9098f4d4e1b5988659e300d48133d14
Maxim Palenov <MAX_ELEK@mail.ru>
2016-03-04 15:43:00 +0300

    Code was cleared

--- protected/modules/partner/views/order/index.php
+++ protected/modules/partner/views/order/index.php
@@ -14,39 +14,43 @@ $this->setPageTitle(\Yii::t('app', 'Счет'));
 $controller = $this;
 ?>
 
-<?php $this->beginClip(Controller::PAGE_HEADER_CLIP_ID);?>
-    <?=\CHtml::link('<span class="fa fa-plus btn-label"></span> ' . \Yii::t('app', 'Выставить счет'), ['edit'], ['class' => 'btn btn-primary btn-labeled']);?>
-<?php $this->endClip();?>
+<?php $this->beginClip(Controller::PAGE_HEADER_CLIP_ID) ?>
+    <?= CHtml::link(
+        '<span class="fa fa-plus btn-label"></span> ' . Yii::t('app', 'Выставить счет'),
+        ['edit'],
+        ['class' => 'btn btn-primary btn-labeled']
+    ) ?>
+<?php $this->endClip() ?>
 
 <div class="panel panel-info">
     <div class="panel-heading">
-        <span class="panel-title"><i class="fa fa-building-o"></i> <?=\Yii::t('app', 'Счета мероприятия');?></span>
+        <span class="panel-title"><i class="fa fa-building-o"></i> <?= Yii::t('app', 'Счета мероприятия') ?></span>
     </div>
     <div class="panel-body">
         <div class="table-info">
-            <?$this->widget('\application\widgets\grid\GridView', [
-                'dataProvider'=> $search->getDataProvider(),
+            <?php $this->widget('application\widgets\grid\GridView', [
+                'dataProvider' => $search->getDataProvider(),
                 'filter' => $search,
                 'summaryCssClass' => 'table-header',
                 'summaryText' => 'Счета {start}-{end} из {count}.',
                 'columns' => [
                     [
-                        'name'  => 'Number',
-                        'type'  => 'raw',
-                        'header'=> $search->getAttributeLabel('Number'),
+                        'name' => 'Number',
+                        'type' => 'raw',
+                        'header' => $search->getAttributeLabel('Number'),
                         'value' => function (Order $order) {
                             return \CHtml::tag('span', ['class' => 'lead'], $order->Number);
                         },
                         'width' => 150
                     ],
                     [
-                        'name'  => 'Type',
-                        'type'  => 'raw',
-                        'header'=> $search->getAttributeLabel('Type'),
+                        'name' => 'Type',
+                        'type' => 'raw',
+                        'header' => $search->getAttributeLabel('Type'),
                         'value' => function (Order $order) {
                             if ($order->Type == OrderType::Juridical) {
                                 $result = \CHtml::tag('span', ['class' => 'label label-info'], OrderType::getTitle($order->Type));
-                                $result.= '<p class="small m-top_5">' . $order->OrderJuridical->Name. ', ' . \Yii::t('app', 'ИНН/КПП:') . ' ' . $order->OrderJuridical->INN . '/' . $order->OrderJuridical->KPP . '</p>';
+                                $result .= '<p class="small m-top_5">' . $order->OrderJuridical->Name . ', ' . \Yii::t('app', 'ИНН/КПП:') . ' ' . $order->OrderJuridical->INN . '/' . $order->OrderJuridical->KPP . '</p>';
                                 return $result;
                             } elseif ($order->Type == OrderType::Receipt) {
                                 return \CHtml::tag('span', ['class' => 'label label-warning'], OrderType::getTitle($order->Type));
@@ -60,8 +64,8 @@ $controller = $this;
                         ]
                     ],
                     [
-                        'name'  => 'Payer',
-                        'header'=> $search->getAttributeLabel('Payer'),
+                        'name' => 'Payer',
+                        'header' => $search->getAttributeLabel('Payer'),
                         'type' => 'raw',
                         'value' => function (Order $order) use ($controller) {
                             return $controller->renderPartial('../partial/grid/user', ['user' => $order->Payer], true);
@@ -86,7 +90,7 @@ $controller = $this;
                         'value' => function (Order $order) use ($search) {
                             if ($order->Paid) {
                                 $result = \CHtml::tag('span', ['class' => 'label label-success'], $search->getStatusData()[Orders::STATUS_PAID]);
-                                $result.= '<p class="small m-top_5">' . \Yii::app()->getDateFormatter()->format('d MMMM y', $order->PaidTime) . '</p>';
+                                $result .= '<p class="small m-top_5">' . \Yii::app()->getDateFormatter()->format('d MMMM y', $order->PaidTime) . '</p>';
                                 return $result;
                             } elseif ($order->Deleted) {
                                 return \CHtml::tag('span', ['class' => 'label label-danger'], $search->getStatusData()[Orders::STATUS_DELETED]);
@@ -116,9 +120,9 @@ $controller = $this;
                                 'label' => '<i class="fa fa-print"></i>',
                                 'url' => '$data->getUrl(true)',
                                 'options' => [
-                                    'class'  => 'btn btn-default',
+                                    'class' => 'btn btn-default',
                                     'target' => '_blank',
-                                    'title'  => 'Печать'
+                                    'title' => 'Печать'
                                 ],
                                 'visible' => '$data->getIsBankTransfer()'
                             ],
@@ -126,15 +130,15 @@ $controller = $this;
                                 'label' => '<i class="fa fa-check"></i>',
                                 'visible' => '!$data->Deleted && !$data->Paid && $data->getIsBankTransfer() && Yii::app()->controller->getAccessFilter()->checkAccess("partner", "order", "activate")',
                                 'options' => [
-                                    'class'  => 'btn btn-warning',
-                                    'title'  => 'Отметить данный счет оплаченным'
+                                    'class' => 'btn btn-warning',
+                                    'title' => 'Отметить данный счет оплаченным'
                                 ],
                                 'url' => 'Yii::app()->controller->createUrl("activate",["id"=>$data->primaryKey])'
                             ]
                         ]
                     ]
                 ]
-            ]);?>
+            ]) ?>
         </div>
     </div>
 </div>
