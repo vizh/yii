659a8a4bc3521a1707404611bfde884308b09eb9
Maxim Palenov <MAX_ELEK@mail.ru>
2016-03-14 13:27:33 +0300

    Shows widget "about" for full width layout without margins

--- protected/modules/competence/views/main/done.php
+++ protected/modules/competence/views/main/done.php
@@ -11,7 +11,7 @@ $this->setPageTitle($test->Title);
 <div class="row">
     <div class="span8 offset2 m-top_30 text-center">
         <?php if (!empty($test->AfterEndText)): ?>
-            <?=$test->AfterEndText;?>
+            <?=$test->AfterEndText?>
         <?php else: ?>
             <p class="lead">БОЛЬШОЕ СПАСИБО ЗА УЧАСТИЕ В НАШЕМ ИССЛЕДОВАНИИ!</p>
         <?php endif ?>
