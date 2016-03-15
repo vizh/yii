0abf245d572c9bc9de6a7893292be06631656db1
Maxim Palenov <MAX_ELEK@mail.ru>
2016-03-15 18:27:06 +0300

    Fixes for svyaz16

--- protected/modules/competence/views/main/end.php
+++ protected/modules/competence/views/main/end.php
@@ -6,15 +6,15 @@
 ?>
 
 <div class="row">
-    <div class="span8 offset2 m-top_30 text-center">
+    <div class="span12 m-top_30 text-center">
         <?php if (!empty($test->AfterEndText)): ?>
-            <?=$test->AfterEndText?>
+            <?= $test->AfterEndText ?>
         <?php else: ?>
-            <p class="lead">БОЛЬШОЕ СПАСИБО ЗА УЧАСТИЕ В НАШЕМ ИССЛЕДОВАНИИ!</p>
+            <p class="lead"><?= Yii::t('app', 'БОЛЬШОЕ СПАСИБО ЗА УЧАСТИЕ В НАШЕМ ИССЛЕДОВАНИИ') ?>!</p>
         <?php endif ?>
 
         <?php if ($done): ?>
-          <p style="color: #ff9900;">Вы уже ответили на вопросы исследования ранее.</p>
+            <p style="color: #ff9900;">Вы уже ответили на вопросы исследования ранее.</p>
         <?php endif ?>
     </div>
 </div>
