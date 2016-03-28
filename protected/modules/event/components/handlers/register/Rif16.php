92de53837e23d656fb368faea1fbcd36e1ba2e71
Maxim Palenov <MAX_ELEK@mail.ru>
2016-03-28 14:25:18 +0300

    Fixes for rif16

--- protected/modules/event/components/handlers/register/Rif16.php
+++ protected/modules/event/components/handlers/register/Rif16.php
@@ -1,6 +1,8 @@
 <?php
 namespace event\components\handlers\register;
 
+use event\models\Role;
+
 class Rif16 extends Base
 {
     /**
@@ -8,6 +10,10 @@ class Rif16 extends Base
      */
     public function getBody()
     {
+        if ($this->role->Id == Role::VIRTUAL_ROLE_ID) {
+            return parent::getBody();
+        }
+
         $params = [
             'user' => $this->user,
             'event' => $this->event,
