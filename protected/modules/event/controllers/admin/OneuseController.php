f4e8d1cac6ffb788eeb6b4bf0a28147327f77c37
Korotov Andrey <andrey.korotov@yandex.ru>
2016-05-19 00:28:46 +0300

    Oneuse action devcon16

--- protected/modules/event/controllers/admin/OneuseController.php
+++ protected/modules/event/controllers/admin/OneuseController.php
@@ -4,6 +4,43 @@ use application\components\controllers\AdminMainController;
 
 class OneuseController extends AdminMainController
 {
+    public function actionDevcon16Products()
+    {
+        $participants = \event\models\Participant::model()->byEventId(2319)
+            ->byRoleId(1)
+            ->findAll();
+
+        $map = \application\components\helpers\ArrayHelper::map(
+            $participants,
+            function (\event\models\Participant $participant) {
+                $account = \api\models\ExternalUser::model()
+                    ->byUserId($participant->UserId)
+                    ->byAccountId(335)
+                    ->find();
+
+                return $account->ExternalId;
+            },
+            function (\event\models\Participant $participant) {
+                $devcon = [4015,4016,4017,4018];
+
+                $criteria = new \CDbCriteria();
+                $criteria->addInCondition('"t"."ProductId"', array_merge($devcon, [4013,4019]));
+
+                $orderItem = \pay\models\OrderItem::model()
+                    ->byAnyOwnerId($participant->UserId)
+                    ->byEventId($participant->EventId)
+                    ->byPaid(true)
+                    ->find($criteria);
+
+                return in_array($orderItem->ProductId, $devcon) ? 'DevCon' : $orderItem->Product->Title;
+            }
+        );
+
+
+        var_dump($map);
+    }
+
+
     public function actionRegister2563()
     {
 
