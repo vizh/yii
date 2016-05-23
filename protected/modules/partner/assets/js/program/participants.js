8bddd44f9ed5627eed6fde82999d45a9fc8c1433
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-05-23 11:40:13 +0300

    wysiwyg для описания секции и тезисов докладчика

--- protected/modules/partner/assets/js/program/participants.js
+++ protected/modules/partner/assets/js/program/participants.js
@@ -1,12 +1,12 @@
 $(function () {
-  var ckeditorCount = 0;
-  $('textarea[name*="Participant[ReportFullInfo]"]').each(function () {
-    ckeditorCount++;
-    var id = 'ckeditor'+ckeditorCount;
-    $(this).attr('id', id);
-    CKEDITOR.replace(id, {
-      'height' :300,
-      'customConfig' : 'config_partner_program.js'
-    })
-  });
+    var ckeditorCount = 0;
+    $('textarea[name*="Participant[ReportFullInfo]"], textarea[name*="Participant[ReportThesis]"]').each(function () {
+        ckeditorCount++;
+        var id = 'ckeditor' + ckeditorCount;
+        $(this).attr('id', id);
+        CKEDITOR.replace(id, {
+            height: 200,
+            customConfig: 'config_partner_program.js'
+        })
+    });
 });
