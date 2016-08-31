9771beea2a75efcd8f77aa1b5017159f025b8303
Alex Krizhanovsky <alex@enot.vc>
2016-08-31 22:09:33 +0300

    PayPal авторизация

--- protected/modules/oauth/controllers/PaypalController.php
+++ protected/modules/oauth/controllers/PaypalController.php
@@ -1,14 +1,14 @@
 <?php
 class PaypalController extends \application\components\controllers\BaseController
 {
-  public function actionRedirect()
-  {
-    $code = \Yii::app()->getRequest()->getParam('code', null);
-    $redirectUrl = \Yii::app()->getSession()->get(\oauth\components\social\PayPal::SessionNameRedirectUrl, null);
-    if ($code == null || $redirectUrl == null)
-      throw new \CHttpException(404);
+    public function actionRedirect()
+    {
+        $code = \Yii::app()->getRequest()->getParam('code', null);
+        $redirectUrl = \Yii::app()->getSession()->get(\oauth\components\social\PayPal::SessionNameRedirectUrl, null);
+        if ($code == null || $redirectUrl == null)
+            throw new \CHttpException(404);
 
-    $redirectUrl .= '&code='.$code;
-    $this->redirect($redirectUrl);
-  }
+        $redirectUrl .= '&code='.$code;
+        $this->redirect($redirectUrl);
+    }
 } 
