38d4daf1ee8feea7ea38a6ab23219f8729a23fda
Максим Махнюк <star.absorber@gmail.com>
2016-09-02 18:00:31 +0300

    Api / Добавлена возможность проксирования указанной в параметре lang локали на страницу платежа через авторизационную платёжную ссылку, выдаваемую api, как PaymentUrl.

--- protected/modules/pay/controllers/cabinet/AuthAction.php
+++ protected/modules/pay/controllers/cabinet/AuthAction.php
@@ -1,39 +1,43 @@
 <?php
 namespace pay\controllers\cabinet;
 
+use Yii;
+
 class AuthAction extends \pay\components\Action
 {
-  public function run($eventIdName, $runetId, $hash)
-  {
-    /** @var $user \user\models\User */
-    $user = \user\models\User::model()->byRunetId($runetId)->find();
-
-    if ($user === null || !$user->checkHash($hash))
+    public function run($eventIdName, $runetId, $hash)
     {
-      throw new \CHttpException(404);
-    }
+        /** @var $user \user\models\User */
+        $user = \user\models\User::model()->byRunetId($runetId)->find();
 
-    if (\Yii::app()->user->getCurrentUser() === null || \Yii::app()->user->getCurrentUser()->RunetId != $runetId)
-    {
-      $identity = new \application\components\auth\identity\RunetId($runetId);
-      $identity->authenticate();
-      if ($identity->errorCode == \application\components\auth\identity\Base::ERROR_NONE)
-      {
-        if (!$user->Temporary)
-        {
-          \Yii::app()->user->login($identity, $identity->GetExpire());
+        if ($user === null || !$user->checkHash($hash)) {
+            throw new \CHttpException(404);
         }
-        else
-        {
-          \Yii::app()->tempUser->login($identity);
-        }
-      }
-      else
-      {
-        throw new \CHttpException(404);
-      }
-    }
 
-    $this->getController()->redirect($this->getController()->createUrl('/pay/cabinet/index'));
-  }
+        if (Yii::app()->user->getCurrentUser() === null || Yii::app()->user->getCurrentUser()->RunetId != $runetId) {
+            $identity = new \application\components\auth\identity\RunetId($runetId);
+            $identity->authenticate();
+            if ($identity->errorCode == \application\components\auth\identity\Base::ERROR_NONE) {
+                if (!$user->Temporary) {
+                    Yii::app()->user->login($identity, $identity->GetExpire());
+                } else {
+                    Yii::app()->tempUser->login($identity);
+                }
+            } else {
+                throw new \CHttpException(404);
+            }
+        }
+
+        // Если в запросе указана локаль, то нам нужно проксировать её настраницу оплат
+        $lang = Yii::app()
+            ->getRequest()
+            ->getParam('lang');
+
+        $prms = [];
+        if ($lang !== null) {
+            $prms = ['lang' => $lang];
+        }
+
+        $this->getController()->redirect($this->getController()->createUrl('/pay/cabinet/index', $prms));
+    }
 }
