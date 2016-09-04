a43de3234a1755ab12195fe3b993598d4aee61d6
Coderfly <alaris.nik@gmail.com>
2016-09-05 01:10:25 +0300

    Fix wrong next questions

--- protected/modules/competence/models/test/runet2016/D2_9.php
+++ protected/modules/competence/models/test/runet2016/D2_9.php
@@ -7,7 +7,7 @@ class D2_9 extends D2
 
     protected $lastCode = 'E1_3';
 
-    protected $nextCodes = ['10'=>'C1_10', '11'=>'C1_11', '12_1'=>'C1_2_1', '12_2'=>'C1_2_2'];
+    protected $nextCodes = ['10'=>'C1_10', '11'=>'C1_11', '12_1'=>'C1_12_1', '12_2'=>'C1_12_2'];
     
     public $subMarkets = [
         '1' => 'Доля заказов с мобильных устройств',
