5322f9aa470c20db56924175e7df6810a66476c7
Максим Махнюк <star.absorber@gmail.com>
2016-09-05 15:21:06 +0300

    Api / Исправлены ошибки в работе метода user/login.

--- protected/modules/api/controllers/user/LoginAction.php
+++ protected/modules/api/controllers/user/LoginAction.php
@@ -2,24 +2,19 @@
 namespace api\controllers\user;
 
 use api\components\Action;
-use user\models\User;
 use api\components\Exception;
 
 class LoginAction extends Action
 {
     public function run()
     {
-        $request = \Yii::app()->getRequest();
+        $user = $this->getRequestedUser();
 
-        $login = $this->getLoginParam();
-        $password = $request->getParam('Password');
-
-        $user = User::model()->byRunetId($login)->byEmail($login, 'OR')->find();
-        if ($user === null) {
-            throw new Exception(211, [$login]);
+        if ($user->Email !== $this->getRequestedParam('Email')) {
+            throw new Exception(210, $this->getRequestedParam('Email'));
         }
 
-        if (!$user->checkLogin($password)) {
+        if ($user->checkLogin($this->getRequestedParam('Password')) === false) {
             throw new Exception(201);
         }
 
@@ -28,21 +23,5 @@ class LoginAction extends Action
             ->createUser($user);
 
         $this->setResult($userData);
-    }
-
-    /**
-     * @return mixed
-     * @throws Exception
-     */
-    public function getLoginParam()
-    {
-        $request = \Yii::app()->getRequest();
-        foreach (['Login', 'Email', 'RunetId'] as $name) {
-            $login = $request->getParam($name);
-            if (!empty($login)) {
-                return trim($login);
-            }
-        }
-        throw new Exception(110);
     }
 }
