f9d8a03b7939c01b3faf3f470bc74eee68b595df
Coderfly <alaris.nik@gmail.com>
2016-09-05 13:40:14 +0300

    Changed C2 question type.

--- protected/modules/competence/models/test/runet2016/C2.php
+++ protected/modules/competence/models/test/runet2016/C2.php
@@ -1,15 +1,15 @@
 <?php
 namespace competence\models\test\runet2016;
 
-use competence\models\form\Single;
+use competence\models\form\Input;
 
-class C2 extends Single
+class C2 extends Input
 {
     public function rules()
     {
         $rules = parent::rules();
         $rules[] = [
-            'other', 'numerical',
+            'value', 'numerical',
             'message' => 'Вводимое значение должно быть числом, дробная часть отделяется точкой.',
             'min' => 0,
             'tooSmall' => 'Вводимое значение не может быть меньше нуля'
