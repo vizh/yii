ea5032f5a44da52a0a02d31e64861917409ae7ac
Максим Махнюк <star.absorber@gmail.com>
2016-09-05 16:09:59 +0300

    Api / Для метода user/login более не нужен RunetId в качестве параметра.

--- protected/modules/api/controllers/user/LoginAction.php
+++ protected/modules/api/controllers/user/LoginAction.php
@@ -3,16 +3,18 @@ namespace api\controllers\user;
 
 use api\components\Action;
 use api\components\Exception;
+use user\models\User;
 
 class LoginAction extends Action
 {
     public function run()
     {
-        $user = $this->getRequestedUser();
+        $user = User::model()
+            ->byEmail($this->getRequestedParam('Email'))
+            ->find();
 
-        if ($user->Email !== $this->getRequestedParam('Email')) {
+        if ($user === null)
             throw new Exception(210, $this->getRequestedParam('Email'));
-        }
 
         if ($user->checkLogin($this->getRequestedParam('Password')) === false) {
             throw new Exception(201);
