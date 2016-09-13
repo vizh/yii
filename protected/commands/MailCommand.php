9d72b0a41b112d5c912534fc08985bd7aecdb197
Максим Махнюк <star.absorber@gmail.com>
2016-09-13 16:04:30 +0300

    Рассылки / Увеличена сессия отправки писем до 6 минут без 10 секунд, дабы нивелировать задержки cron.

--- protected/commands/MailCommand.php
+++ protected/commands/MailCommand.php
@@ -21,7 +21,7 @@ class MailCommand extends BaseConsoleCommand
 
         while (true) {
             $template->send();
-            if ($template->Success || time() - $startTime >= 180)
+            if ($template->Success || time() - $startTime >= 350)
                 return 0;
         }
 
