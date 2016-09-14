d63eb99474a59f794b6c5aae489c45ed2e90c259
Maxim Borzov <max.borzov@gmail.com>
2016-09-15 02:16:05 +0300

    Closes #4304: Программа / Детальная информация о секциях в модуальном окне

---
+++ protected/modules/event/widgets/assets/js/programgrid.js
@@ -0,0 +1,47 @@
+var CProgramGrid= function () {
+    this.$widget = $('#event_widgets_ProgramGrid');
+    this.isInitialized = false;
+}
+
+CProgramGrid.prototype = {
+    'init' : function () {
+        var self = this;
+
+        self.$widget.find('h5').click(function (e) {
+            var $target = $(e.currentTarget);
+            self.showSectionDetails($target.siblings('.details-text'));
+            e.preventDefault();
+        });
+    },
+
+    'showSectionDetails': function ($section) {
+        console.log($section);
+        var self = this,
+            $modal = $('<div/>', {'class' : 'modal'});
+
+        $modal.html(
+            '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h3>' + $section.context.innerText + '</h3></div>'+
+            '<div class="modal-body"><h4 class="text-error text-center">' + $section[0].innerHTML + '</h4></div>' +
+            '<div class="modal-footer"><a href="#" class="btn" data-dismiss="modal">Закрыть</a></div>'
+        );
+        $modal.on('hidden', function () {
+            $modal.remove();
+        })
+        $modal.modal('show');
+    }
+
+}
+
+$(function () {
+    var programGrid = new CProgramGrid();
+    function initProgramGrid() {
+        if (!programGrid.isInitialized && programGrid.$widget.is(':visible')) {
+            programGrid.init();
+        }
+    }
+
+    initProgramGrid();
+    $('#event-tabs').on('tabsactivate', function( event, ui ) {
+        initProgramGrid();
+    });
+});
--- protected/modules/event/widgets/views/program-grid.php
+++ protected/modules/event/widgets/views/program-grid.php
@@ -30,10 +30,7 @@ use event\models\section\LinkUser;
             <?if ($flag):?>
               <tr <?if ($data->Sections[$hallId][$time]->ColSpan == sizeof($data->Halls) && $data->Sections[$hallId][$time]->Section->TypeId == 4):?>class="info"<?endif;?>>
                 <td class="time">
-                    <?php if ($this->getEvent()->IdName == 'next2015' && trim($label) == '12:25 &mdash; 12:30'): //TODO: Костыль для next2015?>
-                    <?php else:?>
-                        <?=$label;?>
-                    <?php endif;?>
+                    <?=$label;?>
                 </td>
                 <?$flag = false;?>
             <?endif;?>
@@ -41,55 +38,89 @@ use event\models\section\LinkUser;
               <?$colspan = $data->Sections[$hallId][$time]->ColSpan;?>
               <td colspan="<?=$colspan;?>">
                 <?$section = $data->Sections[$hallId][$time]->Section;?>
+                <? if (isset($this->DetailedModal) && $this->DetailedModal == '1' && $data->Sections[$hallId][$time]->Section->TypeId != 4): ?>
+                <h5><a href="#"><?=$section->Title;?></a></h5>
+                <? else: ?>
                 <h5><?=$section->Title;?></h5>
-                  <?if (!empty($section->Info)):?>
-                  <p><?=$section->Info;?></p>
-                  <?endif;?>
-                <?/** @var LinkUser[] $links */?>
-                <?foreach ($data->Sections[$hallId][$time]->Links as $links):?>
-                  <div class="m-bottom_10">
-                  <?if ($links[0]->Role->Type != \event\models\RoleType::Speaker):?>
-                    <b><?=\Yii::t('app', sizeof($links) > 1 ? 'Ведущие' : 'Ведущий');?>:</b><br/>
-                    <?foreach ($links as $link):?>
-                          <?if ($link->User != null):?>
-                              <a href="<?=$link->User->getUrl();?>"><?=$link->User->getFullName();?></a>
-                              <?if ($link->User->getEmploymentPrimary() !== null):?>(<?=$link->User->getEmploymentPrimary()->Company->Name;?>)<?endif;?>
-                          <?elseif ($link->Company != null):?>
-                              <a href="<?=$link->Company->getUrl();?>"><?=$link->Company->Name;?></a>
-                          <?else:?>
-                              <?=$link->CustomText;?>
-                          <?endif;?>
-                          <?php if (!empty($link->Report) && !empty($link->Report->Url)):?>
-                              <a title="Скачать презентацию" target="_blank" href="<?=$link->Report->Url;?>"><img style="vertical-align: bottom;" src="/images/icon-adobe-pdf.png" alt="Скачать презентацию"/></a>
-                          <?php endif;?>
-                      <br/>
-                    <?endforeach;?>
-                  <?else:?>
-                    <?=\Yii::t('app', $section->Type->Code == 'round' ? 'Участники круглого стола' : 'Докладчики');?>:
-                    <?/** @var \user\models\User $user */;?>
-                    <ul>
-                    <?foreach ($links as $link):?>
-                      <li>
-                          <?if (!empty($link->Report->Title)):?>
-                            <strong><?=$link->Report->Title;?></strong><br>
-                          <?endif;?>
-                          <?if ($link->User != null):?>
-                              <a href="<?=$link->User->getUrl();?>"><?=$link->User->getFullName();?></a>
-                              <?if ($link->User->getEmploymentPrimary() !== null):?>(<?=$link->User->getEmploymentPrimary()->Company->Name;?>)<?endif;?>
-                          <?elseif ($link->Company != null):?>
-                              <a href="<?=$link->Company->getUrl();?>"><?=$link->Company->Name;?></a>
-                          <?else:?>
-                              <?=$link->CustomText;?>
-                          <?endif;?>
-                          <?php if (!empty($link->Report) && !empty($link->Report->Url)):?>
-                              <a title="Скачать презентацию" target="_blank" href="<?=$link->Report->Url;?>"><img style="vertical-align: bottom;" src="/images/icon-adobe-pdf.png" alt="Скачать презентацию"/></a>
-                          <?php endif;?>
-                      </li>
-                    <?endforeach;?>
-                    </ul>
-                  <?endif;?>
+                <? endif; ?>
+                  <? if (isset($this->DetailedModal) && $this->DetailedModal == '1'): ?>
+                    <? foreach ($data->Sections[$hallId][$time]->Links as $links): ?>
+                      <? if ($links[0]->Role->Type != \event\models\RoleType::Speaker): ?>
+                          <div class="section-text" data-section="<?= $section->Id; ?>">
+                              <b><?= \Yii::t('app', sizeof($links) > 1 ? 'Ведущие' : 'Ведущий'); ?>:</b><br/>
+                              <? foreach ($links as $link): ?>
+                                  <? if ($link->User != null): ?>
+                                      <a href="<?= $link->User->getUrl(); ?>"><?= $link->User->getFullName(); ?></a>
+                                      <? if ($link->User->getEmploymentPrimary() !== null): ?>(<?= $link->User->getEmploymentPrimary()->Company->Name; ?>)<? endif; ?>
+                                  <? elseif ($link->Company != null): ?>
+                                      <a href="<?= $link->Company->getUrl(); ?>"><?= $link->Company->Name; ?></a>
+                                  <? else: ?>
+                                      <?= $link->CustomText; ?>
+                                  <? endif; ?>
+                                  <?php if (!empty($link->Report) && !empty($link->Report->Url)): ?>
+                                      <a title="Скачать презентацию" target="_blank" href="<?= $link->Report->Url; ?>"><img style="vertical-align: bottom;" src="/images/icon-adobe-pdf.png" alt="Скачать презентацию"/></a>
+                                  <?php endif; ?>
+                                  <br/>
+                              <? endforeach; ?>
+                          </div>
+                      <? endif; ?>
+                  <? endforeach; ?>
+                  <div class="details-text" data-section="<?=$section->Id;?>" style="display: none">
+                  <? else: ?>
+                  <div class="section-text" data-section="<?=$section->Id;?>">
+                  <? endif; ?>
+                      <?if (!empty($section->Info)):?>
+                        <p><?=$section->Info;?></p>
+                      <?endif;?>
+                      <? /** @var LinkUser[] $links */ ?>
+                      <? foreach ($data->Sections[$hallId][$time]->Links as $links): ?>
+                          <div class="m-bottom_10">
+                              <? if ($links[0]->Role->Type != \event\models\RoleType::Speaker): ?>
+                                  <b><?= \Yii::t('app', sizeof($links) > 1 ? 'Ведущие' : 'Ведущий'); ?>:</b><br/>
+                                  <? foreach ($links as $link): ?>
+                                      <? if ($link->User != null): ?>
+                                          <a href="<?= $link->User->getUrl(); ?>"><?= $link->User->getFullName(); ?></a>
+                                          <? if ($link->User->getEmploymentPrimary() !== null): ?>(<?= $link->User->getEmploymentPrimary()->Company->Name; ?>)<? endif; ?>
+                                      <? elseif ($link->Company != null): ?>
+                                          <a href="<?= $link->Company->getUrl(); ?>"><?= $link->Company->Name; ?></a>
+                                      <? else: ?>
+                                          <?= $link->CustomText; ?>
+                                      <? endif; ?>
+                                      <?php if (!empty($link->Report) && !empty($link->Report->Url)): ?>
+                                          <a title="Скачать презентацию" target="_blank"
+                                             href="<?= $link->Report->Url; ?>"><img style="vertical-align: bottom;" src="/images/icon-adobe-pdf.png" alt="Скачать презентацию"/></a>
+                                      <?php endif; ?>
+                                      <br/>
+                                  <? endforeach; ?>
+                              <? else: ?>
+                                  <?= \Yii::t('app',
+                                      $section->Type->Code == 'round' ? 'Участники круглого стола' : 'Докладчики'); ?>:
+                                  <? /** @var \user\models\User $user */; ?>
+                                  <ul>
+                                      <? foreach ($links as $link): ?>
+                                          <li>
+                                              <? if (!empty($link->Report->Title)): ?>
+                                                  <strong><?= $link->Report->Title; ?></strong><br>
+                                              <? endif; ?>
+                                              <? if ($link->User != null): ?>
+                                                  <a href="<?= $link->User->getUrl(); ?>"><?= $link->User->getFullName(); ?></a>
+                                                  <? if ($link->User->getEmploymentPrimary() !== null): ?>(<?= $link->User->getEmploymentPrimary()->Company->Name; ?>)<? endif; ?>
+                                              <? elseif ($link->Company != null): ?>
+                                                  <a href="<?= $link->Company->getUrl(); ?>"><?= $link->Company->Name; ?></a>
+                                              <? else: ?>
+                                                  <?= $link->CustomText; ?>
+                                              <? endif; ?>
+                                              <?php if (!empty($link->Report) && !empty($link->Report->Url)): ?>
+                                                  <a title="Скачать презентацию" target="_blank"
+                                                     href="<?= $link->Report->Url; ?>"><img style="vertical-align: bottom;" src="/images/icon-adobe-pdf.png" alt="Скачать презентацию"/></a>
+                                              <?php endif; ?>
+                                          </li>
+                                      <? endforeach; ?>
+                                  </ul>
+                              <? endif; ?>
+                          </div>
+                      <? endforeach; ?>
                   </div>
-                <?endforeach;?>
               </td>
             <?elseif ($colspan <= 0):?>
               <td></td>
