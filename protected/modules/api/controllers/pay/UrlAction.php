9af02b986443fd238690947b314726a698ea2d85
Максим Махнюк <star.absorber@gmail.com>
2016-09-21 13:16:38 +0300

    Api / Более детальная информация об ошибках обработки параметров PayerRunetId и OwnerRunetId.

--- protected/modules/api/controllers/pay/UrlAction.php
+++ protected/modules/api/controllers/pay/UrlAction.php
@@ -1,25 +1,18 @@
 <?php
 namespace api\controllers\pay;
 
+use Yii;
+
 class UrlAction extends \api\components\Action
 {
     public function run()
     {
-        $request = \Yii::app()->getRequest();
-        $payerRunetId = $request->getParam('PayerRunetId', null);
-        if ($payerRunetId === null) {
-            $payerRunetId = $request->getParam('PayerRocId', null);
-        }
-
-        /** @var $payer \user\models\User */
-        $payer = \user\models\User::model()->byRunetId($payerRunetId)->find();
-        if ($payer == null) {
-            throw new \api\components\Exception(202, [$payerRunetId]);
-        }
-
-        $result = new \stdClass();
-        $result->Url = \Yii::app()->createAbsoluteUrl('/pay/cabinet/auth',
-            ['eventIdName' => $this->getEvent()->IdName, 'runetId' => $payer->RunetId, 'hash' => $payer->getHash()]);
-        $this->setResult($result);
+        $this->setResult((object)[
+            'Url' => Yii::app()->createAbsoluteUrl('/pay/cabinet/auth', [
+                'eventIdName' => $this->getEvent()->IdName,
+                'runetId' => $this->getRequestedPayer()->RunetId,
+                'hash' => $this->getRequestedPayer()->getHash()
+            ])
+        ]);
     }
 }
