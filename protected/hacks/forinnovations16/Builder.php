01da48d4cf6b6bf1834f2365ab8b4eb282a593b5
Максим Махнюк <star.absorber@gmail.com>
2016-10-11 12:43:43 +0300

    Api / Возможность подставить значения ФИО и названия компании из атрибутов.

--- protected/hacks/forinnovations16/Builder.php
+++ protected/hacks/forinnovations16/Builder.php
@@ -39,6 +39,22 @@ class Builder extends \api\components\builders\Builder
 
         $this->user->AttributesRaw = UserData::getDefinedAttributeValues($this->account->Event, $user);
 
+        // Вот так можно переписать ФИО из атрибутов.
+//        $this->user->LastName = $this->user->Attributes->last_name;
+//        $this->user->FirstName = $this->user->Attributes->first_name;
+//        $this->user->FatherName = $this->user->Attributes->father_name;
+//
+//        foreach (['last_name' => 'LastName', 'first_name' => 'FirstName', 'father_name' => 'FatherName'] as $attribute => $parameter) {
+//            $attributeValue = $this->user->AttributesRaw[$attribute];
+//            if (!empty($attributeValue) && is_array($attributeValue)) {
+//                foreach ($attributeValue as $lang => $value) {
+//                    if (isset($this->user->Locales[$lang])) {
+//                        $this->user->Locales[$lang][$parameter] = $value;
+//                    }
+//                }
+//            }
+//        }
+
         return $this->user;
     }
 
