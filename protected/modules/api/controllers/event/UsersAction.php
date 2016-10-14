a8b0725d41ec311bdc0a2237d142a8a7374a4be0
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2016-10-14 14:50:26 +0300

    Возможность выбирать билдеры в event/users

--- protected/modules/api/controllers/event/UsersAction.php
+++ protected/modules/api/controllers/event/UsersAction.php
@@ -1,26 +1,33 @@
 <?php
+
 namespace api\controllers\event;
 
+use api\components\Action;
 use api\components\builders\Builder;
+use CDbCriteria;
+use user\models\User;
+use Yii;
 
-class UsersAction extends \api\components\Action
+class UsersAction extends Action
 {
     public function run()
     {
-        $request = \Yii::app()->getRequest();
+        $request = Yii::app()->getRequest();
         $maxResults = (int)$request->getParam('MaxResults', $this->getMaxResults());
         $maxResults = min($maxResults, $this->getMaxResults());
         $pageToken = $request->getParam('PageToken', null);
         $roles = $request->getParam('RoleId');
 
-        $command = \Yii::app()->getDb()->createCommand()
+        $command = Yii::app()->getDb()->createCommand()
             ->select('EventParticipant.UserId')->from('EventParticipant')
             ->where('"EventParticipant"."EventId" = '.$this->getEvent()->Id);
+
         if (!empty($roles)) {
             $command->andWhere(['in', 'EventParticipant.RoleId', $roles]);
         }
 
-        $criteria = new \CDbCriteria();
+        $criteria = new CDbCriteria();
+
         if ($pageToken === null) {
             $criteria->limit = $maxResults;
             $criteria->offset = 0;
@@ -33,33 +40,41 @@ class UsersAction extends \api\components\Action
             'Participants' => [
                 'on' => '"Participants"."EventId" = :EventId',
                 'params' => [
-                    'EventId' => $this->getEvent()->Id
+                    'EventId' => $this->getEvent()->Id,
                 ],
-                'together' => false
+                'together' => false,
             ],
             'Employments.Company' => ['on' => '"Employments"."Primary"', 'together' => false],
-            'LinkPhones.Phone' => ['together' => false]
+            'LinkPhones.Phone' => ['together' => false],
         ];
         $criteria->order = '"t"."LastName" ASC, "t"."FirstName" ASC';
-
         $criteria->addCondition('"t"."Id" IN ('.$command->getText().')');
 
-        $users = \user\models\User::model()->findAll($criteria);
-        $totalCount = \user\models\User::model()->count($criteria);
+        $users = User::model()->findAll($criteria);
+        $totalCount = User::model()->count($criteria);
 
         $result = [];
         $result['Users'] = [];
 
-        // Определим какие данные будут в результате
-        $builders = [
+        // Билдеры по умолчанию
+        $defaultBuilders = [
             Builder::USER_EVENT,
             Builder::USER_EMPLOYMENT,
-            Builder::USER_ATTRIBUTES
+            Builder::USER_ATTRIBUTES,
         ];
 
         // Не совсем понятно почему, но ок..
-        if ($this->getAccount()->Role !== 'mobile')
+        if ($this->getAccount()->Role !== 'mobile') {
+            $defaultBuilders[] = Builder::USER_CONTACTS;
+        }
+
+        // Определим какие данные будут в результате
+        $builders = $request->getParam('Builders', $defaultBuilders);
+
+        // Не совсем понятно почему, но ок..
+        if ($this->getAccount()->Role !== 'mobile') {
             $builders[] = Builder::USER_CONTACTS;
+        }
 
         $result['TotalCount'] = $totalCount;
 
@@ -69,12 +84,12 @@ class UsersAction extends \api\components\Action
                 ->createUser($user, $builders);
         }
 
-        if ($this->hasRequestParam('ArchivePhotos')){
+        if ($this->hasRequestParam('ArchivePhotos')) {
             $archive = \Yii::getPathOfAlias('application.runtime').'/'.uniqid().'.tar';
             $tar = new \PharData($archive);
             foreach ($users as $user) {
                 $photo = $user->getPhoto()->getOriginal(true);
-                if (is_file($photo)){
+                if (is_file($photo)) {
                     $tar->addFile($photo, basename($photo));
                 }
             }
@@ -84,7 +99,7 @@ class UsersAction extends \api\components\Action
             $archive .= '.gz';
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename="'.basename($archive).'"');
-            header('Content-Length: ' . filesize($archive));
+            header('Content-Length: '.filesize($archive));
             readfile($archive);
             unlink($archive);
             exit;
