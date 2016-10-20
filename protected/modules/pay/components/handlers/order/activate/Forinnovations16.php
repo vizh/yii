2e707fadfc5f41ac85b2615f5163980ba3de6991
Maxim Borzov <max.borzov@gmail.com>
2016-10-20 13:08:30 +0300

    Правка письма с уведомлением об оплате

--- protected/modules/pay/components/handlers/order/activate/Forinnovations16.php
+++ protected/modules/pay/components/handlers/order/activate/Forinnovations16.php
@@ -3,18 +3,66 @@ namespace pay\components\handlers\order\activate;
 
 class Forinnovations16 extends Base
 {
+    /**
+     * @return string
+     */
+    public function getLayoutName()
+    {
+        return 'oi16';
+    }
+
+    /**
+     * @return string
+     */
     public function getFrom()
     {
         return 'support@forinnovations.ru';
     }
 
+    /**
+     * @return string
+     */
     public function getFromName()
     {
         return 'Open Innovations 2016';
     }
 
-    public function getLayoutName()
+    /**
+     * @return string
+     */
+    public function getSubject()
     {
-        return 'oi16';
+        if ($this->order->Type == \pay\models\OrderType::Receipt) {
+            return 'Успешная оплата квитанции на участие в форуме '.$this->event->Title;
+        } elseif ($this->order->Type == \pay\models\OrderType::Juridical) {
+            return 'Успешная оплата счета на участие в форуме '.$this->event->Title;
+        }
+
+        return 'Успешная оплата участия в форуме '.$this->event->Title;
     }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getJuridicalViewPath()
+    {
+        return $this->getPhysicalViewPath();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getPhysicalViewPath()
+    {
+        return 'pay.views.mail.order.activate.forinnovations16';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function showFooter()
+    {
+        return false;
+    }
+
 }
