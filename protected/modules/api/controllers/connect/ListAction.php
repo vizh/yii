48d99519b2d8380db55a2be3ad4cba6d8e0e15e5
Максим Махнюк <star.absorber@gmail.com>
2016-10-28 11:17:15 +0300

    Api / Постраничная выдача встреч в методе connect/list.

--- protected/modules/api/controllers/connect/ListAction.php
+++ protected/modules/api/controllers/connect/ListAction.php
@@ -1,6 +1,7 @@
 <?php
 namespace api\controllers\connect;
 
+use application\components\CDbCriteria;
 use application\components\helpers\ArrayHelper;
 use connect\models\Meeting;
 
@@ -24,7 +25,7 @@ class ListAction extends \api\components\Action
             $meetings->byUserId($this->getRequestedUser('UserId')->Id);
         }
 
-        if ($this->hasRequestParam('Type')){
+        if ($this->hasRequestParam('Type')) {
             $meetings->byType($this->getRequestParam('Type'));
         }
 
@@ -32,17 +33,37 @@ class ListAction extends \api\components\Action
             $meetings->byStatus($this->getRequestParam('Status'));
         }
 
+        // Тонкость постраничной разбивки тут в том, что она не обязательна. Пока идёт ОИ16
+        // нет возможности её форсировать и нужно сохранить совместимость с мобильными приложениями.
+        if ($this->hasRequestParam('PageToken')) {
+            $pageToken = $this->getRequestParam('PageToken');
+            $pageLimit = (int)$this->getRequestParam('PageLimit', $this->getMaxResults());
+
+            $pageCriteria = CDbCriteria::create()
+                ->setOrder('"t"."Id" ASC')
+                ->setOffset($pageToken === 'start' ? 0 : $this->getController()->parsePageToken($pageToken))
+                ->setLimit($pageLimit)
+                ->apply($meetings);
+        }
         $meetings = $meetings->findAll();
 
-        $result = [];
+        $result = [
+            'Success' => true,
+            'Meetings' => []
+        ];
+
         foreach ($meetings as $meeting) {
-            $result[] = $this
+            $result['Meetings'][] = $this
                 ->getDataBuilder()
                 ->createMeeting($meeting);
         }
 
-        ArrayHelper::multisort($result, 'UserCount', SORT_DESC);
+        if (isset($pageLimit, $pageCriteria) && count($meetings) === $pageLimit) {
+            $result['NextPageToken'] = $this->getController()->getPageToken($pageCriteria->offset + $pageLimit);
+        }
 
-        $this->setResult(['Success' => true, 'Meetings' => $result]);
+        ArrayHelper::multisort($result['Meetings'], 'UserCount', SORT_DESC);
+
+        $this->setResult($result);
     }
 }
