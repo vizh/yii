fb28b6e75953d43c476ff7ea8f852afae1dd8858
Максим Махнюк <star.absorber@gmail.com>
2016-11-22 12:40:38 +0300

    Актуалилизация шаблонов рассылок.

---
+++ protected/modules/mail/views/templates/template1225.php
@@ -0,0 +1,41 @@
+<a href="http://conf.optimization.ru/program/?utm_source=runetid&utm_medium=mail&utm_campaign=opt16"><img alt="" src="http://runet-id.com/img/mail/2016/opi16.png" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; max-width: 100%; margin: 0; padding: 0;" /></a>
+
+<h3><strong>Здравствуйте, <?=$user->getShortName()?>!</strong></h3>
+
+<p>12 декабря в Digital October состоится конференция Optimization &laquo;Поисковый маркетинг и продвижение бизнеса в Интернете&raquo;. На ней вы сможете узнать, как привлекать в текущих реалиях клиентов из поиска, адаптировать рекламные кампании под мобильных пользователей и улучшать проекты для повышения конверсии и увеличения продаж. На конференции выступит более 50 экспертов из таких компаний как:&nbsp; Яндекс, Альфа Банк, Ostrovok, Google, Mail.Ru Group, Lamoda, &laquo;Ашманов и партнеры&raquo;, CallTouch.</p>
+
+<h3>Выступления, которые нельзя пропустить</h3>
+
+<ul>
+	<p><li>Игорь Ашманов проведет круглый стол с представителями Яндекс, Google и Mail.ru Group, которые ответят на вопросы участников конференции.</li></p>
+	<p><li>Как продвигать приложения в поисках App Store и Google Play расскажет Анастасия Ащеулова (AppFollow).</li></p>
+	<p><li>Как тестировать и оптимизировать мобильную рекламу для повышения &nbsp;эффективности научит Вера Козлова (Lamoda.ru).</li></p>
+	<p><li>Чем отличается поисковая оптимизация медиапроектов и как переезжать на новую версию сайта без потерь в трафике объяснит Владимир Харев (Mail.Ru Group).</li></p>
+	<p><li>Как составлять digital-микс и распределять бюджеты по каналам в 2017-м году расскажет Иван Калюжный («Альфа-Банк»).</li></p>
+	<p><li>Как использовать офлайн данные в интернет-рекламе объяснит Дмитрий Буров (myTarget) на примере реальных проектов &mdash; аптеки, кафе и банка.</li></p>
+	<p><li>Результаты мониторинга факторов ранжирования Яндекса и Google представит Михаил Волович (&laquo;Ашманов и партнеры&raquo;).</li></p>
+</ul>
+
+<h3>Стоимость со скидкой</h3>
+
+<p>Стоимость участия в конференции при регистрации и оплате до 20 ноября &mdash; 14 400 рублей. Чтобы активировать скидку 20%, введите в личном кабинете промокод &mdash;<strong> 20OPT16RUNETID</strong></p>
+
+<div style="text-align: center; border: 0px dashed #23B0E0; margin-top: 20px;">
+	<p style="margin-top: 10px 0; text-align: center;"><a href="http://conf.optimization.ru/registration/?utm_source=runetid&amp;utm_medium=mail&amp;utm_campaign=opt16" style="font-size: 100%; color: #fff; text-decoration: none; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 0px; text-transform: uppercase; background-color: #F1592D; margin: 0 10px 0 0; padding: 0; border-color: #F1592D; border-style: solid; border-width: 10px 40px;">Зарегистрироваться</a></p>
+</div>
+
+<h3>Пять причин стать участником Optimization 2016</h3>
+
+<ol>
+	<p><li><strong>Узнать, как устроено SEO в крупных проектах</strong>: особенности поисковой оптимизации медиа, работа с большой семантикой, как измерять эффективность поискового маркетинга и что требовать от исполнителей.</li></p>
+	<p><li><strong>Получить данные последних исследований алгоритмов поисковых машин </strong>&mdash; внутренних и внешних факторов ранжирования. Работают ли сегодня ссылки, как оптимизировать контент и улучшать поведенческие факторы, какие коммерческие факторы имеют значение.</li></p>
+	<p><li><strong>Задать вопросы представителям поисковых систем:</strong> Андрею Калинину (Mail.Ru Group), Сергею Архангельском (Google) и Александру Садовскому (Яндекс).</li></p>
+	<p><li><strong>Получить аудит: </strong>SEO-анализ сайта в Лаборатории поисковой аналитики &laquo;Ашманов и партнеры&raquo;, юзабилити-исследование или аудит рекламных кампаний.</li></p>
+	<p><li><strong>Выяснить, какие требования к digital-специалистам предъявляют компании</strong>, откуда специалисты приходят в профессию, как повышают квалификацию и в какие сферы уходят из интернет-маркетинга.</li></p>
+</ol>
+
+
+<div style="text-align: center; border: 0px dashed #23B0E0; margin-top: 20px;">
+<p style="text-align: center;"><strong>Промокод со скидкой 20% &mdash; 20OPT16RUNETID.</strong></p>
+	<p style="margin-top: 10px 0; text-align: center;"><a href="http://conf.optimization.ru/registration/?utm_source=runetid&amp;utm_medium=mail&amp;utm_campaign=opt16" style="font-size: 100%; color: #fff; text-decoration: none; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 0px; text-transform: uppercase; background-color: #F1592D; margin: 0 10px 0 0; padding: 0; border-color: #F1592D; border-style: solid; border-width: 10px 40px;">Зарегистрироваться</a></p>
+</div>
