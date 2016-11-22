b7f2bb8bdcc0ec26c7795b8db4f91287b1ff09fc
Максим Махнюк <star.absorber@gmail.com>
2016-11-22 19:54:56 +0300

    Бутстрап консольного yii приложения добавлен в репозиторий, так как он теперь общий для станции разработчика и для боевого сервера.

---
+++ protected/yiic.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('memory_limit', '1024M');
+
+date_default_timezone_set('Europe/Moscow');
+defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+
+$dirname = dirname(__FILE__);
+$config = "$dirname/../config/console.php";
+
+require_once "$dirname/../vendor/autoload.php";
+require_once "$dirname/Yii.php";
+
+$app = Yii::createConsoleApplication($config);
+$app->commandRunner->addCommands(YII_PATH . '/cli/commands');
+
+\Yii::setPathOfAlias('webroot', "$dirname/../www/");
+$env = @getenv('YII_CONSOLE_COMMANDS');
+if (!empty($env))
+    $app->commandRunner->addCommands($env);
+
+$app->run();
