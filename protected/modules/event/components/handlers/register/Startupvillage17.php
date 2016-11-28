74feb2388a06a5b2819a1d34253886af46625fed
Maxim Borzov <max.borzov@gmail.com>
2016-11-29 01:47:20 +0300

    Правка письма для Startup Village 2017

---
+++ protected/modules/event/components/handlers/register/Startupvillage17.php
@@ -0,0 +1,12 @@
+<?php
+namespace event\components\handlers\register;
+
+class Startupvillage17 extends Base
+{
+
+    public function getFromName()
+    {
+        return 'Startup Village 2017';
+    }
+
+}
