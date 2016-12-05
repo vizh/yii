f98441c62a804c552f6d09e0ec94453e21e3ba53
Максим Махнюк <star.absorber@gmail.com>
2016-12-05 19:48:59 +0300

    Рефакторинг / Подключение Yii через Composer, удаление фреймворка из репозитория.

--- www/index.php
+++ www/index.php
@@ -2,8 +2,8 @@
 
 define('BASE_PATH', dirname(__DIR__));
 
+require BASE_PATH.'/vendor/autoload.php';
 require BASE_PATH.'/config/init.php';
 require BASE_PATH.'/protected/Yii.php';
-require BASE_PATH.'/vendor/autoload.php';
 
 Yii::createWebApplication(BASE_PATH.'/config/main.php')->run();
