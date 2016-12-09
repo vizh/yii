e997f940dac529505ba7ca999be689213085dd06
Никита Андреевич <bazifal@gmail.com>
2016-12-09 13:35:10 +0300

    Closes #1684: Добавлять ID в виджет регистрации

--- protected/widgets/views/auth/modal-bootstrap3.php
+++ protected/widgets/views/auth/modal-bootstrap3.php
@@ -1,4 +1,4 @@
-<div id="ModalAuth" class="modal fade" style="outline-style: none; overflow: hidden; margin-top: 0; position: absolute;" data-src="<?=\Yii::app()->createUrl('/oauth/main/auth')?>" data-width="640" data-height="662" tabindex="-1" role="dialog" data-bootstrap-version="3">
+<div id="ModalAuth" class="modal fade" style="outline-style: none; overflow: hidden; margin-top: 0; position: absolute;" data-src-login="<?=\Yii::app()->createUrl('/oauth/main/auth')?>" data-src-register="<?=\Yii::app()->createUrl('/oauth/main/register')?>" data-width="640" data-height="662" tabindex="-1" role="dialog" data-bootstrap-version="3">
     <div class="modal-dialog">
 
     </div>
