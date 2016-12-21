d04e707fb9dfa5e73f9d492ba426926c5dfca383
Максим Махнюк <star.absorber@gmail.com>
2016-12-21 11:36:13 +0300

    Closes #5120: Api / Решение проблем обработки исчерпания квот: # Предотвращена блокировка любого аккаунта для которого не была указана квота. Решение проблем фэкапа на запуске. # Теперь происходит выборка только partner_woc аккаунтов. # Решена проблема из-за которой время в причине блокировки устанавливалось всегда во время последнего запуска скрипта. Блокировка происходила каждый раз, даже если аккаунт был уже заблокирован. # Отображение отладочной информации в процессе работы.

--- protected/commands/ApiCommand.php
+++ protected/commands/ApiCommand.php
@@ -1,14 +1,29 @@
 <?php
 
 use api\models\Account;
+use application\components\CDbCriteria;
 use application\components\console\BaseConsoleCommand;
 
 class ApiCommand extends BaseConsoleCommand
 {
     public function actionQuotas()
     {
-        foreach (Account::model()->findAll() as $account) {
-            if (count($account->QuotaUsers) > $account->QuotaByUser) {
+        $accounts = Account::model()
+            ->with('QuotaUsers')
+            ->byRole(Account::ROLE_PARTNER_WOC)
+            ->findAll(CDbCriteria::create()
+                ->addCondition('"t"."QuotaByUser" > 0'));
+
+        foreach ($accounts as $account) {
+            // Немного информации для отладки
+            printf("AccountKey: %s, QuotaByUser: %d, QuotaUsed: %d\n",
+                $account->Key,
+                $account->QuotaByUser,
+                count($account->QuotaUsers)
+            );
+
+            // Блокируем аккаунт, если превышена квота по пользователям
+            if ($account->Blocked !== true && count($account->QuotaUsers) > $account->QuotaByUser) {
                 $account->Blocked = true;
                 $account->BlockedReason = 'Заблокирован в '.date('Y-m-d H:i')." по причине исчерпания квоты в {$account->QuotaByUser} посетителей";
                 $account->save(false);
