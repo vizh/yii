acd51302b09ac8bf3dbc05adfd8724ea0c4a3bb4
Максим Махнюк <star.absorber@gmail.com>
2016-12-21 14:24:46 +0300

    Closes #5120: Api / Поправлена миграция.

--- protected/migrations/m161219_120509_quota_log_pk.php
+++ protected/migrations/m161219_120509_quota_log_pk.php
@@ -4,6 +4,8 @@ class m161219_120509_quota_log_pk extends CDbMigration
 {
 	public function safeUp()
 	{
+	    // Очищаем накопленные данные по квотам, так как есть дубликаты и не получится создать ключ
+        $this->delete('ApiAccountQuotaByUserLog');
 	    $this->addPrimaryKey('ApiAccountQuotaByUserLog_pkey', 'ApiAccountQuotaByUserLog', ['AccountId', 'UserId']);
 	    $this->addColumn('ApiAccount', 'BlockedReason', 'text');
 	}
@@ -11,6 +13,6 @@ class m161219_120509_quota_log_pk extends CDbMigration
 	public function safeDown()
 	{
 	    $this->dropColumn('ApiAccount', 'BlockedReason');
-	    $this->dropForeignKey('ApiAccountQuotaByUserLog_pkey', 'ApiAccountQuotaByUserLog');
+        $this->dropPrimaryKey('ApiAccountQuotaByUserLog_pkey', 'ApiAccountQuotaByUserLog');
 	}
 }
