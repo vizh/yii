616a696dcb649fd87ff932f19260b8a7978da38e
Maxim Borzov <max.borzov@gmail.com>
2016-12-28 10:37:32 +0300

    Кастомный отправитель регистрационных писем по CSF17

---
+++ protected/modules/event/components/handlers/register/Csf17.php
@@ -0,0 +1,12 @@
+<?php
+namespace event\components\handlers\register;
+
+class Csf17 extends Base
+{
+
+    public function getFromName()
+    {
+        return 'CSF 2017 // Форум по кибербезопасности';
+    }
+
+}
