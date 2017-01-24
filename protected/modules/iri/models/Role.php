6e39b43ec3575583988996d6f3a6ec7eca016ed9
Максим Махнюк <star.absorber@gmail.com>
2017-01-24 14:56:39 +0300

    Рефакторинг / Единые правила оформления и функционал моделей модуля iri.

--- protected/modules/iri/models/Role.php
+++ protected/modules/iri/models/Role.php
@@ -1,37 +1,42 @@
 <?php
 namespace iri\models;
+
 use application\components\ActiveRecord;
 
 /**
- * This is the model class for table "IriRole".
- *
- * The followings are the available columns in table 'IriRole':
  * @property integer $Id
  * @property string $Title
  * @property integer $Priority
  *
- * @method Role findByPk()
+ * Описание вспомогательных методов
+ * @method Role   with($condition = '')
+ * @method Role   find($condition = '', $params = [])
+ * @method Role   findByPk($pk, $condition = '', $params = [])
+ * @method Role   findByAttributes($attributes, $condition = '', $params = [])
+ * @method Role[] findAll($condition = '', $params = [])
+ * @method Role[] findAllByAttributes($attributes, $condition = '', $params = [])
  *
- * The followings are the available model relations:
+ * @method Role byId(int $id, bool $useAnd = true)
  */
 class Role extends ActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Role the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     *
+     * @param string $className active record class name.
+     * @return Role the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'IriRole';
-	}
-
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'IriRole';
+    }
 }
--- protected/modules/iri/models/User.php
+++ protected/modules/iri/models/User.php
@@ -1,5 +1,6 @@
 <?php
 namespace iri\models;
+
 use application\components\ActiveRecord;
 use application\models\ProfessionalInterest;
 
@@ -7,6 +8,7 @@ use application\models\ProfessionalInterest;
  * This is the model class for table "IriUser".
  *
  * The followings are the available columns in table 'IriUser':
+ *
  * @property integer $Id
  * @property integer $UserId
  * @property integer $RoleId
@@ -15,61 +17,69 @@ use application\models\ProfessionalInterest;
  * @property string $Type
  * @property integer $ProfessionalInterestId
  *
- * The followings are the available model relations:
  * @property \user\models\User $User
  * @property Role $Role
  * @property ProfessionalInterest $ProfessionalInterest
  *
- * @method User byUserId(integer $userId)
- * @method User byRoleId(integer $roleId)
- * @method User byType(string $type)
- * @method User byProfessionalInterestId(integer $professionalInterestId)
- * @method User find()
+ * Описание вспомогательных методов
+ * @method User   with($condition = '')
+ * @method User   find($condition = '', $params = [])
+ * @method User   findByPk($pk, $condition = '', $params = [])
+ * @method User   findByAttributes($attributes, $condition = '', $params = [])
+ * @method User[] findAll($condition = '', $params = [])
+ * @method User[] findAllByAttributes($attributes, $condition = '', $params = [])
+ *
+ * @method User byId(int $id, bool $useAnd = true)
+ * @method User byUserId(int $id, bool $useAnd = true)
+ * @method User byRoleId(int $id, bool $useAnd = true)
+ * @method User byType(string $type, bool $useAnd = true)
+ * @method User byProfessionalInterestId(int $profinterest, bool $useAnd = true)
  */
 class User extends ActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return User the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     *
+     * @param string $className active record class name.
+     * @return User the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'IriUser';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'IriUser';
+    }
 
-
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		return [
-			'User' => [self::BELONGS_TO, '\user\models\User', 'UserId'],
-			'Role' => [self::BELONGS_TO, '\iri\models\Role', 'RoleId'],
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        return [
+            'User' => [self::BELONGS_TO, '\user\models\User', 'UserId'],
+            'Role' => [self::BELONGS_TO, '\iri\models\Role', 'RoleId'],
             'ProfessionalInterest' => [self::BELONGS_TO, '\application\models\ProfessionalInterest', 'ProfessionalInterestId']
-		];
-	}
+        ];
+    }
 
     /**
      * @return string
      */
-    function __toString()
+    public function __toString()
     {
         $result = $this->Role->Title;
         if (!empty($this->ProfessionalInterest)) {
-            $result .= ', ' . \CHtml::link(\Yii::t('app', 'экосистема') . ' «' . $this->ProfessionalInterest->Title . '»', 'http://ири.рф/experts/', ['target' => '_blank']);
+            $result .= ', '.\CHtml::link(\Yii::t('app', 'экосистема').' «'.$this->ProfessionalInterest->Title.'»', 'http://ири.рф/experts/', ['target' => '_blank']);
         }
+
         return $result;
     }
-
 
 }
