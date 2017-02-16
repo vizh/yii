8059bca21908734b6b7d188f1024ebb95f900ae8
Максим Махнюк <star.absorber@gmail.com>
2017-02-16 20:47:43 +0300

    Рефакторинг / Лишний код.

--- protected/modules/partner/PartnerModule.php
+++ protected/modules/partner/PartnerModule.php
@@ -2,31 +2,4 @@
 
 class PartnerModule extends CWebModule
 {
-
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
-			return true;
-		}
-		else
-			return false;
-	}
-
-  public function afterControllerAction($controller, $action)
-  {
-    parent::afterControllerAction($controller, $action);
-//
-//    echo 'Executiontime:' . Yii::getLogger()->executionTime . '<br>';
-//
-//    echo 'Memory:' . Yii::getLogger()->getMemoryUsage() . '<br>';
-//
-//    $logs = Yii::getLogger()->getProfilingResults();
-//
-//    echo '<pre>';
-//    var_dump($logs);
-//    echo '</pre>';
-  }
-
-
 }
