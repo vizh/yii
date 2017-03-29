7dafeb4efb4f429212550e9b62630218a8cfa2f4
borzov <max.borzov@gmail.com>
2017-03-29 23:39:56 +0300

    Обновление лого PayOnline и текста на странице с информацией об оплате

--- protected/modules/page/views/info/pay.php
+++ protected/modules/page/views/info/pay.php
@@ -12,12 +12,13 @@
   <div class="container">
     <div class="row">
       <article class="content span8 offset1">
-        <p>Вы можете оплатить свой заказ банковской картой <strong>Visa</strong> или <strong>MasterCard</strong>, либо электронными деньгами <strong>WebMoney</strong>, <strong>Яндекс.Деньги</strong> или <strong>QIWI</strong> через процессинговый центр нашего партнера (<a href="http://payonline.ru" target="_blank">PayOnline</a> или <a href="http://uniteller.ru" target="_blank">Uniteller</a>), осуществляющий интернет эквайринг.</p>
+        <p>Вы можете оплатить свой заказ банковской картой <strong>Visa</strong> или <strong>MasterCard</strong>, либо электронными деньгами <strong>Яндекс.Деньги</strong> через процессинговый центр нашего партнера (<a href="http://payonline.ru" target="_blank">PayOnline</a>), осуществляющий интернет эквайринг. Это простой, удобный и безопасный способ оплаты с моментальным зачислением средств.</p>
+        <p>Дополнительно услуги могут быть оплачены физифческими через <strong>PayPal</strong> или по <strong>квитанции</strong>, а для юридических лиц система может сгенерировать <strong>счет на оплату</strong> с последующей выдачей закрывающих документов на мероприятии или их отправкой по почте.</p>
         <p>После подтверждения заказа Вы будете перенаправлены на защищенную платежную страницу, где Вам необходимо ввести данные Вашей банковской карты или реквизиты электронного кошелька.</p>
         <p>После успешной оплаты Вы получите электронный чек. Информация, указанная на чеке, содержит все данные о проведенной платежной транзакции.</p>
       </article>
-      <aside class="content span2">
-        <p><a href="http://payonline.ru" target="_blank"><img src="http://getlogo.org/img/payonline/10/120x/" alt="PayOnline" title="PayOnline" /></a></p>
+      <aside class="content span2 text-center">
+        <p><a href="http://payonline.ru" target="_blank"><img src="http://getlogo.org/img/payonline/1513/120x/" alt="PayOnline" title="PayOnline" /></a></p>
         <p>Эксклюзивный платежный партнер</p>
       </aside>
     </div>
