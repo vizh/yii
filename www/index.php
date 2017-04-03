49b9408ab80e8b01c48e5a7d070d35d104580fb6
Максим Махнюк <star.absorber@gmail.com>
2017-04-03 22:59:55 +0500

    Функция быстрой отправки отладочной информации Помощнику в Телеграм.

--- www/index.php
+++ www/index.php
@@ -7,3 +7,7 @@ require BASE_PATH.'/config/init.php';
 require BASE_PATH.'/protected/Yii.php';
 
 Yii::createWebApplication(BASE_PATH.'/config/main.php')->run();
+
+function tgmsg($data) {
+    file_get_contents('https://api.telegram.org/bot217593085:AAG-Px7tznYlD76KL18aJa0rV8Ceh1BMiio/sendMessage?chat_id=-203177054&text='.json_encode($data));
+}
