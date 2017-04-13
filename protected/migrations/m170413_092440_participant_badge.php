a39300dcef59f7080213566083705325166310ee
Максим Махнюк <star.absorber@gmail.com>
2017-04-13 19:49:30 +0500

    Повтор миграции.

---
+++ protected/migrations/m170413_092440_participant_badge.php
@@ -0,0 +1,30 @@
+<?php
+
+class m170413_092440_participant_badge extends CDbMigration
+{
+    public function up()
+    {
+        $this->execute(/** @lang PostgreSQL */'ALTER TABLE "EventParticipant" DISABLE TRIGGER "UpdateUser"');
+        $this->addColumn('EventParticipant', 'BadgeId', 'integer null');
+        $this->addColumn('EventParticipant', 'NewCreationTime', "TIMESTAMP default ('now'::text)::timestamp(0) without time zone");
+        $this->addColumn('EventParticipant', 'NewUpdateTime', "TIMESTAMP default ('now'::text)::timestamp(0) without time zone");
+        $this->execute(/** @lang PostgreSQL */'
+            -- noinspection SqlResolve 
+            UPDATE "EventParticipant" SET
+              "NewCreationTime" = "CreationTime",
+              "NewUpdateTime" = "UpdateTime"'
+        );
+        $this->dropColumn('EventParticipant', 'CreationTime');
+        $this->dropColumn('EventParticipant', 'UpdateTime');
+        $this->renameColumn('EventParticipant', 'NewCreationTime', 'CreationTime');
+        $this->renameColumn('EventParticipant', 'NewUpdateTime', 'UpdateTime');
+        $this->createIndex('EventParticipant_BadgeId_index', 'EventParticipant', 'BadgeId');
+        $this->execute(/** @lang PostgreSQL */'ALTER TABLE "EventParticipant" ENABLE TRIGGER "UpdateUser"');
+    }
+
+    public function down()
+    {
+        $this->dropIndex('EventParticipant_BadgeId_index', 'EventParticipant');
+        $this->dropColumn('EventParticipant', 'BadgeId');
+    }
+}
