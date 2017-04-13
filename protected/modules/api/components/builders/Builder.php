6f90c9495a0156d40eaf7f365792bfcb6f10fc1f
Максим Махнюк <star.absorber@gmail.com>
2017-04-13 19:13:37 +0500

    Восстановление отменённых изменений предыдущим коммитом.

--- protected/modules/api/components/builders/Builder.php
+++ protected/modules/api/components/builders/Builder.php
@@ -215,6 +215,10 @@ class Builder
     {
         $isOnePart = empty($this->account->Event->Parts);
         foreach ($user->Participants as $participant) {
+            // Для оффлайн сервисов добавляем в выдачу идентификатор RFID-бейджа
+            if ($this->account->Role === Account::ROLE_OFFLINE && empty($participant->BadgeId) !== true) {
+                $this->user->BadgeId = $participant->BadgeId;
+            }
             if ($participant->EventId == $this->account->EventId) {
                 if ($isOnePart) {
                     $this->user->Status = new \stdClass();
--- protected/modules/paperless/models/Material.php
+++ protected/modules/paperless/models/Material.php
@@ -18,6 +18,21 @@ use event\models\Event;
  *
  * @property Event $Event
  * @property MaterialLinkRole[] $RoleLinks
+ *
+ * Описание вспомогательных методов
+ * @method Material   with($condition = '')
+ * @method Material   find($condition = '', $params = [])
+ * @method Material   findByPk($pk, $condition = '', $params = [])
+ * @method Material   findByAttributes($attributes, $condition = '', $params = [])
+ * @method Material[] findAll($condition = '', $params = [])
+ * @method Material[] findAllByAttributes($attributes, $condition = '', $params = [])
+ *
+ * @method Material byId(int $id, bool $useAnd = true)
+ * @method Material byEventId(int $id, bool $useAnd = true)
+ * @method Material byName(string $name, bool $useAnd = true)
+ * @method Material byComment(string $comment, bool $useAnd = true)
+ * @method Material byFile(string $file, bool $useAnd = true)
+ * @method Material byActive(bool $active, bool $useAnd = true)
  */
 class Material extends ActiveRecord
 {
