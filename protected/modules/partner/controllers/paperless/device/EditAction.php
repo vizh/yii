0b925c49e5c097b7571e7ab59b805efc79255e18
Максим Махнюк <star.absorber@gmail.com>
2017-05-28 00:20:44 +0500

    Партнёрка / Отображение лога событий для каждого устройства без учёта правил обработки.

--- protected/modules/partner/controllers/paperless/device/EditAction.php
+++ protected/modules/partner/controllers/paperless/device/EditAction.php
@@ -4,6 +4,7 @@ namespace partner\controllers\paperless\device;
 
 use application\components\Exception;
 use application\models\paperless\Device as DeviceModel;
+use application\models\paperless\DeviceSignal;
 use partner\components\Action;
 use partner\models\forms\paperless\Device;
 use Yii;
@@ -33,9 +34,16 @@ class EditAction extends Action
             }
         }
 
+        $signals = DeviceSignal::model()
+            ->byDeviceNumber($device->DeviceNumber)
+            ->with(['Participant' => ['with' => ['User' => ['with' => ['Employments', 'Settings', 'LinkPhones']]]]])
+            ->orderBy(['"t"."Id"' => SORT_DESC])
+            ->findAll();
+
         $this->getController()->render('device/edit', [
             'form' => $form,
-            'device' => $device
+            'device' => $device,
+            'signals' => $signals
         ]);
     }
 }
