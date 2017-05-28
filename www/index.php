be69abbdb67dba833eab520b20af90b826a170d2
Максим Махнюк <star.absorber@gmail.com>
2017-05-28 17:39:15 +0500

    Поддержка HTML форматирования для отладочной функции tgmsg().

--- www/index.php
+++ www/index.php
@@ -8,6 +8,13 @@ require BASE_PATH.'/protected/Yii.php';
 
 Yii::createWebApplication(BASE_PATH.'/config/main.php')->run();
 
-function tgmsg($data) {
-    file_get_contents('https://api.telegram.org/bot217593085:AAG-Px7tznYlD76KL18aJa0rV8Ceh1BMiio/sendMessage?chat_id=-203177054&text='.json_encode($data, JSON_UNESCAPED_UNICODE));
+function tgmsg($data)
+{
+    (new GuzzleHttp\Client())->get('https://api.telegram.org/bot217593085:AAG-Px7tznYlD76KL18aJa0rV8Ceh1BMiio/sendMessage', [
+        'query' => [
+            'chat_id' => -203177054,
+            'parse_mode' => 'HTML',
+            'text' => is_string($data) ? $data : json_encode($data)
+        ]
+    ]);
 }
