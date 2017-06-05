78a2407186ea52b62ba386da25f20fc3194c1876
Максим Махнюк <star.absorber@gmail.com>
2017-06-05 16:04:31 +0500

    Микрооптимизация количества вызова функций и магических методов на каждом хите.

--- protected/components/auth/PhpAuthManager.php
+++ protected/components/auth/PhpAuthManager.php
@@ -1,35 +1,37 @@
 <?php
+
 namespace application\components\auth;
+
+use application\models\admin\Group;
+use Yii;
 
 class PhpAuthManager extends \CPhpAuthManager
 {
-  public function init()
-  {
-    // Иерархию ролей расположим в файле auth.php в директории config приложения
-    if($this->authFile === null)
+    public function init()
     {
-      $this->authFile = \Yii::getPathOfAlias('application.components.auth.auth').'.php';
-    }
-
-    parent::init();
-
-    // Для гостей у нас и так роль по умолчанию guest.
-    if(!\Yii::app()->user->getIsGuest())
-    {
-      /** @var $groups \application\models\admin\Group[] */
-      $groups = \application\models\admin\Group::model()
-          ->byUserId(\Yii::app()->user->getCurrentUser()->Id)
-          ->with('Roles')->findAll();
-      if (sizeof($groups) !== null)
-      {
-        foreach ($groups as $group)
-        {
-          foreach ($group->Roles as $role)
-          {
-            $this->assign($role->Code, \Yii::app()->user->getId());
-          }
+        // Иерархию ролей расположим в файле auth.php в директории config приложения
+        if ($this->authFile === null) {
+            $this->authFile = Yii::getPathOfAlias('application.components.auth.auth').'.php';
         }
-      }
+
+        parent::init();
+
+        // Для гостей у нас и так роль по умолчанию guest.
+        $user = Yii::app()->getUser();
+        if (false === $user->getIsGuest()) {
+            $groups = Group::model()
+                ->byUserId($user->getCurrentUser()->Id)
+                ->with('Roles')
+                ->findAll();
+
+            if (false === empty($groups)) {
+                $userid = $user->getId();
+                foreach ($groups as $group) {
+                    foreach ($group->Roles as $role) {
+                        $this->assign($role->Code, $userid);
+                    }
+                }
+            }
+        }
     }
-  }
 }
