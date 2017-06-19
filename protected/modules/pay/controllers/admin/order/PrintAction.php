c6ff492dd4f88fca71d4c21294252d71ff8ec516
Максим Махнюк <star.absorber@gmail.com>
2017-06-19 14:12:22 +0500

    Админка / Решена проблема с числом страниц в выгрузке счетов на печать.

--- protected/modules/pay/controllers/admin/order/PrintAction.php
+++ protected/modules/pay/controllers/admin/order/PrintAction.php
@@ -2,20 +2,27 @@
 
 namespace pay\controllers\admin\order;
 
+use application\components\utility\Paginator;
+use event\models\Event;
+use pay\models\forms\admin\OrderPrint;
+use pay\models\Order;
+
 class PrintAction extends \CAction
 {
     public function run()
     {
         $request = \Yii::app()->getRequest();
 
-        $paginator = null;
         $event = null;
 
-        $form = new \pay\models\forms\admin\OrderPrint();
+        $form = new OrderPrint();
         $form->attributes = $request->getParam(get_class($form));
+
         if ($request->getParam('find') !== null && $form->validate()) {
-            $event = \event\models\Event::model()->findByPk($form->EventId);
-            if ($event == null) {
+            $event = Event::model()
+                ->findByPk($form->EventId);
+
+            if ($event === null) {
                 throw new \CHttpException(404);
             }
 
@@ -27,14 +34,19 @@ class PrintAction extends \CAction
                 $criteria->addCondition('"t"."PaidTime" >= :DateFrom');
                 $criteria->params['DateFrom'] = \Yii::app()->getDateFormatter()->format('yyyy-MM-dd', $form->DateFrom).' 00:00:00';
             }
-            $count = \pay\models\Order::model()->byJuridical(true)->count($criteria);
-            $paginator = new \application\components\utility\Paginator($count, ['print' => 1]);
+
+            $count = Order::model()
+                ->byJuridical(true)
+                ->count($criteria);
+
+            $paginator = new Paginator($count, ['print' => 1]);
+            $paginator->pages = null;
             $paginator->perPage = 50;
 
             if ($request->getParam('print') !== null) {
                 $this->getController()->layout = '/layouts/bill';
                 $criteria->mergeWith($paginator->getCriteria());
-                $orders = \pay\models\Order::model()->byJuridical(true)->findAll($criteria);
+                $orders = Order::model()->byJuridical(true)->findAll($criteria);
                 $content = '';
                 foreach ($orders as $order) {
                     $content .= $this->renderOrder($order);
@@ -47,10 +59,14 @@ class PrintAction extends \CAction
             }
         }
         $this->getController()->setPageTitle(\Yii::t('app', 'Печать счетов'));
-        $this->getController()->render('print', ['paginator' => $paginator, 'form' => $form, 'event' => $event]);
+        $this->getController()->render('print', [
+            'paginator' => $paginator,
+            'form' => $form,
+            'event' => $event
+        ]);
     }
 
-    private function renderOrder(\pay\models\Order $order)
+    private function renderOrder(Order $order)
     {
         $result = $this->getController()->renderPartial('pay.views.order.'.str_replace('/', '.', $order->getViewName()), [
             'order' => $order,
@@ -63,4 +79,4 @@ class PrintAction extends \CAction
         $result = '<div style="page-break-after: always;">'.$result.'</div>';
         return $result;
     }
-} 
+}
