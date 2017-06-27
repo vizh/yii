5fccbd00dfcffd82adf35bcaa76a92d1a626bdde
Максим Махнюк <star.absorber@gmail.com>
2017-06-27 15:21:42 +0500

    Api / Документирован и введён в строй метод загрузки фотографии пользователя из multipart файла или ссылки на файл.

--- protected/modules/api/controllers/user/SetphotoAction.php
+++ protected/modules/api/controllers/user/SetphotoAction.php
@@ -4,36 +4,50 @@ namespace api\controllers\user;
 
 use api\components\Action;
 use api\components\Exception;
+use CUploadedFile;
+use nastradamus39\slate\annotations\Action\Param;
+use nastradamus39\slate\annotations\Action\Request;
+use nastradamus39\slate\annotations\ApiAction;
 use user\models\forms\edit\Photo;
-use user\models\User;
 
-/**
- * Class SetphotoAction
- * @package api\controllers\user
- */
 class SetphotoAction extends Action
 {
     /**
-     * @param int $RunetId
-     * @throws Exception
+     * @ApiAction(
+     *     controller="User",
+     *     title="Установка фотографии",
+     *     description="Устанавливает фотографию посетителя из файла изображения или ссылки на него.",
+     *     request=@Request(
+     *          method="GET",
+     *          url="/user/setphoto",
+     *          body="",
+     *          params={
+     *              @Param(title="Photo", type="Файл", defaultValue="", description="Файл с фотографией посетителя."),
+     *              @Param(title="PhotoUrl", type="Строка", defaultValue="", description="URL адрес фотографии посетителя.")
+     *          }
+     *     )
+     * )
      */
-    public function run($RunetId)
+    public function run()
     {
-        # TODO: права на изменение фотки
-        $user = User::model()->byRunetId($RunetId)->find();
+        $user = $this->getRequestedUser();
 
-        if (!$user) {
-            throw new Exception(202, [$RunetId]);
-        }
+        if ($this->hasRequestParam('Photo')) {
+            $form = new Photo();
+            $form->Image = CUploadedFile::getInstanceByName('Photo');
 
-        $form = new Photo();
+            if ($form->validate() === false) {
+                throw new Exception(3008, [$this->getRequestedUser()->RunetId]);
+            }
 
-        $form->Image = \CUploadedFile::getInstanceByName('Image');
-        if ($form->validate()) {
             $user->getPhoto()->SavePhoto($form->Image);
-            $this->setSuccessResult();
-        } else {
-            throw new Exception(3008, [$RunetId]);
         }
+
+        if ($this->hasRequestParam('PhotoUrl')) {
+            $user->getPhoto()->save($this->getRequestParam('PhotoUrl'));
+            $user->save();
+        }
+
+        $this->setSuccessResult();
     }
 }
