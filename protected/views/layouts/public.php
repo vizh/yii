34863ea1db50cf1ed9ac63d857ac8e754aa0fae5
borzov <max.borzov@gmail.com>
2017-07-06 19:06:18 +0300

    GA

--- protected/views/layouts/public.php
+++ protected/views/layouts/public.php
@@ -146,6 +146,14 @@
     </div>
 </footer>
 
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-54464656-2', 'auto', {'allowLinker': true});
+    ga('require', 'linker');
+    ga('linker:autoLink', ['pm-moscow.com'] );
+    ga('send', 'pageview');
+</script>
+
 <?$this->renderPartial('//layouts/counters/google')?>
 <?$this->renderPartial('//layouts/counters/yandex')?>
 
