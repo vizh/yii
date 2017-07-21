883b89c7db168d46d8d01f1a93a4316f303fe225
borzov <max.borzov@gmail.com>
2017-07-21 13:06:03 +0300

    Сквозная аналитика для PM-2017

--- protected/views/layouts/public.php
+++ protected/views/layouts/public.php
@@ -22,8 +22,20 @@
     <!--[if lte IE 7]>
     <link rel="stylesheet" href="/stylesheets/ie/lte-ie-7.css">
     <![endif]-->
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T5ZMCQC');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body id="<?=$this->bodyId?>">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5ZMCQC"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <noscript>JavaScript disabled</noscript>
 <header id="header" role="banner">
     <div class="navbar navbar-fixed-top navbar-inverse">
@@ -145,14 +157,6 @@
         </div>
     </div>
 </footer>
-
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-54464656-2', 'auto', {'allowLinker': true});
-    ga('require', 'linker');
-    ga('linker:autoLink', ['pm-moscow.com'] );
-    ga('send', 'pageview');
-</script>
 
 <?$this->renderPartial('//layouts/counters/google')?>
 <?$this->renderPartial('//layouts/counters/yandex')?>
