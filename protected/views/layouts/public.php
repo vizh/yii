19d9ea116706d58c4d02fb3a379c65f6ecd47d38
borzov <max.borzov@gmail.com>
2017-08-01 17:54:20 +0300

    Удаление сквозной аналитики для PM-2017

--- protected/views/layouts/public.php
+++ protected/views/layouts/public.php
@@ -22,20 +22,9 @@
     <!--[if lte IE 7]>
     <link rel="stylesheet" href="/stylesheets/ie/lte-ie-7.css">
     <![endif]-->
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-T5ZMCQC');</script>
-    <!-- End Google Tag Manager -->
     <? $this->renderClip('event-counter-head'); ?>
 </head>
 <body id="<?=$this->bodyId?>">
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5ZMCQC"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
 <noscript>JavaScript disabled</noscript>
 <header id="header" role="banner">
